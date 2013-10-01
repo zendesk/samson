4b1d6b3f0fef16512952f9964fe665f42ba6c271
Steven Davidovitz <sdavidovitz@zendesk.com>
2013-10-01 14:08:48 -0700

    resque config for heroku

--- config/initializers/redis.rb
+++
@@ -1,6 +0,0 @@
-require 'redis'
-
-redis_config = YAML.load_file(Rails.root + 'config/redis.yml')[Rails.env]
-host, port = redis_config.split(":")
-
-
--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,5 +1,10 @@
 require 'resque'
 
-resque_config = YAML.load_file(Rails.root + 'config/resque.yml')[Rails.env]
-host, port = resque_config.split(":")
-Resque.redis = Redis.new(host: host, port: port, driver: :ruby)
+if ENV["REDISTOGO_URL"]
+  uri = URI.parse(ENV["REDISTOGO_URL"])
+  Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+else
+  resque_config = YAML.load_file(Rails.root + 'config/resque.yml')[Rails.env]
+  host, port = resque_config.split(":")
+  Resque.redis = Redis.new(host: host, port: port, driver: :ruby)
+end
