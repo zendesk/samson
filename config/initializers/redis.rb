e9f15d8393a5cf7b90e2bb8f424ab9e57a607070
Steven Davidovitz <sdavidovitz@zendesk.com>
2013-10-01 16:18:12 -0700

    don't try and load redis.yml in prod

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -3,8 +3,8 @@ require 'redis'
 if ENV["REDISTOGO_URL"]
   uri = URI.parse(ENV["REDISTOGO_URL"])
   REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
-else
-  redis_config = YAML.load_file(Rails.root + 'config/redis.yml')[Rails.env]
+elsif !Rails.env.production?
+  redis_config = YAML.load_file(Rails.root.join('config/redis.yml'))[Rails.env]
   host, port = redis_config.split(":")
   REDIS = Redis.new(host: host, port: port, driver: :ruby)
 end
