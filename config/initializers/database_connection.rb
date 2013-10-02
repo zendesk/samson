4e828737da8b80a14ab8ee153f02b27cf5296c1d
Steven Davidovitz <sdavidovitz@zendesk.com>
2013-10-02 10:29:31 -0700

    recommended by heroku https://devcenter.heroku.com/articles/concurrency-and-database-connections

---
+++ config/initializers/database_connection.rb
@@ -0,0 +1,10 @@
+Rails.application.config.after_initialize do
+  ActiveRecord::Base.connection_pool.disconnect!
+
+  ActiveSupport.on_load(:active_record) do
+    config = Rails.application.config.database_configuration[Rails.env]
+    config['reaping_frequency'] = ENV['DB_REAP_FREQ'] || 10 # seconds
+    config['pool']              = ENV['DB_POOL']      || 5
+    ActiveRecord::Base.establish_connection(config)
+  end
+end
