d37198dc914a87cc48239319a9f49e01dbcd5493
Steven Davidovitz <sdavidovitz@zendesk.com>
2013-10-02 11:19:56 -0700

    Revert "move ssh modification into deploy"
    This reverts commit 44ac5070fd3b1d023c1931bd136371a5e08e1d52.

--- app/jobs/deploy.rb
+++ app/jobs/deploy.rb
@@ -1,15 +1,6 @@
 require 'net/ssh'
 require 'net/ssh/shell'
 
-Net::SSH::Connection::Session.class_eval do
-  alias :old_loop :loop
-  # Non-blocking loop, Net::SSH doesn't
-  # let us pass through when using start
-  def loop(wait = 0, &block)
-    old_loop(wait, &block)
-  end
-end
-
 class Deploy
   STOP_MESSAGE = "stop"
 
