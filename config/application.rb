325ef5f36cf0cc9c3603c2489dc6d2ca32d6db11
Steven Davidovitz <sdavidovitz@zendesk.com>
2013-10-02 10:44:02 -0700

    require assets

--- config/application.rb
+++ config/application.rb
@@ -4,7 +4,7 @@ require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
-Bundler.require(:default, Rails.env)
+Bundler.require(:default, :assets, Rails.env)
 
 module ZendeskPusher
   class Application < Rails::Application
