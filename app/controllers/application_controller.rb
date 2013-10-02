b54b0d67ebea35023235e473fa473be4b7fed863
Steven Davidovitz <sdavidovitz@zendesk.com>
2013-10-01 20:48:03 -0700

    force_ssl in prod

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   include CurrentUser
 
+  force_ssl if Rails.env.production?
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
