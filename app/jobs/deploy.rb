428c9454185fa6b25d8e1cd7d9dd906f93e6c5ec
Steven Davidovitz <sdavidovitz@zendesk.com>
2013-10-07 16:51:07 -0700

    add a timeout for ssh-agent

--- app/jobs/deploy.rb
+++ app/jobs/deploy.rb
@@ -49,12 +49,18 @@ class Deploy
       Process.spawn("bash -c \"#{Rails.root.join("lib/ssh-agent.sh")} #{options[:passphrase]}\"")
       socket = Rails.root.join("tmp/auth_sock")
 
-      until File.exist?(socket)
-        if @stopped
-          publish_messages("Deploy stopped.\n")
-          success = false
-          break
+      begin
+        Timeout.timeout(5) do
+          until File.exist?(socket)
+            if @stopped
+              publish_messages("Deploy stopped.\n")
+              return false
+            end
+          end
         end
+      rescue Timeout::Error
+        publish_messages("SSH Agent failed to start.\n")
+        return false
       end
 
       ENV["SSH_AUTH_SOCK"] = File.readlink(socket)
