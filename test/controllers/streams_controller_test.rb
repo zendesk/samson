4d5c1ba0ba2a7d6f672d5e04b6f14d56579855a3
Steven Davidovitz <sdavidovitz@zendesk.com>
2013-10-07 19:10:52 -0700

    fix streams controller test

--- test/controllers/streams_controller_test.rb
+++ test/controllers/streams_controller_test.rb
@@ -1,9 +1,4 @@
 require 'test_helper'
 
-class StreamsControllerTest < ActionController::TestCase
-  test "should get show" do
-    get :show
-    assert_response :success
-  end
-
+describe StreamsController do
 end
