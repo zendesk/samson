0c7033836631961a069f7829e2dfcf853333539f
Steven Davidovitz <sdavidovitz@zendesk.com>
2013-10-07 19:11:11 -0700

    remove info message

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -14,15 +14,12 @@ Redis.instance_eval do
 end
 
 Thread.abort_on_exception = !Rails.env.production?
-
 Thread.main[:streams] = {}
 
 thread = Thread.new do
   include ApplicationHelper # for render_log
 
   Redis.driver.psubscribe("deploy:*") do |on|
-    Rails.logger.info("Subscribed")
-
     on.pmessage do |pattern, channel, message|
       if (streams = Thread.main[:streams][channel])
         data = JSON.dump(msg: render_log(message).to_s)
