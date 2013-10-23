e9dfc22e7bfe2da0414831b382491059568605f6
Steven Davidovitz <sdavidovitz@zendesk.com>
2013-10-23 15:44:35 -0700

    do the opposite of the previous commit; pull only if on a branch because the detached text changed from git 1.7 -> 1.8

--- app/jobs/deploy.rb
+++ app/jobs/deploy.rb
@@ -56,7 +56,7 @@ class Deploy
           "cd #{@job.project.name.parameterize("_")}",
           "git fetch -ap",
           "git checkout -f #{@job.sha}",
-          "(git status | grep 'HEAD detached') || git pull",
+          "(git status | grep 'On branch') && git pull",
           "capsu $(pwd) $(rvm current | tail -1) #{@job.environment} deploy TAG=#{@job.sha}"
         ].each do |command|
           if !exec!(sh, command)
