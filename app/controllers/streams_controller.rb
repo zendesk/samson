35bdfc4f9373ec8fc2c4619643c4faa55b8cf544
Steven Davidovitz <sdavidovitz@zendesk.com>
2014-01-08 14:45:42 +1100

    reconfigure streams a little

--- app/controllers/streams_controller.rb
+++ app/controllers/streams_controller.rb
@@ -2,18 +2,17 @@ class StreamsController < ApplicationController
   include ActionController::Live
   include ApplicationHelper
 
-  before_filter :find_job_execution
-
   def show
     ActiveRecord::Base.connection_pool.release_connection
 
     response.headers['Content-Type'] = 'text/event-stream'
     response.headers['Cache-Control'] = 'no-cache'
 
-    # Heartbeat thread
-    @job_execution.output.each_message do |message|
-      msg = JSON.dump(msg: render_log(message).to_s)
-      response.stream.write("data: #{msg}\n\n")
+    if job.active? && (execution = JobExecution.find_by_id(job.id))
+      execution.output.each_message do |message|
+        msg = JSON.dump(msg: render_log(message).to_s)
+        response.stream.write("data: #{msg}\n\n")
+      end
     end
   rescue IOError
     # Raised on stream close
@@ -23,9 +22,7 @@ class StreamsController < ApplicationController
 
   protected
 
-  def find_job_execution
-    @job_execution = JobExecution.find_by_id(params[:id])
-
-    head :not_found if @job_execution.nil?
+  def job
+    @job ||= Job.find(params[:id])
   end
 end
