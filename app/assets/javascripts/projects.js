32c38113774f97c0027734ba0824e5e57b4a7eb7
Po Chen <pchen@zendesk.com>
2014-01-09 15:35:42 +1100

    Prepare for backend action

--- app/assets/javascripts/projects.js
+++ app/assets/javascripts/projects.js
@@ -1,2 +1,21 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+$(function() {
+  var $stagesBox  = $("#stages"),
+      $saveButton = $("#save");
+
+  $stagesBox.sortable();
+  $saveButton.on("click", function() {
+    console.log($(this).data("url"));
+    $.ajax({
+      url: $(this).data("url"),
+      data: $stagesBox.sortable("serialize", { attribute: "data-order" }),
+      type: 'PUT',
+    }).done(function(data) {
+      console.log(data);
+      console.log("success");
+    }).fail(function() {
+      console.log("failure");
+    });
+  });
+});
