4229c0cf4c0678fc74cf1e8537e609abd83d29de
Steven Davidovitz <sdavidovitz@zendesk.com>
2014-01-09 13:49:58 +1100

    compile assets on the fly in staging

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -20,14 +20,14 @@ ZendeskPusher::Application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = false
+  config.serve_static_assets = true
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs.
   config.assets.digest = true
