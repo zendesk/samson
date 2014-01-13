fd6e674ad1c16643a4ed1393f48d9d16363603c4
Daniel Schierbeck <dasch@zendesk.com>
2014-01-13 02:26:57 +0000

    Send notifications both before and after deploys

--- app/models/deploy_service.rb
+++ app/models/deploy_service.rb
@@ -9,10 +9,12 @@ class DeployService
     job = project.jobs.create!(user: user, command: stage.command)
     deploy = stage.deploys.create!(commit: commit, job: job)
 
+    send_before_notifications(stage, deploy)
+
     job_execution = JobExecution.start_job(commit, job)
 
     job_execution.subscribe do |_|
-      send_notifications(stage, deploy)
+      send_after_notifications(stage, deploy)
     end
 
     deploy
@@ -20,11 +22,19 @@ class DeployService
 
   private
 
-  def send_notifications(stage, deploy)
+  def send_before_notifications(stage, deploy)
+    send_flowdock_notification(stage, deploy)
+  end
+
+  def send_after_notifications(stage, deploy)
     if stage.send_email_notifications?
       DeployMailer.deploy_email(stage, deploy).deliver
     end
 
+    send_flowdock_notification(stage, deploy)
+  end
+
+  def send_flowdock_notification(stage, deploy)
     if stage.send_flowdock_notifications?
       FlowdockNotification.new(stage, deploy).deliver
     end
--- app/models/flowdock_notification.rb
+++ app/models/flowdock_notification.rb
@@ -3,10 +3,12 @@ require 'flowdock'
 class FlowdockNotification
   def initialize(stage, deploy)
     @stage, @deploy = stage, deploy
+    @project = @stage.project
   end
 
   def deliver
-    flow.push_to_team_inbox(subject: @deploy.summary, content: @deploy.summary)
+    subject = "[#{@project.name}] #{@deploy.summary}"
+    flow.push_to_team_inbox(subject: subject, content: @deploy.summary)
   rescue Flowdock::Flow::ApiError
     # Invalid token or something.
   end
