48179039dcce3b09074b483e93302ec3bafcd907
Steven Davidovitz <sdavidovitz@zendesk.com>
2014-01-13 20:15:15 +1100

    up the db pool

--- config/initializers/database.rb
+++ config/initializers/database.rb
@@ -3,8 +3,8 @@ Rails.application.config.after_initialize do
 
   ActiveSupport.on_load(:active_record) do
     config = Rails.application.config.database_configuration[Rails.env]
-    config['reaping_frequency'] = ENV['DB_REAP_FREQ'] || 10 # seconds
-    config['pool']              = ENV['DB_POOL']      || 5
+    config['reaping_frequency'] = ENV['DB_REAP_FREQ'] || 5 # seconds
+    config['pool']              = ENV['DB_POOL']      || 100
     ActiveRecord::Base.establish_connection(config)
   end
 end
