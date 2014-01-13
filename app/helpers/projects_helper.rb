56f497e912b31b93fa9bf54b2aca7c73f61d5b94
Steven Davidovitz <sdavidovitz@zendesk.com>
2014-01-13 11:11:03 +1100

    use rails syntax for project form

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -13,14 +13,6 @@ module ProjectsHelper
     end
   end
 
-  def project_form_method
-    if project.new_record?
-      :post
-    else
-      :put
-    end
-  end
-
   def project_form_legend
     if project.new_record?
       "New Project"
