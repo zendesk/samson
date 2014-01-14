0d9da5827f384cd40eebcb6413cc7db64cf6d08d
Daniel Schierbeck <dasch@zendesk.com>
2014-01-14 00:33:42 +0000

    Document OutputBuffer

--- app/models/output_buffer.rb
+++ app/models/output_buffer.rb
@@ -1,5 +1,25 @@
 require 'thread_safe'
 
+# Allows fanning out a stream to multiple listening threads. Each thread will
+# have to call `#each`, and will receive each chunk of data that is written
+# to the buffer. If a thread starts listening after the buffer has been written
+# to, it will receive the previous chunks immediately and then start streaming
+# new chunks.
+#
+# Example:
+#
+#   buffer = OutputBuffer.new
+#
+#   listener1 = Thread.new { c = ""; buffer.each {|chunk| c << chunk }; c }
+#   listener2 = Thread.new { c = ""; buffer.each {|chunk| c << chunk }; c }
+#
+#   buffer.write("hello ")
+#   buffer.write("world!")
+#   buffer.close
+#
+#   listener1.value #=> "hello world!"
+#   listener2.value #=> "hello world!"
+#
 class OutputBuffer
   CLOSE = Object.new
 
