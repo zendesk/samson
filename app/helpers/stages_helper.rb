ee27cad3789303cd848fb2ff5ae563059a67cf79
Daniel Schierbeck <dasch@zendesk.com>
2014-01-14 04:30:48 +0000

    Display the entire command text in the stage editor

--- app/helpers/stages_helper.rb
+++
@@ -1,9 +0,0 @@
-module StagesHelper
-  def command_check_boxes(form)
-    form.collection_check_boxes(:command_ids, @stage.all_commands, :id, :command) do |b|
-      content_tag(:div, data: { id: b.value }, class: 'checkbox') do
-        b.label { b.check_box + content_tag(:pre, b.text.truncate(30)) }
-      end
-    end
-  end
-end
