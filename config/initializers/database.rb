043d160042a9b80eb55d12b55aca2f7bddf541cf
Steven Davidovitz <sdavidovitz@zendesk.com>
2014-01-14 13:15:35 +1100

    make sure reaping_frequency, pool are integers

--- config/initializers/database.rb
+++ config/initializers/database.rb
@@ -3,8 +3,8 @@ Rails.application.config.after_initialize do
 
   ActiveSupport.on_load(:active_record) do
     config = Rails.application.config.database_configuration[Rails.env]
-    config['reaping_frequency'] = ENV['DB_REAP_FREQ'] || 5 # seconds
-    config['pool']              = ENV['DB_POOL']      || 100
+    config['reaping_frequency'] = ENV['DB_REAP_FREQ'].to_i || 5 # seconds
+    config['pool']              = ENV['DB_POOL'].to_i      || 100
     ActiveRecord::Base.establish_connection(config)
   end
 end
