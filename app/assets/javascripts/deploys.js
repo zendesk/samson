95a386784da50397e44117d7d33edeccff605e59
Daniel Schierbeck <dasch@zendesk.com>
2014-01-15 04:53:07 +0000

    Allow toggling an inline patch view in the file list

--- app/assets/javascripts/deploys.js
+++ app/assets/javascripts/deploys.js
@@ -1,4 +1,13 @@
-$('#deploy-tabs a').click(function (e) {
-    e.preventDefault()
-    $(this).tab('show')
-})
+$(function () {
+  $('#deploy-tabs a').click(function (e) {
+      e.preventDefault()
+      $(this).tab('show')
+  });
+
+  $('.file-summary').click(function (e) {
+    var row = $(this);
+    var patch = row.next();
+
+    patch.toggle();
+  });
+});
