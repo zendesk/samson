3afeeeefbb529a335b8d14f46e9a1f08f105bf57
Daniel Schierbeck <dasch@zendesk.com>
2014-01-15 00:53:35 +0000

    Show changesets on deploy pages

---
+++ app/helpers/deploys_helper.rb
@@ -0,0 +1,17 @@
+module DeploysHelper
+  def file_status_label(status)
+    mapping = {
+      "added"    => "success",
+      "modified" => "info",
+      "removed"  => "danger"
+    }
+
+    type = mapping[status]
+
+    content_tag :span, status[0].upcase, class: "label label-#{type}"
+  end
+
+  def file_changes_label(count, type)
+    content_tag :span, count.to_s, class: "label label-#{type}" unless count.zero?
+  end
+end
