49745e9257e07119c2cf90e8d9b9e707f224190c
Po Chen <pchen@zendesk.com>
2014-01-17 12:02:46 +1100

    Add test for users controller

--- test/controllers/admin/users_controller_test.rb
+++ test/controllers/admin/users_controller_test.rb
@@ -1,14 +1,53 @@
 require 'test_helper'
 
-class Admin::UsersControllerTest < ActionController::TestCase
+describe Admin::UsersController do
   describe 'a GET to #show' do
     as_a_admin do
       setup do
-        get '/admin/users'
+        get :show
       end
 
       it 'succeeds' do
         assert_template :show
+      end
+    end
+
+    as_a_deployer do
+      setup do
+        get :show
+      end
+
+      it 'redirects to home page' do
+        assert_redirected_to root_path
+      end
+    end
+  end
+
+  describe 'a DELETE to #destroy' do
+
+    let(:user) { users(:viewer) }
+
+    as_a_admin do
+      setup do
+        delete :destroy, id: user.id
+      end
+
+      it 'soft delete the user' do
+        user.reload.deleted_at.wont_be_nil
+      end
+
+      it 'redirects to admin users page' do
+        assert_redirected_to admin_users_path
+      end
+    end
+
+    as_a_deployer do
+      setup do
+        delete :destroy, id: user.id
+      end
+
+      it 'redirects to home page' do
+        assert_redirected_to root_path
       end
     end
   end
