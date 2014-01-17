411e64a2db1366e6f13a48ec353b7abf085b876d
Po Chen <pchen@zendesk.com>
2014-01-17 11:18:44 +1100

    Start writing the test

--- test/controllers/admin/users_controller_test.rb
+++ test/controllers/admin/users_controller_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class Admin::UsersControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  describe 'a GET to #show' do
+    as_a_admin do
+      setup do
+        get '/admin/users'
+      end
+
+      it 'succeeds' do
+        assert_template :show
+      end
+    end
+  end
 end
