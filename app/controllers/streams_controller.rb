8ce77d22e6cc532de3da9d960eef623f2d8018c3
Steven Davidovitz <sdavidovitz@zendesk.com>
2014-02-14 11:55:27 -0800

    split out event handling in StreamsController

--- app/controllers/streams_controller.rb
+++ app/controllers/streams_controller.rb
@@ -8,38 +8,46 @@ class StreamsController < ApplicationController
     response.headers['Content-Type'] = 'text/event-stream'
     response.headers['Cache-Control'] = 'no-cache'
 
-    job = Job.find(params[:deploy_id])
-    execution = JobExecution.find_by_id(job.id)
+    @job = Job.find(params[:deploy_id])
+    @execution = JobExecution.find_by_id(@job.id)
 
-    streamer = EventStreamer.new(response.stream) do |event, data|
-      case event
-      when :viewers
-        viewers = data.uniq.reject {|user| user == current_user}
-        viewers.to_json(only: [:id, :name])
-      when :finished
-        execution.viewers.delete(current_user) if execution
+    streamer = EventStreamer.new(response.stream, &method(:event_handler))
+    puts response.stream.inspect
 
-        ActiveRecord::Base.connection_pool.with_connection do |connection|
-          connection.verify!
+    puts @job.inspect
+    puts @execution
 
-          job.reload
-
-          @project = job.project
-          @deploy = job.deploy
-        end
-
-        JSON.dump(html: render_to_string(partial: 'deploys/header', formats: :html))
-      else
-        JSON.dump(msg: render_log(data))
-      end
-    end
-
-    if job.active? && execution
-      execution.viewers.push(current_user)
+    if @job.active? && @execution
+      @execution.viewers.push(current_user)
       ActiveRecord::Base.clear_active_connections!
-      streamer.start(execution.output)
+      streamer.start(@execution.output)
     else
       streamer.finished
+    end
+  end
+
+  private
+
+  def event_handler(event, data)
+    case event
+    when :viewers
+      viewers = data.uniq.reject {|user| user == current_user}
+      viewers.to_json(only: [:id, :name])
+    when :finished
+      @execution.viewers.delete(current_user) if @execution
+
+      ActiveRecord::Base.connection_pool.with_connection do |connection|
+        connection.verify!
+
+        @job.reload
+
+        @project = @job.project
+        @deploy = @job.deploy
+      end
+
+      JSON.dump(html: render_to_string(partial: 'deploys/header', formats: :html))
+    else
+      JSON.dump(msg: render_log(data))
     end
   end
 end
