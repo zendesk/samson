910393dd18dcffad48d0f29f85d8ac50d6d838ae
Steven Davidovitz <sdavidovitz@zendesk.com>
2014-02-14 12:05:40 -0800

    clean up streams controller a little more

--- app/controllers/streams_controller.rb
+++ app/controllers/streams_controller.rb
@@ -12,18 +12,12 @@ class StreamsController < ApplicationController
     @execution = JobExecution.find_by_id(@job.id)
 
     streamer = EventStreamer.new(response.stream, &method(:event_handler))
-    puts response.stream.inspect
 
-    puts @job.inspect
-    puts @execution
+    return streamer.finished unless @job.active? && @execution
 
-    if @job.active? && @execution
-      @execution.viewers.push(current_user)
-      ActiveRecord::Base.clear_active_connections!
-      streamer.start(@execution.output)
-    else
-      streamer.finished
-    end
+    @execution.viewers.push(current_user)
+    ActiveRecord::Base.clear_active_connections!
+    streamer.start(@execution.output)
   end
 
   private
@@ -34,20 +28,24 @@ class StreamsController < ApplicationController
       viewers = data.uniq.reject {|user| user == current_user}
       viewers.to_json(only: [:id, :name])
     when :finished
-      @execution.viewers.delete(current_user) if @execution
-
-      ActiveRecord::Base.connection_pool.with_connection do |connection|
-        connection.verify!
-
-        @job.reload
-
-        @project = @job.project
-        @deploy = @job.deploy
-      end
-
-      JSON.dump(html: render_to_string(partial: 'deploys/header', formats: :html))
+      finished_response
     else
       JSON.dump(msg: render_log(data))
     end
+  end
+
+  def finished_response
+    @execution.viewers.delete(current_user) if @execution
+
+    ActiveRecord::Base.connection_pool.with_connection do |connection|
+      connection.verify!
+
+      @job.reload
+
+      @project = @job.project
+      @deploy = @job.deploy
+    end
+
+    JSON.dump(html: render_to_string(partial: 'deploys/header', formats: :html))
   end
 end
