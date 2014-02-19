751e2b3a5013c34e9f428f02e5c5d52027928eb6
Jason Smale <jsmale@zendesk.com>
2014-02-20 10:26:56 +1100

    clean up some silly tabs

--- app/assets/javascripts/login.js
+++ app/assets/javascripts/login.js
@@ -1,9 +1,9 @@
 $(function() {
 
   $('.no-github a').click(function(){
-  	$(this).parent().hide();
-  	$('.action.zendesk').css('display', 'block');
-  	return false;
+    $(this).parent().hide();
+    $('.action.zendesk').css('display', 'block');
+    return false;
   });
 
 });
