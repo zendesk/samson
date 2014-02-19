f9e4324a4db3d0036871613b3d273d34fa756ab2
Steven Davidovitz <sdavidovitz@zendesk.com>
2014-02-18 22:08:06 -0800

    fix Octokit configuration

--- config/initializers/octokit.rb
+++ config/initializers/octokit.rb
@@ -1,10 +1,10 @@
 require 'octokit'
 require 'faraday-http-cache'
 
-Octokit.middleware = Faraday::Builder.new do |builder|
+Octokit.middleware = Faraday::RackBuilder.new do |builder|
   builder.use Faraday::HttpCache, store: Rails.cache, serializer: Marshal
   builder.use Octokit::Response::RaiseError
   builder.adapter Faraday.default_adapter
-
-  builder.options[:open_timeout] = 2
 end
+
+Octokit.connection_options[:request] = { open_timeout: 2 }
