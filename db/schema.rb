4ff76c0bbe94a4e19f228833df52a4133c920d1b
Jason Smale <jsmale@zendesk.com>
2014-02-27 23:43:03 -0800

    Add schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140203155241) do
+ActiveRecord::Schema.define(version: 20140227211403) do
 
   create_table "commands", force: true do |t|
     t.text     "command",    limit: 16777215
@@ -93,6 +93,7 @@ ActiveRecord::Schema.define(version: 20140203155241) do
     t.datetime "deleted_at"
     t.boolean  "confirm",              default: true
     t.string   "datadog_tags"
+    t.boolean  "deployment",           default: true
   end
 
   create_table "users", force: true do |t|
