b3a039369e3686ba97d6872db5c6c5b87113ffb7
Steven Davidovitz <sdavidovitz@zendesk.com>
2014-03-07 16:56:24 -0800

    remove stream debugging

--- app/controllers/streams_controller.rb
+++ app/controllers/streams_controller.rb
@@ -15,7 +15,6 @@ class StreamsController < ApplicationController
 
     return response.stream.close unless @job.active? && @execution
 
-    Rails.logger.info("Opening stream for #{response.stream.object_id}")
     @execution.viewers.push(current_user)
     ActiveRecord::Base.clear_active_connections!
     streamer.start(@execution.output)
