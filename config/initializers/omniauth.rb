4656fe8d4885750074103d0974948d5f48cac979
Steven Davidovitz <sdavidovitz@zendesk.com>
2014-03-11 10:36:29 -0700

    use read:org permission instead of repo
    fixes #100

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -9,9 +9,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :github,
     ENV["GITHUB_CLIENT_ID"],
     ENV["GITHUB_SECRET"],
-    # need the repo spec for organizations teams
-    # unfortunately, they do not have a readonly version
-    scope: "user:email,repo"
+    scope: "user:email,read:org"
 
   provider OmniAuth::Strategies::ZendeskOAuth2,
     "deployment",
