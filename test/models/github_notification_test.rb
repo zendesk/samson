ab6cf7dcc15198c5982421c911918f64d5b468e9
Deepti Buchi <dbuchi@zendesk.com>
2014-03-17 14:54:07 -0700

    Add tests

---
+++ test/models/github_notification_test.rb
@@ -0,0 +1,34 @@
+require 'test_helper'
+
+describe GithubNotification do
+  let(:stage) { stub(name: "staging") }
+  let(:project) { stub(name: "Glitter", github_repo: "glitter") }
+  let(:changeset) { stub_everything(commits: [], files: [], pull_requests: [pull_requests]) }
+  let(:deploy) { stub(changeset: changeset, project: project, short_reference: "7e6c415") }
+  let(:notification) { GithubNotification.new(stage, deploy) }
+  let(:endpoint) { "https://api.github.com/repos/glitter/issues/9/comments" }
+
+  describe 'when there are pull requests' do
+
+    let(:pull_requests) { stub(number: 9)}
+
+    it "adds a comment" do
+      comment = stub_request(:post, endpoint).
+                  with(:body => "{\"body\":\"This PR was deployed to staging. Reference: 7e6c415\"}",
+                     :headers => {'Accept'=>'application/vnd.github.beta+json', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'Authorization'=>'token 5bc05403120c092c8e92773f977765cf1d2daec2', 'User-Agent'=>'Octokit Ruby Gem 2.7.0'}).
+                  to_return(:status => 201, :body => "", :headers => {})
+      notification.deliver
+
+      assert_requested comment
+    end
+  end
+
+  describe 'when there are no pull requests' do
+    let(:pull_requests) {}
+
+    it "doesn't add a comment" do
+      notification.deliver
+      assert_not_requested :post, endpoint
+    end
+  end
+end
