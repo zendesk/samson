3eab3e6cc8668a96be954a82d203bdbb204a2174
Po Chen <pchen@zendesk.com>
2014-03-24 16:52:27 +1100

    Bootstrap style dropdown/select menu

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,6 +19,7 @@
 //= require bootstrap/tab
 //= require bootstrap/tooltip
 //= require bootstrap/popover
+//= require bootstrap-select
 //= require jquery.ui.all
 //= require bootstrap3-editable/bootstrap-editable
 //= require rickshaw_with_d3
