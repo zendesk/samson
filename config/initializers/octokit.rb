49f3899e86eeb1dcba66a11c4b1d093b8dbeda49
Steven Davidovitz <sdavidovitz@zendesk.com>
2014-03-26 23:37:23 -0700

    remove faraday http cache, seems to cache 404s

--- config/initializers/octokit.rb
+++ config/initializers/octokit.rb
@@ -2,7 +2,6 @@ require 'octokit'
 require 'faraday-http-cache'
 
 Octokit.middleware = Faraday::RackBuilder.new do |builder|
-  builder.use Faraday::HttpCache, store: Rails.cache, serializer: Marshal
   builder.use Octokit::Response::RaiseError
   builder.adapter Faraday.default_adapter
 end
