3c4f558adec797b66bcde74f04105641908965a2
Ilkka Oksanen <ioksanen@zendesk.com>
2014-04-07 16:45:40 +0200

    Add more release info to releases page

--- app/assets/javascripts/releases.js
+++ app/assets/javascripts/releases.js
@@ -1,1 +1,19 @@
 //= require changesets
+
+$(function() {
+  var $previous_select = null;
+
+  $('.release-list .release-summary').click(function() {
+    var $this = $(this);
+
+    if ($previous_select && $this[0] !== $previous_select[0]) {
+      $previous_select.removeClass('active');
+      $previous_select.next().addClass('collapse');
+    }
+
+    $this.toggleClass('active');
+    $this.next().toggleClass('collapse');
+
+    $previous_select = $this;
+  });
+});
