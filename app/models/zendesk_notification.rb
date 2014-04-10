44969af687bae2401ee23c8f024b67eb627dd881
Deepti Buchi <dbuchi@zendesk.com>
2014-04-10 14:06:36 -0700

    Update zendesk ticket message

--- app/models/zendesk_notification.rb
+++ app/models/zendesk_notification.rb
@@ -3,7 +3,6 @@ require 'zendesk_api'
 class ZendeskNotification
   cattr_accessor(:token) { ENV['CLIENT_SECRET']}
   cattr_accessor(:zendesk_url) { ENV['ZENDESK_URL']}
-  cattr_accessor(:zendesk_user) { ENV['ZENDESK_USER']}
 
   def initialize(stage, deploy)
     @stage, @deploy = stage, deploy
@@ -13,17 +12,16 @@ class ZendeskNotification
     zendesk_tickets = @deploy.changeset.zendesk_tickets
 
     zendesk_tickets.each do |ticket_id|
-      ticket = zendesk_client.tickets.find(:id => ticket_id)
+      attributes = {
+        :id => ticket_id,
+        :status => "open",
+        :comment => {:value => content(ticket_id), :public => false }
+      }
 
-      # Update ticket just once with comment
-      if !is_comment_added?(ticket)
-        ticket.comment = {:value => body, :public => false }
-
-        if ticket.save
-          Rails.logger.info "Updated Zendesk ticket: #{ticket_id} with a comment"
-        else
-          Rails.logger.warn("Failed to modify ticket with GitHub update: #{ticket.errors}")
-        end
+      if zendesk_client.tickets.update(attributes)
+        Rails.logger.info "Updated Zendesk ticket: #{ticket_id} with a comment"
+      else
+        Rails.logger.warn("Failed to modify ticket with GitHub update: #{ticket.errors}")
       end
     end
   end
@@ -34,15 +32,11 @@ class ZendeskNotification
     @zendesk_client ||= ZendeskAPI::Client.new do |config|
       config.token = token
       config.url = "#{zendesk_url}/api/v2"
-      config.username = zendesk_user
+      config.username = "#{@deploy.user.email}"
     end
   end
 
-  def body
-    "A fix for this issue has been deployed to #{@stage.name}"
-  end
-
-  def is_comment_added?(ticket)
-    ticket.comments.map(&:body).include?(body)
+  def content(ticket_id)
+    @content = ZendeskNotificationRenderer.render(@deploy, ticket_id)
   end
 end
--- test/models/zendesk_notification_test.rb
+++ test/models/zendesk_notification_test.rb
@@ -2,36 +2,24 @@ require 'test_helper'
 
 describe ZendeskNotification do
   let(:stage) { stub(name: "Production")}
-  let(:changeset) { stub_everything(commits: ["ZD#18 this fixes a very bad bug"], zendesk_tickets: [18]) }
-  let(:deploy) { stub(changeset: changeset) }
+  let(:user) { stub(email: "deploys@example.org")}
+  let(:changeset) { stub(commits: "ZD#18 this fixes a very bad bug", zendesk_tickets: [18]) }
+  let(:deploy) { stub(changeset: changeset, user: user) }
   let(:notification) { ZendeskNotification.new(stage, deploy) }
   let(:api_response_headers) { {:headers => {:content_type => "application/json"}} }
 
   describe 'when commit messages include Zendesk tickets' do
-    it "comments on the ticket when comment is not already added" do
-      ZendeskNotification.any_instance.stubs(:is_comment_added?).returns(false)
-      ticket = stub_api_request(:get, "api/v2/tickets/18")
-
-      comment = stub_api_request(:put, "api/v2/tickets/18").
-                  with(:body => "{\"ticket\":{\"comment\":{\"value\":\"A fix for this issue has been deployed to Production\",\"public\":false}}}")
-
-      notification.deliver
-
-      assert_requested ticket
-      assert_requested comment
+    before do
+      ZendeskNotificationRenderer.stubs(:render).returns("A fix to project has been deployed to Production. Deploy details: v2.14")
     end
 
-    it "does not update the ticket when comment is not already added" do
-      ZendeskNotification.any_instance.stubs(:is_comment_added?).returns(true)
-      ticket = stub_api_request(:get, "api/v2/tickets/18")
-
-      comment = stub_api_request(:get, "api/v2/tickets/18").
-                  with(:body => "{\"ticket\":{\"comment\":{\"value\":\"A fix for this issue has been deployed to Production\",\"public\":false}}}")
+    it "comments on the ticket" do
+      comment = stub_api_request(:put, "api/v2/tickets/18").
+                  with(:body => "{\"ticket\":{\"status\":\"open\",\"comment\":{\"value\":\"A fix to project has been deployed to Production. Deploy details: v2.14\",\"public\":false}}}")
 
       notification.deliver
 
-      assert_requested ticket
-      assert_not_requested comment
+      assert_requested comment
     end
   end
 
@@ -39,9 +27,9 @@ describe ZendeskNotification do
 
   def stub_api_request(method, path)
     token = ENV['CLIENT_SECRET']
-    user = ENV['ZENDESK_USER']
     url = ENV['ZENDESK_URL'].split('//')[1]
-    body = '{ "ticket": { "id": 18, "comment": {"value": "Comment body", "public": true }}}'
+    user = "#{deploy.user.email}"
+    body = '{ "ticket": { "id": 18, "comment": {"value": "Comment body", "public": true }, "status": "open"}}'
 
     stub_request(method, "https://#{user}%2Ftoken:#{token}@#{url}/#{path}").
       with(:headers => {'Accept'=>'application/json', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'User-Agent'=>'ZendeskAPI API 1.3.4'}).
