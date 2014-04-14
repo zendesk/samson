497ebacf74db135db1de4262aa92497cf8e94742
Jason Smale <jsmale@zendesk.com>
2014-04-15 00:05:01 +1000

    Remove email address validation on user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,6 @@ class User < ActiveRecord::Base
   has_many :starred_projects, through: :stars, source: :project
 
   validates :role_id, inclusion: { in: Role.all.map(&:id) }
-  validates :email, presence: true
 
   before_create :set_token
 
