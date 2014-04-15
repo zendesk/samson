a8e9d017dcde5d489abf6decd4e7fff638e3e746
Steven Davidovitz <sdavidovitz@zendesk.com>
2014-04-14 18:19:49 -0700

    Revert "Don't allow editing of user profile, only display API token"
    This reverts commit e57a2af3aa2afae6f88e7e7783f5ee902eda60ac.

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,7 +1,25 @@
 class ProfilesController < ApplicationController
 
+  def update
+    @user = current_user
+
+    if @user.update_attributes(user_params)
+      redirect_to profile_path, notice: 'Your profile has been updated.'
+    else
+      render action: "show"
+    end
+  end
+
   def show
     @user = current_user
+  end
+
+  protected
+
+  def user_params
+    params.require(:user).permit(
+      :name
+    )
   end
 
 end
