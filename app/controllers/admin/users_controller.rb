9e795ef333325780e5b8da0e70173f412de2e4f8
Jason Smale <jsmale@zendesk.com>
2014-04-22 18:32:24 +1000

    Removed extra space

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -42,6 +42,6 @@ class Admin::UsersController < ApplicationController
   end
 
   def sort_direction
-    %w[asc desc].include?(params[:direction]) ?  params[:direction] : "asc"
+    %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
   end
 end
