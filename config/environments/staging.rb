e49264249c6592ca896a4fff803912b233895ee8
Ilkka Oksanen <ioksanen@zendesk.com>
2014-05-06 10:19:34 +0200

    Remove Zendesk prefix from staging config file

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -1,4 +1,4 @@
-ZendeskSamson::Application.configure do
+Samson::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
