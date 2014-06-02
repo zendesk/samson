978fd9b9b23dc2f4ddb5fa2e25e143bbaf800b85
Jason Smale <jsmale@zendesk.com>
2014-06-03 08:30:30 +1000

    Remove Zendesk authentication

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,7 +2,6 @@ require 'omniauth'
 
 OmniAuth.config.logger = Rails.logger
 
-require 'omniauth/strategies/zendesk_oauth2'
 require 'omniauth-github'
 require 'omniauth-google-oauth2'
 
@@ -11,11 +10,6 @@ Rails.application.config.middleware.use OmniAuth::Builder do
     ENV["GITHUB_CLIENT_ID"],
     ENV["GITHUB_SECRET"],
     scope: "user:email,read:org"
-
-  provider OmniAuth::Strategies::ZendeskOAuth2,
-    "deployment",
-    ENV["CLIENT_SECRET"],
-    scope: "users:read"
 
   provider OmniAuth::Strategies::GoogleOauth2,
     ENV["GOOGLE_CLIENT_ID"],
