4f5e87aa3e2bb4f96dbb69133aefad5eb0678097
Po Chen <pchen@zendesk.com>
2014-06-04 16:49:54 +1000

    Make ajax easier in Angular

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -3,3 +3,7 @@ var samson = angular.module("samson", []);
 var A = angular;
 
 A.$ = A.element;
+
+samson.config(["$httpProvider", function($httpProvider) {
+  $httpProvider.defaults.headers.common['X-CSRF-Token'] = $('meta[name=csrf-token]').attr('content');
+}]);
