3af8cb714a08b90afb22194bae0f4dc1fae2e852
Po Chen <pchen@zendesk.com>
2014-06-04 16:55:34 +1000

    Frontend implementation

--- app/assets/javascripts/admin/users.js
+++ app/assets/javascripts/admin/users.js
@@ -1,2 +1,30 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
+samson.controller("UsersCtrl",
+["$scope", "$http", "$timeout",
+function($scope, $http, $timeout) {
+  $scope.updateUser = function($event) {
+    var userId = angular.element($event.target).closest("tr").data("id"),
+        roleId;
+
+    if ($event.target.type === "radio") {
+      roleId = $event.target.value;
+
+      $http.put("/admin/users/" + userId, { role_id: roleId })
+        .success(function() {
+          $timeout.cancel($scope.popupCancellation);
+          $scope.saveFailure = false;
+          $scope.saveSuccess = true;
+          $scope.popupCancellation = $timeout(function() {
+            $scope.saveSuccess = null;
+          }, 1500);
+        })
+        .error(function() {
+          $timeout.cancel($scope.popupCancellation);
+          $scope.saveSuccess = false;
+          $scope.saveFailure = true;
+          $scope.popupCancellation = $timeout(function() {
+            $scope.saveFailure = null;
+          }, 1500);
+        });
+    }
+  };
+}]);
