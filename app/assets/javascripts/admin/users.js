42cd8983cbb2efcc99e5e0951662379948536a32
Po Chen <pchen@zendesk.com>
2014-06-05 13:01:52 +1000

    Style improvements

--- app/assets/javascripts/admin/users.js
+++ app/assets/javascripts/admin/users.js
@@ -1,8 +1,13 @@
 samson.controller("UsersCtrl",
 ["$scope", "$http", "$timeout",
 function($scope, $http, $timeout) {
+  var SUCCESS = "<div class=\"save-success\">Saved</div>",
+      FAILURE = "<div class=\"save-failure\">Failed</div>";
+
   $scope.updateUser = function($event) {
-    var userId = A.$($event.target).closest("tr").data("id"),
+    var roleTd  = A.$($event.target).closest(".role"),
+        wrapper = roleTd.find(".relative-wrapper"),
+        userId  = roleTd.data("user-id"),
         roleId;
 
     if ($event.target.type === "radio") {
@@ -10,20 +15,14 @@ function($scope, $http, $timeout) {
 
       $http.put("/admin/users/" + userId, { role_id: roleId })
         .success(function() {
-          $timeout.cancel($scope.popupCancellation);
-          $scope.saveFailure = false;
-          $scope.saveSuccess = true;
-          $scope.popupCancellation = $timeout(function() {
-            $scope.saveSuccess = null;
-          }, 1500);
+          $(SUCCESS).appendTo(wrapper).delay(1500).fadeOut(500, function() {
+            this.remove();
+          });
         })
         .error(function() {
-          $timeout.cancel($scope.popupCancellation);
-          $scope.saveSuccess = false;
-          $scope.saveFailure = true;
-          $scope.popupCancellation = $timeout(function() {
-            $scope.saveFailure = null;
-          }, 1500);
+          $(FAILURE).appendTo(wrapper).delay(1500).fadeOut(500, function() {
+            this.remove();
+          });
         });
     }
   };
