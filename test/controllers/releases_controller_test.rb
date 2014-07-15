1e7d8e7ce3334554e58fe7badfa8928981ce9829
Jason Smale <jsmale@zendesk.com>
2014-07-15 15:52:08 +1000

    Merge pull request #45 from zendesk/jsmale/releases_as_viewer_fix
    Stop viewers being able to create releases

--- test/controllers/releases_controller_test.rb
+++ test/controllers/releases_controller_test.rb
@@ -1,16 +1,23 @@
 require_relative '../test_helper'
 
 describe ReleasesController do
+  let(:project) { projects(:test) }
+
   describe "#create" do
+    let(:release_params) { { commit: "abcd" } }
+
+    as_a_viewer do
+      it "doesn't creates a new release" do
+        count = Release.count
+        post :create, project_id: project.id, release: release_params
+        assert_equal count, Release.count
+      end
+    end
+
     as_a_deployer do
       it "creates a new release" do
-        project = projects(:test)
-        release_params = { commit: "abcd" }
-
         count = Release.count
-
         post :create, project_id: project.id, release: release_params
-
         assert_equal count + 1, Release.count
       end
     end
