d682bf9529b75693e98246882ad79f21e5977a9c
bolddane <pobrien@zendesk.com>
2014-07-17 22:03:51 -0700

    Return dropped column back to db

--- db/migrate/20140717013754_add_production_to_stages.rb
+++ db/migrate/20140717013754_add_production_to_stages.rb
@@ -1,5 +1,6 @@
 class AddProductionToStages < ActiveRecord::Migration
   def change
+    add_column :stages, :comment_on_zendesk_tickets, :boolean, default: false
     add_column :stages, :production, :boolean, default: false
   end
 end
