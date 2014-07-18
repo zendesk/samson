2aa367365656a3027fce9212bca5303472d2e022
bolddane <pobrien@zendesk.com>
2014-07-18 07:00:46 -0700

    Remove column added in d682bf9

--- db/migrate/20140717013754_add_production_to_stages.rb
+++ db/migrate/20140717013754_add_production_to_stages.rb
@@ -1,6 +1,5 @@
 class AddProductionToStages < ActiveRecord::Migration
   def change
-    add_column :stages, :comment_on_zendesk_tickets, :boolean, default: false
     add_column :stages, :production, :boolean, default: false
   end
 end
