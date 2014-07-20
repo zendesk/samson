4fa04ce2489718f2e4ee8a8e66b8989fb910a1fa
bolddane <pobrien@zendesk.com>
2014-07-20 15:50:07 -0700

    Refactor all DeployMailer test code into one test file

--- test/mailers/deploy_mailer_test.rb
+++ test/mailers/deploy_mailer_test.rb
@@ -1,35 +1,95 @@
 require_relative '../test_helper'
 
 describe DeployMailer do
-  let(:stage) { stages(:test_staging) }
-  let(:deploy) { Deploy.create!(stage: stage, job: job, reference: 'master') }
 
-  let(:job) do
-    Job.create!(command: 'true', project: projects(:test), user: users(:admin))
+  describe "deploy email" do
+    let(:stage) { stages(:test_staging) }
+    let(:deploy) { Deploy.create!(stage: stage, job: job, reference: 'master') }
+
+    let(:job) do
+      Job.create!(command: 'true', project: projects(:test), user: users(:admin))
+    end
+
+    before do
+      stage.update_attributes!(notify_email_address: 'test@test.com')
+
+      changeset = stub_everything(files: [], commits: [], pull_requests: [])
+      Changeset.stubs(:find).returns(changeset)
+
+      DeployMailer.deploy_email(stage, deploy).deliver
+    end
+
+    subject do
+      ActionMailer::Base.deliveries.first
+    end
+
+    it 'is from deploys@' do
+      subject.from.must_equal(['deploys@samson-deployment.com'])
+    end
+
+    it 'sends to notify_email_address' do
+      subject.to.must_equal(['test@test.com'])
+    end
+
+    it 'sets a subject' do
+      subject.subject.wont_be_empty
+    end
   end
 
-  before do
-    stage.update_attributes!(notify_email_address: 'test@test.com')
+  describe "bypass email" do
+    let(:project) { projects(:test) }
+    let(:user) { users(:deployer) }
+    let(:service) { DeployService.new(project, user) }
+    let(:stage) { stages(:test_staging) }
+    let(:reference) { 'master' }
+    let(:job) {Job.create!(command: 'true', project: project, user: user) }
+    let(:deploy) { Deploy.create!(stage: stage, job: job, reference: reference) }
 
-    changeset = stub_everything(files: [], commits: [], pull_requests: [])
-    Changeset.stubs(:find).returns(changeset)
+    let(:buddy_is_deployer) { user }
+    let(:buddy_is_other) { users(:deployer_buddy) }
 
-    DeployMailer.deploy_email(stage, deploy).deliver
+    before do
+      BuddyCheck.stubs(:bypass_email_address).returns("test1@test.com")
+
+      changeset = stub_everything(files: [], commits: [], pull_requests: [])
+      Changeset.stubs(:find).returns(changeset)
+    end
+
+    describe "bypass deploy" do
+      before do
+        service.confirm_deploy!(deploy, stage, reference, buddy_is_deployer)
+      end
+
+      subject do
+        ActionMailer::Base.deliveries.first
+      end
+
+      it 'is from deploys@' do
+        subject.from.must_equal(['deploys@samson-deployment.com'])
+      end
+
+      it 'sends to bypass_email_address' do
+        subject.to.must_equal(['test1@test.com'])
+      end
+
+      it 'sets a bypass subject' do
+        subject.subject.must_match /BYPASS/
+      end
+    end
+
+    describe "non-bypass deploy" do
+      before do
+        service.confirm_deploy!(deploy, stage, reference, buddy_is_other)
+      end
+
+      subject do
+        ActionMailer::Base.deliveries.first
+      end
+
+      it 'does not generate a bypass mail' do
+        subject.must_be_nil
+      end
+    end
   end
 
-  subject do
-    ActionMailer::Base.deliveries.first
-  end
-
-  it 'is from deploys@' do
-    subject.from.must_equal(['deploys@samson-deployment.com'])
-  end
-
-  it 'sends to notify_email_address' do
-    subject.to.must_equal(['test@test.com'])
-  end
-
-  it 'sets a subject' do
-    subject.subject.wont_be_empty
-  end
 end
