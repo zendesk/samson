9b5c77e10d00958a93cd853d359af23da0a08ff9
bolddane <pobrien@zendesk.com>
2014-07-20 17:07:31 -0700

    Added flowdock notification and job execution test stubs

--- test/mailers/deploy_mailer_test.rb
+++ test/mailers/deploy_mailer_test.rb
@@ -44,20 +44,27 @@ describe DeployMailer do
     let(:reference) { 'master' }
     let(:job) {Job.create!(command: 'true', project: project, user: user) }
     let(:deploy) { Deploy.create!(stage: stage, job: job, reference: reference) }
+    let(:job_execution) { JobExecution.new(reference, job) }
 
     let(:buddy_is_deployer) { user }
     let(:buddy_is_other) { users(:deployer_buddy) }
 
     before do
+      job_execution.stubs(:execute!)
+      JobExecution.stubs(:start_job).with(reference, deploy.job).returns(job_execution)
+
       BuddyCheck.stubs(:bypass_email_address).returns("test1@test.com")
 
       changeset = stub_everything(files: [], commits: [], pull_requests: [])
       Changeset.stubs(:find).returns(changeset)
+
+      service.stubs(:send_flowdock_notification)
     end
 
     describe "bypass deploy" do
       before do
         service.confirm_deploy!(deploy, stage, reference, buddy_is_deployer)
+        job_execution.run!
       end
 
       subject do
@@ -80,6 +87,7 @@ describe DeployMailer do
     describe "non-bypass deploy" do
       before do
         service.confirm_deploy!(deploy, stage, reference, buddy_is_other)
+        job_execution.run!
       end
 
       subject do
