9949da275e88f4786f345c94a4b603c0c8d42613
bolddane <pobrien@zendesk.com>
2014-07-27 11:00:01 -0700

    Should have been removed in 9163d4f

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,9 +12,9 @@ class User < ActiveRecord::Base
 
   before_create :set_token
 
-  def self.name_for_id(user_id)
-    User.find(user_id).name if user_id
-  end
+  #def self.name_for_id(user_id)
+  #  User.find(user_id).name if user_id
+  #end
 
   def starred_project?(project)
     starred_projects.include?(project)
