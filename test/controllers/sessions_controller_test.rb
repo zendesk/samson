daafdf868010c887ede22b5180036c4921af7540
Praveen Swadi <pswadi@zendesk.com>
2014-08-26 18:24:00 -0700

    Lookup email restriction domain from env using a function include tests for email restriction case

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -1,4 +1,5 @@
 require_relative '../test_helper'
+require 'byebug'
 
 describe SessionsController do
   describe "a GET to #new" do
@@ -40,6 +41,7 @@ describe SessionsController do
 
     setup do
       @controller.stubs(github_authorization: stub(role_id: Role::VIEWER.id))
+      @controller.stubs(:restricted_email_domain).returns(nil) 
 
       @request.env.merge!(env)
       @request.env.merge!('omniauth.auth' => auth_hash)
@@ -63,6 +65,45 @@ describe SessionsController do
         assert_redirected_to '/hello'
       end
     end
+  end
+
+  describe "a POST to #github with email restriction" do
+    let(:env) {{}}
+    let(:user) { users(:github_viewer) }
+    let(:strategy) { stub(name: 'github') }
+    let(:auth_hash) do
+      Hashie::Mash.new(
+        uid: '3',
+        info: Hashie::Mash.new(
+          name: user.name,
+          email: user.email
+        )
+      )
+    end
+
+    setup do
+      @controller.stubs(github_authorization: stub(role_id: Role::VIEWER.id))
+      @controller.stubs(:restricted_email_domain).returns("@uniqlo.com") 
+
+      @request.env.merge!(env)
+      @request.env.merge!('omniauth.auth' => auth_hash)
+      @request.env.merge!('omniauth.strategy' => strategy)
+
+      post :github
+    end
+
+    it 'does not log the user in' do
+      @controller.current_user.must_be_nil
+    end
+
+    it "renders" do
+      assert_template :new
+    end
+
+    it "sets a flash error" do
+      request.flash[:error].wont_be_nil
+    end
+
   end
 
   describe "a GET to #failure" do
