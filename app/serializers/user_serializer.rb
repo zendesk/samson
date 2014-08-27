3543a21c670a2cc538a4b35589e5404372a9248c
Praveen Swadi <pswadi@zendesk.com>
2014-08-26 19:29:06 -0700

    Removed role from users.json

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,9 +1,6 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :name, :email, :role_id, :role, :gravatar_url
+  attributes :id, :name, :email, :role_id, :gravatar_url
 
-  def role
-    Role.find(object.role_id).name
-  end
   def gravatar_url
     object.gravatar_url
   end
