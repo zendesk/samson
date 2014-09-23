a1e7dbc13035fad797bec9a55dd83012d3c5f7e7
grosser <michael@grosser.it>
2014-09-22 17:46:18 -0700

    fix migration blowing up on deleted projects

--- db/migrate/20140908154402_add_permalink_to_projects.rb
+++ db/migrate/20140908154402_add_permalink_to_projects.rb
@@ -5,9 +5,11 @@ class AddPermalinkToProjects < ActiveRecord::Migration
 
     Project.reset_column_information
 
-    Project.find_each do |project|
-      project.send(:generate_permalink)
-      project.update_column(:permalink, project.permalink)
+    Project.with_deleted do
+      Project.find_each do |project|
+        project.send(:generate_permalink)
+        project.update_column(:permalink, project.permalink)
+      end
     end
 
     change_column :projects, :permalink, :string, null: false
