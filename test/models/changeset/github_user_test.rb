d0672a39f544d4fad0de20a6eea2e689be5ebb01
Daniel Schierbeck <dasch@zendesk.com>
2014-09-24 10:29:03 +0000

    Correctly compute Github users' avatars

---
+++ test/models/changeset/github_user_test.rb
@@ -0,0 +1,12 @@
+require 'test_helper'
+
+describe Changeset::GithubUser do
+  describe "#avatar_url" do
+    it "returns the URL for the user's avatar" do
+      data = stub("data", avatar_url: "https://avatars.githubusercontent.com/u/1337?v=2")
+
+      user = Changeset::GithubUser.new(data)
+      user.avatar_url.must_equal "https://avatars.githubusercontent.com/u/1337?v=2&s=20"
+    end
+  end
+end
