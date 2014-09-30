523afde501675f4c642596650caa53193b054f3f
Shane Hender <shender@zendesk.com>
2014-09-30 15:03:41 +0100

    Added a new SSE msg to send to viewers at start of deploy to update buddy text

--- app/controllers/streams_controller.rb
+++ app/controllers/streams_controller.rb
@@ -26,6 +26,8 @@ class StreamsController < ApplicationController
 
   def event_handler(event, data)
     case event
+    when :started
+      started_response
     when :viewers
       viewers = data.uniq.reject {|user| user == current_user}
       viewers.to_json(only: [:id, :name])
@@ -36,6 +38,29 @@ class StreamsController < ApplicationController
     end
   end
 
+  # Primarily used for updating the originating requestor's browser page when a buddy
+  # approved their deploy.
+  def started_response
+    ActiveRecord::Base.connection.verify!
+    @job.reload
+    @project = @job.project
+    @deploy = @job.deploy
+
+    Rails.logger.debug("started_response(#{current_user.id}): deploy status = " + @deploy.summary)
+
+    if @deploy
+      JSON.dump(
+          title: deploy_page_title,
+          html: render_to_body(partial: 'deploys/header', formats: :html)
+      )
+    else
+      JSON.dump(
+          title: job_page_title,
+          html: render_to_body(partial: 'jobs/header', formats: :html)
+      )
+    end
+  end
+
   def finished_response
     @execution.viewers.delete(current_user) if @execution
 
@@ -43,6 +68,7 @@ class StreamsController < ApplicationController
 
     @project = @job.project
     @deploy = @job.deploy
+    Rails.logger.debug("#{current_user.id}: finished_response: deploy status = " + @deploy.summary)
 
     if @deploy
       JSON.dump(
