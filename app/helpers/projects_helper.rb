eaff60dd1ba8c13736dd7b968b1d7b710739768b
Steven Davidovitz <sdavidovitz@zendesk.com>
2014-10-08 17:31:55 -0700

    show a warning when github is down

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,4 +1,6 @@
 module ProjectsHelper
+  cattr_accessor(:github_status_cache_key, instance_writer: false) { 'github-status-ok' }
+
   def star_for_project(project)
     content_tag :span, class: "star" do
       if current_user.starred_project?(project)
@@ -27,5 +29,24 @@ module ProjectsHelper
     else
       "failed"
     end
+  end
+
+  def github_ok?
+    return true unless (status_url = Rails.application.config.samson.github.status_url)
+
+    github_ok = Rails.cache.read(github_status_cache_key)
+
+    if github_ok.nil?
+      response = Faraday.get('https://' + status_url + '/api/status.json')
+      github_ok = response.status == 200 && JSON.parse(response.body)['status'] == 'good'
+
+      if github_ok
+        Rails.cache.write(github_status_cache_key, github_ok, expires_in: 5.minutes)
+      end
+    end
+
+    github_ok
+  rescue Faraday::ClientError
+    false
   end
 end
