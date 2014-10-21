f7129601b2bc1ff0b3b596592944f120de007602
grosser <michael@grosser.it>
2014-10-21 13:42:16 -0700

    use describe helpers

--- test/models/buddy_check_test.rb
+++ test/models/buddy_check_test.rb
@@ -1,6 +1,6 @@
 require_relative '../test_helper'
 
-class BuddyCheckDeployServiceTest < ActiveSupport::TestCase
+describe BuddyCheck, :model do
   let(:project) { job.project }
   let(:user) { job.user }
   let(:service) { DeployService.new(project, user) }
