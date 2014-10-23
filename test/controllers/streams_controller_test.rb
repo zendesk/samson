0aa967853e3c797088b89db0715c65646cb75533
Shane Hender <shender@zendesk.com>
2014-10-22 17:00:17 -0700

    Fixed flakey test problem.

--- test/controllers/streams_controller_test.rb
+++ test/controllers/streams_controller_test.rb
@@ -4,25 +4,23 @@ describe StreamsController do
   let(:project) { projects(:test) }
   let(:stage) { stages(:test_staging) }
   let(:deployer) { users(:deployer) }
-  let(:command) { "echo hello" }
-  let(:job) { Job.create!(command: command, project: project, user: deployer) }
-  let(:deploy) { Deploy.create!(stage: stage, job: job, reference: "foo") }
+  let(:job) { jobs(:succeeded_test) }
 
   as_a_deployer do
     describe "a GET to :show" do
       it "should have an initial :started SSE and a :finished SSE" do
         # Override the job retrieval in the streams controller. This way we don't have
         # to stub out all the rest of the JobExecution setup/execute/... flow.
-        @fake_execution = JobExecution.new("foo", job)
-        JobExecution.expects(:find_by_id).returns(@fake_execution)
+        fake_execution = JobExecution.new("foo", job)
+        JobExecution.expects(:find_by_id).returns(fake_execution)
 
         # Get the :show page to open the SSE stream
         get :show, id: job.id
 
         # Write some msgs to our fake TerminalExecutor stream
-        @fake_execution.output.write("Hello there!\n")
+        fake_execution.output.write("Hello there!\n")
         # Close the stream to denote the job finishing, which will trigger sending the :finished SSE
-        @fake_execution.output.close
+        fake_execution.output.close
         response.status.must_equal(200)
 
         # Collect the output from the ActiveController::Live::Buffer stream
@@ -30,10 +28,10 @@ describe StreamsController do
         response.stream.each { |l| lines << l }
 
         # Ensure we have at least the :started and :finished SSE msgs
-        assert lines.grep(/started\ndata:/)
-        assert lines.grep(/finished\ndata:/)
+        assert lines.grep(/event: started\ndata:/)
+        assert lines.grep(/event: append\ndata:.*Hello there!/)
+        assert lines.grep(/event: finished\ndata/)
       end
     end
   end
-
 end
