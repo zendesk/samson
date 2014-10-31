5cdf74b41f712d36b8d8d5c7cf826a63a177cafe
Steven Davidovitz <sdavidovitz@zendesk.com>
2014-10-31 11:07:21 -0700

    use integration support helper

--- test/controllers/integrations/github_controller_test.rb
+++ test/controllers/integrations/github_controller_test.rb
@@ -1,6 +1,8 @@
 require_relative '../../test_helper'
 
 describe Integrations::GithubController do
+  extend IntegrationsControllerTestHelper
+
   let(:commit) { "dc395381e650f3bac18457909880829fc20e34ba" }
   let(:project) { projects(:test) }
 
@@ -49,58 +51,6 @@ describe Integrations::GithubController do
       request.headers['X-Hub-Signature'] = "sha1=#{hmac}"
     end
 
-    it "triggers a deploy if there's a webhook mapping for the branch" do
-      post :create, payload.merge(token: project.token)
-
-      deploy = project.deploys.first
-      deploy.commit.must_equal commit
-    end
-
-    describe 'if there is no webhook mapping' do
-      let(:payload) do
-        {
-          ref: 'refs/heads/origin/foobar',
-          head: commit
-        }.with_indifferent_access
-      end
-
-      it "doesn't trigger a deploy" do
-        post :create, payload.merge(token: project.token)
-
-        project.deploys.must_equal []
-      end
-    end
-
-    it "deploys as the correct user" do
-      post :create, payload.merge(token: project.token)
-
-      user = project.deploys.first.user
-      user.name.must_equal user_name
-    end
-
-    it "creates the ci user if it does not exist" do
-      post :create, payload.merge(token: project.token)
-
-      User.find_by_name(user_name).wont_be_nil
-    end
-
-    it "responds with 200 OK if the request is valid" do
-      post :create, payload.merge(token: project.token)
-
-      response.status.must_equal 200
-    end
-
-    it "responds with 422 OK if deploy cannot be started" do
-      post :create, payload.merge(token: project.token)
-      post :create, payload.merge(token: project.token)
-
-      response.status.must_equal 422
-    end
-
-    it "responds with 404 Not Found if the token is invalid" do
-      post :create, payload.merge(token: "foobar")
-
-      response.status.must_equal 404
-    end
+    test_regular_commit "Github", no_mapping: { ref: 'refs/heads/origin/foobar' }, failed: false
   end
 end
