e02fb52f98d939aa137ef39fcf95a64960baed60
Jason Stillwell <jstillwell@zendesk.com>
2014-11-24 14:05:19 -0800

    validate that we don't create duplicate webhooks.

--- app/models/webhook.rb
+++ app/models/webhook.rb
@@ -1,5 +1,6 @@
 class Webhook < ActiveRecord::Base
   has_soft_deletion default_scope: true
+  validates :branch, uniqueness: { scope: [ :stage, :deleted_at ], message: "one webhook per (stage, branch) combination." }
 
   belongs_to :project
   belongs_to :stage
