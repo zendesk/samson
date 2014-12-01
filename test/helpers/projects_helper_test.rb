9e17e442933b5639a1294539aaa80d474c43ed0f
Fábio Neves <fneves@zendesk.com>
2014-12-01 09:31:24 +0000

    adding spec

--- test/helpers/projects_helper_test.rb
+++ test/helpers/projects_helper_test.rb
@@ -4,6 +4,7 @@ describe ProjectsHelper do
   describe "#star_link" do
     let(:project) { projects(:test) }
     let(:current_user) { users(:admin) }
+    let(:stage) { stages(:test_staging) }
 
     it "star a project" do
       current_user.stubs(:starred_project?).returns(false)
@@ -18,5 +19,16 @@ describe ProjectsHelper do
       assert_includes link, %{href="/stars/#{project.to_param}"}
       assert_includes link, %{data-method="delete"}
     end
+
+    it 'returns the deployment alert data' do
+      job = project.jobs.create!(command: 'cat foo', user: users(:deployer), status: 'failed')
+      deploy = stage.deploys.create!(reference: 'master', job: job)
+      data = deployment_alert_data(project, stage)
+      data[:url].must_equal project_deploy_path(project, deploy)
+      data[:reference].must_equal deploy.reference
+      data[:timestamp].must_equal deploy.updated_at.strftime('%m/%d/%Y %H:%M:%S')
+      data[:user].must_equal deploy.user.name
+    end
+
   end
 end
