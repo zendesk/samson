c597313d4fde0cd084b267b9769504a9687372b4
Fábio Neves <fneves@zendesk.com>
2014-12-08 11:35:48 +0000

    Removing the JSON response from the releases controller.

--- app/controllers/releases_controller.rb
+++ app/controllers/releases_controller.rb
@@ -10,11 +10,6 @@ class ReleasesController < ApplicationController
   def index
     @stages = @project.stages
     @releases = @project.releases.sort_by_version.page(params[:page])
-
-    respond_to do |format|
-      format.json { render json: @releases.map(&:version), root: false }
-      format.html
-    end
   end
 
   def new
@@ -23,7 +18,6 @@ class ReleasesController < ApplicationController
 
   def create
     @release = ReleaseService.new(@project).create_release(release_params)
-
     redirect_to project_release_path(@project, @release)
   end
 
