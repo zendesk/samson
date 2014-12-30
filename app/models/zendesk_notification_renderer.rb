062767903d79114b4435ae919e342123306ccf72
Jason Smale <jsmale@zendesk.com>
2014-12-30 11:06:12 +1100

    Use rails root for folder path, just because @steved555 did it for flowdock in 1dbdeff3

--- app/models/zendesk_notification_renderer.rb
+++ app/models/zendesk_notification_renderer.rb
@@ -1,7 +1,7 @@
 class ZendeskNotificationRenderer
   def self.render(deploy, ticket_id)
     controller = ActionController::Base.new
-    view = ActionView::Base.new('app/views/zendesk', {}, controller)
+    view = ActionView::Base.new(Rails.root.join('app/views/zendesk'), {}, controller)
     locals = { deploy: deploy, commits: deploy.changeset.commits, ticket_id: ticket_id, url: url(deploy) }
     view.render(template: 'notification', locals: locals).chomp
   end
