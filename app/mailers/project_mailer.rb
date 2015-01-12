5ff126fce423627aab57d3fb51fbc955cc1cac42
Jason Stillwell <jstillwell@zendesk.com>
2015-01-12 10:06:40 -0800

    cosmetic: break up long line.

--- app/mailers/project_mailer.rb
+++ app/mailers/project_mailer.rb
@@ -2,6 +2,9 @@ class ProjectMailer < ApplicationMailer
   layout 'mailer'
 
   def created_email(user, project)
-    mail(to: ENV['PROJECT_CREATED_NOTIFY_ADDRESS'], subject: "Samson Project Created: #{project.name}", body: "#{user.name_and_email} just created a new project #{project.name}")
+    address = ENV['PROJECT_CREATED_NOTIFY_ADDRESS']
+    subject = "Samson Project Created: #{project.name}"
+    body = "#{user.name_and_email} just created a new project #{project.name}"
+    mail(to: address, subject: subject, body: body)
   end
 end
