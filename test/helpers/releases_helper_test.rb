2e48aa7e9c474c4b63be81a03a092befe01d035b
Michael Grosser <michael@grosser.it>
2015-03-09 17:54:16 -0700

    fix invalid link to new deploy

---
+++ test/helpers/releases_helper_test.rb
@@ -0,0 +1,18 @@
+require_relative '../test_helper'
+
+describe ReleasesHelper do
+  describe "#link_to_deploy_stage" do
+    let(:stage) { stages(:test_staging) }
+    let(:release) { Release.new }
+    before { @project = stage.project } # ugly ...
+
+    it "links to a new deploy when it needs to be confirmed" do
+      stage.confirm = true
+      link_to_deploy_stage(stage, release).must_include "/projects/foo/stages/staging/deploys/new?"
+    end
+
+    it "links creating a deploy when it does not needs to be confirmed" do
+      link_to_deploy_stage(stage, release).must_include "/projects/foo/deploys?"
+    end
+  end
+end
