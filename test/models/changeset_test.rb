07ead3a33acff0489668c9ca50a7dbe34fd273eb
Michael Grosser <michael@grosser.it>
2015-03-10 15:51:04 -0700

    catch all kinds of Oktokit errors

--- test/models/changeset_test.rb
+++ test/models/changeset_test.rb
@@ -14,10 +14,16 @@ describe Changeset do
       Changeset.new("foo/bar", "a", "b").comparison.to_h.must_equal :x => "y"
     end
 
-    it "catches exceptions" do
-      GITHUB.expects(:compare).raises(Octokit::NotFound)
-      comparison = Changeset.new("foo/bar", "a", "b").comparison
-      comparison.error.must_equal "Commit not found"
+    {
+      Octokit::NotFound => "Github: Not found",
+      Octokit::Unauthorized => "Github: Unauthorized",
+      Octokit::InternalServerError => "Github: Internal server error",
+    }.each do |exception, message|
+      it "catches #{exception} exceptions" do
+        GITHUB.expects(:compare).raises(exception)
+        comparison = Changeset.new("foo/bar", "a", "b").comparison
+        comparison.error.must_equal message
+      end
     end
   end
 
