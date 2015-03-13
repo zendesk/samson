c1926ddb619b31b93d5267c5d8a8066eecaf956e
Shane Hender <shender@zendesk.com>
2015-03-13 14:15:24 +0000

    Fix problem with displaying dashboard waaaay back in time.

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,9 +1,11 @@
 class DashboardsController < ApplicationController
+  before_action :find_environment
+
   def show
     @before = Time.parse(params[:before] || Time.now.to_s(:db))
     @deploys = ordered_projects.each_with_object({}) do |project, hash|
       hash[project] = project.last_deploy_by_group(@before)
-      hash[project].select! { |id, _v| environment.deploy_group_ids.include?(id) }
+      hash[project].select! { |id, _v| @environment.deploy_group_ids.include?(id) }
     end
   end
 
@@ -13,7 +15,7 @@ class DashboardsController < ApplicationController
     Project.ordered_for_user(current_user).with_deploy_groups
   end
 
-  def environment
-    @environment ||= Environment.find_by_param!(params[:id])
+  def find_environment
+    @environment = Environment.find_by_param!(params[:id])
   end
 end
