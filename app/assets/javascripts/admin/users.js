8ce5005a4a92562e3dd51006e1d413125b676ce0
Michael Grosser <michael@grosser.it>
2015-03-18 09:39:40 +0100

    dry up users js and normalize users controller

--- app/assets/javascripts/admin/users.js
+++ app/assets/javascripts/admin/users.js
@@ -8,22 +8,19 @@ function($scope, $http, $timeout) {
     var roleTd  = A.$($event.target).closest(".role"),
         wrapper = roleTd.find(".relative-wrapper"),
         userId  = roleTd.data("user-id"),
+        showStatus = function(text){
+          $(text).appendTo(wrapper).delay(1500).fadeOut(500, function() {
+            this.remove();
+          });
+        },
         roleId;
 
     if ($event.target.type === "radio") {
       roleId = $event.target.value;
 
-      $http.put("/admin/users/" + userId, { role_id: roleId })
-        .success(function() {
-          $(SUCCESS).appendTo(wrapper).delay(1500).fadeOut(500, function() {
-            this.remove();
-          });
-        })
-        .error(function() {
-          $(FAILURE).appendTo(wrapper).delay(1500).fadeOut(500, function() {
-            this.remove();
-          });
-        });
+      $http.put("/admin/users/" + userId, { user: { role_id: roleId } })
+        .success(function() { showStatus(SUCCESS) })
+        .error(function() { showStatus(FAILURE) })
     }
   };
 }]);
