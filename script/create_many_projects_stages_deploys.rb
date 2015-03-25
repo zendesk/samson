181bd0310f66279b51f684f09ce6fa0c4c2d8507
Shane Hender <shender@zendesk.com>
2015-03-25 16:56:51 +0000

    Tidy script.

--- script/create_many_projects_stages_deploys.rb
+++ script/create_many_projects_stages_deploys.rb
@@ -2,6 +2,9 @@
 
 # Creates lots of projects + stages + deploys with environments and deploy groups.
 
+NUM_PROJECTS = 10
+MAX_DEPLOYS_PER_PROJECTS = 15
+
 root = File.expand_path('..', File.dirname(__FILE__))
 require File.expand_path('config/environment', root)
 
@@ -20,9 +23,10 @@ pod99 = DeployGroup.unscoped.find_or_create_by!(name: 'Pod99', environment: mast
 pod100 = DeployGroup.unscoped.find_or_create_by!(name: 'Pod100', environment: stage_env)
 pod101 = DeployGroup.unscoped.find_or_create_by!(name: 'Pod101', environment: stage_env)
 
-10.times do |i|
-  if Project.unscoped.where(name: "Project #{i}").count == 0
-    project = Project.create!(name: "Project #{i}", repository_url: "git@github.com:samson-test-org/example-project.git")
+NUM_PROJECTS.times do |i|
+  project_name = "Project#{i}"
+  if Project.unscoped.where(name: project_name).count == 0
+    project = Project.create!(name: project_name, repository_url: "git@github.com:samson-test-org/example-project.git")
     project.stages.create!(name: "Production", deploy_groups: [pod1, pod2, pod3, pod4, pod5, pod6])
     project.stages.create!(name: "Staging", deploy_groups: [pod100, pod101])
     project.stages.create!(name: "Master", deploy_groups: [pod98, pod99])
@@ -32,7 +36,7 @@ pod101 = DeployGroup.unscoped.find_or_create_by!(name: 'Pod101', environment: st
 
     job = Job.find_or_create_by!(command: 'true', user: User.first, project: project, status: 'succeeded', output: 'foobar', commit: 'master')
 
-    rand(15).times do
+    rand(MAX_DEPLOYS_PER_PROJECTS).times do
       start_time = Time.now - rand(20.days)
       Deploy.create!(stage: project.stages.sample, job: job, reference: %w(master v1 v2 v4 v7 v9 v11 v12 v10 v6).sample, created_at: start_time, started_at: start_time, updated_at: (start_time + 10.minutes) )
     end
