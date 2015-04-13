a3ffe9578cf1591b75b93df98de7e2670e760208
Michael Grosser <michael@grosser.it>
2015-04-13 12:09:16 -0700

    do not trigger a ajax call after just having loaded the page

--- app/assets/javascripts/current_deploys.js
+++ app/assets/javascripts/current_deploys.js
@@ -1,14 +1,10 @@
 $(function() {
+  var refresh = 5 * 1000;
   var $badge = $('.current-deploys .badge');
 
   var updateBadgeDisplay = function(number) {
-    if (number !== undefined) { $badge.text(number); }
-
-    if (parseInt($badge.text()) > 0) {
-      $badge.removeClass('hide');
-    } else {
-      $badge.addClass('hide');
-    }
+    $badge.text(number);
+    $badge.toggle(number !== 0);
   };
 
   var getBadgeStatus = function() {
@@ -16,15 +12,15 @@ $(function() {
       url: $badge.attr('data-refresh-url'),
       success: function(data) {
         updateBadgeDisplay(data.deploys.length);
-        setTimeout(getBadgeStatus, 5000);
+        setTimeout(getBadgeStatus, refresh);
       },
       error: function(data) {
         console.log('Unable to get currently deploy count. Retrying in 60 seconds.');
-        setTimeout(getBadgeStatus, 60000);
+        setTimeout(getBadgeStatus, 60 * 1000);
       }
     });
   };
 
-  updateBadgeDisplay();
-  getBadgeStatus();
+  updateBadgeDisplay(parseInt($badge.text(), 10));
+  setTimeout(getBadgeStatus, refresh);
 });
