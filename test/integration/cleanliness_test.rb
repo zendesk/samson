0ddbad3161a446167a8a55c3b17a468f0756c023
Michael Grosser <michael@grosser.it>
2015-04-16 14:03:13 -0700

    covert everything to rails-assets

--- test/integration/cleanliness_test.rb
+++ test/integration/cleanliness_test.rb
@@ -5,4 +5,10 @@ describe "cleanliness" do
   it "does not have boolean limit 1 in schema since this breaks mysql" do
     File.read("db/schema.rb").wont_match /\st\.boolean.*limit: 1/
   end
+
+  it "does not include rails-assets-bootstrap" do
+    # make sure rails-assets-bootstrap did not get included by accident (dependency of some other bootstrap thing)
+    # if it is not avoidable see http://stackoverflow.com/questions/7163264
+    File.read('Gemfile.lock').wont_include 'rails-assets-bootstrap '
+  end
 end
