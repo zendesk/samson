be2d38543be6e3060c8fbfd502335d82524a141e
Roman Sandler <rsandler@zendesk.com>
2015-06-10 15:24:40 -0700

    fix migrations

--- db/migrate/20150605194346_rename_build_columns.rb
+++ db/migrate/20150605194346_rename_build_columns.rb
@@ -1,8 +1,8 @@
 class RenameBuildColumns < ActiveRecord::Migration
   def change
     change_table :builds do |t|
-      t.rename :docker_sha, :docker_sha
-      t.rename :docker_ref, :docker_ref
+      t.rename :container_sha, :docker_sha
+      t.rename :container_ref, :docker_ref
     end
   end
 end
