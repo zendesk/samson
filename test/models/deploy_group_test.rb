e8970f40ea0915027f576048450a86ee32a11039
Michael Grosser <michael@grosser.it>
2015-06-19 11:16:44 -0700

    when changing or deleting a deploy group projects#show stage stayed unchanged

--- test/models/deploy_group_test.rb
+++ test/models/deploy_group_test.rb
@@ -1,18 +1,30 @@
 require_relative '../test_helper'
 
 describe DeployGroup do
-  let(:prod_env) { environments(:production) }
+  let(:stage) { stages(:test_staging) }
+  let(:environment) { environments(:production) }
+  let(:deploy_group) { deploy_groups(:pod1) }
+
+  def self.it_expires_stage(method)
+    it "expires stages when #{method}" do
+      stage.deploy_groups << deploy_group
+      stage.update_column(:updated_at, 1.minute.ago)
+      old = stage.updated_at.to_s(:db)
+      deploy_group.send(method)
+      stage.reload.updated_at.to_s(:db).wont_equal old
+    end
+  end
 
   describe '.new' do
     it 'saves' do
-      deploy_group = DeployGroup.new(name: 'test deploy name', environment: prod_env)
+      deploy_group = DeployGroup.new(name: 'test deploy name', environment: environment)
       assert_valid(deploy_group)
     end
   end
 
   describe 'validations' do
     it 'require a name' do
-      deploy_group = DeployGroup.new(name: nil, environment: prod_env)
+      deploy_group = DeployGroup.new(name: nil, environment: environment)
       refute_valid(deploy_group)
     end
 
@@ -22,7 +34,7 @@ describe DeployGroup do
     end
 
     it 'require a unique name' do
-      deploy_group = DeployGroup.new(name: 'Pod1', environment: prod_env)
+      deploy_group = DeployGroup.new(name: 'Pod1', environment: environment)
       refute_valid(deploy_group)
     end
   end
@@ -31,17 +43,21 @@ describe DeployGroup do
     env = Environment.create!(name: 'env666')
     dg1 = DeployGroup.create!(name: 'Pod666', environment: env)
     dg2 = DeployGroup.create!(name: 'Pod667', environment: env)
-    DeployGroup.create!(name: 'Pod668', environment: prod_env)
+    DeployGroup.create!(name: 'Pod668', environment: environment)
     env.deploy_groups.must_equal [dg1, dg2]
   end
 
   describe "#initialize_env_value" do
     it 'prefils env_value' do
-      DeployGroup.create!(name: 'Pod666 - the best', environment: prod_env).env_value.must_equal 'Pod666 - the best'
+      DeployGroup.create!(name: 'Pod666 - the best', environment: environment).env_value.must_equal 'Pod666 - the best'
     end
 
     it 'can set env_value' do
-      DeployGroup.create!(name: 'Pod666 - the best', env_value: 'pod:666', environment: prod_env).env_value.must_equal 'pod:666'
+      DeployGroup.create!(name: 'Pod666 - the best', env_value: 'pod:666', environment: environment).env_value.must_equal 'pod:666'
     end
   end
+
+  it_expires_stage :save
+  it_expires_stage :destroy
+  it_expires_stage :soft_delete
 end
