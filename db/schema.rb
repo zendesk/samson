19bd365b88e66c8414e65496b6cc73ef367c31b5
Vinh <vinh@listia.com>
2015-06-25 14:31:51 -0700

    Fix schema verion

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150619170905) do
+ActiveRecord::Schema.define(version: 20150624230037) do
 
   create_table "build_statuses", force: :cascade do |t|
     t.integer  "build_id",                                     null: false
