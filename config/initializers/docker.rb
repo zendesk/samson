a7b6f274350d75cd8cc56e675904eb6126ef6f32
Roman Sandler <rsandler@zendesk.com>
2015-06-26 15:28:20 +1000

    longer docker timeout for slower ops like compiling native node modules

--- config/initializers/docker.rb
+++ config/initializers/docker.rb
@@ -2,6 +2,7 @@ require 'docker'
 
 if ENV['DOCKER_URL'].present? && !Rails.env.test? && !ENV['PRECOMPILE']
   Docker.url = ENV['DOCKER_URL']
+  Docker.options = { read_timeout: 600 }
 
   # Confirm the Docker daemon is a recent enough version
   Docker.validate_version!
