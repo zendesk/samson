3ae7297639fa81b1b94d8e86c001d88081bac615
Michael Grosser <michael@grosser.it>
2015-06-30 14:29:06 -0700

    refactor Environment a bit

--- plugins/env/app/models/environment_variable.rb
+++ plugins/env/app/models/environment_variable.rb
@@ -4,23 +4,34 @@ class EnvironmentVariable < ActiveRecord::Base
   validates :name, presence: true
   validates :scope_type, inclusion: ["Environment", "DeployGroup", nil]
 
-  def self.env(project, deploy_group)
-    variables = project.environment_variables + project.environment_variable_groups.flat_map(&:environment_variables)
-    variables.sort_by! { |ev| [ev.parent_type == "Project" ? 1 : 0, ev.send(:priority)] } # TODO move into priority
-    env = variables.each_with_object({}) do |ev, all|
-      match = (
-        !ev.scope_id || # for all
-        (deploy_group && (
-          (ev.scope_type == "DeployGroup" && ev.scope_id == deploy_group.id) || # matches deploy group
-          (ev.scope_type == "Environment" && ev.scope_id == deploy_group.environment_id) # matches deploy group's environment
-        ))
-      )
-      all[ev.name] = ev.value if match
+  class << self
+    def env(project, deploy_group)
+      variables = project.environment_variables + project.environment_variable_groups.flat_map(&:environment_variables)
+      variables.sort_by! { |ev| ev.send :priority }
+      env = variables.each_with_object({}) do |ev, all|
+        all[ev.name] = ev.value if matches?(ev, deploy_group)
+      end
+
+      resolve_dollar_variables(env)
     end
 
-    env.each_value do |value|
-      value.gsub!(/\$\{(\w+)\}|\$(\w+)/) do |original|
-        env[$1 || $2] || original
+    private
+
+    def matches?(ev, deploy_group)
+      return true unless ev.scope_id # for all
+      return false unless deploy_group # unscoped -> no specific groups
+      case ev.scope_type
+      when "DeployGroup" then ev.scope_id == deploy_group.id # matches deploy group
+      when "Environment" then ev.scope_id == deploy_group.environment_id # matches deploy group's environment
+      else raise "Unsupported scope #{ev.scope_type}"
+      end
+    end
+
+    def resolve_dollar_variables(env)
+      env.each_value do |value|
+        value.gsub!(/\$\{(\w+)\}|\$(\w+)/) do |original|
+          env[$1 || $2] || original
+        end
       end
     end
   end
@@ -38,7 +49,9 @@ class EnvironmentVariable < ActiveRecord::Base
   private
 
   def priority
-    case scope_type
+    result = []
+    result << (parent_type == "Project" ? 1 : 0)
+    result << case scope_type
     when nil then 0
     when "Environment" then 1
     when "DeployGroup" then 2
