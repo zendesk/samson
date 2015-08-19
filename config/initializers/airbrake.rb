b52c75fccc55f13069d252451e0ba79226c97d24
Shane Hender <henders@gmail.com>
2015-08-19 14:26:22 +0100

    Allow airbrakes to be output to log when in development mode.

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -1,7 +1,17 @@
-if !defined?(Rails) || Rails.env.staging? || Rails.env.production?
-  require 'airbrake'
+require 'airbrake'
 
+if !defined?(Rails) || Rails.env.staging? || Rails.env.production?
   Airbrake.configure do |config|
     config.api_key = ENV['AIRBRAKE_API_KEY']
+  end
+else
+  module Airbrake
+    def self.notify(ex, *args)
+      logger.error "AIRBRAKE: #{ex.message} - #{ex.backtrace[0..5].join("\n")}"
+    end
+
+    def self.notify_or_ignore(ex, *args)
+      notify(ex)
+    end
   end
 end
