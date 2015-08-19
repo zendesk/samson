92bbe8d55fcad0fe409d960eec829e1bcf91c83a
Yi Hong Ang <ang.yi.hong@gmail.com>
2015-08-19 18:10:03 +0800

    SAMSON-170: add simple JIRA issue key matching
    Requires an environment variable `JIRA_BASE_URL` to be set.
    Format should be a URL including the trailing slash.
    Example: https://jira.zendesk.com/browse/
    
    If `JIRA_BASE_URL` is not set, behaviour reverts to a more general case of the current behavior.
    It will match generic URLs with a /browse/ISSUE-KEY format instead of just atlassian ones.
    
    If `JIRA_BASE_URL` is set, plain issue keys (e.g., ABC-123) will have full URLs generated.
    e.g., https://jira.zendesk.com/browse/ABC-123
    
    Works for issue keys in both title and body of the PR.

--- app/models/changeset/pull_request.rb
+++ app/models/changeset/pull_request.rb
@@ -1,13 +1,19 @@
 class Changeset::PullRequest
+  # Common patterns
+  CODE_ONLY = "[A-Z][A-Z\\d]+-\\d+"  # e.g., S4MS0N-123, SAM-456
+  PUNCT = "\\s|\\p{Punct}|~|="
+
   # Matches a section heading named "Risks".
   RISKS_SECTION = /#+\s+Risks.*\n/i
 
   # Matches URLs to JIRA issues.
-  JIRA_ISSUE = %r[https://\w+\.atlassian\.net/browse/[\w-]+]
+  JIRA_ISSUE_URL = %r[https?:\/\/[\da-z\.\-]+\.[a-z\.]{2,6}\/browse\/#{CODE_ONLY}(?=#{PUNCT}|$)]
 
   # Matches "VOICE-1234" or "[VOICE-1234]"
-  JIRA_CODE = %r[(\[)*([a-zA-Z]+)-((\d)+)(\])*]
+  JIRA_CODE_TITLE = %r[(\[)*(#{CODE_ONLY})(\])*]
 
+  # Matches "VOICE-1234" only
+  JIRA_CODE = %r[(?<=#{PUNCT}|^)(#{CODE_ONLY})(?=#{PUNCT}|$)]
   # Finds the pull request with the given number.
   #
   # repo   - The String repository name, e.g. "zendesk/samson".
@@ -34,7 +40,7 @@ class Changeset::PullRequest
   delegate :number, :title, :additions, :deletions, to: :@data
 
   def title_without_jira
-    title.gsub(JIRA_CODE, "").strip
+    title.gsub(JIRA_CODE_TITLE, "").strip
   end
 
   def url
@@ -62,15 +68,25 @@ class Changeset::PullRequest
   end
 
   def jira_issues
-    @jira_issues ||= parse_jira_issues!
+    @jira_issues ||= parse_jira_issues
   end
 
   private
 
-  def parse_jira_issues!
-    body.scan(JIRA_ISSUE).map do |match|
-      Changeset::JiraIssue.new(match)
+  def parse_jira_issues
+    custom_jira_url = ENV['JIRA_BASE_URL']
+    title_and_body = "#{title} #{body}"
+    jira_issue_map = {}
+    if custom_jira_url
+      title_and_body.scan(JIRA_CODE).each do |match|
+        jira_issue_map[match[0]] = custom_jira_url + match[0]
+      end
     end
+    # explicit URLs should take precedence for issue links
+    title_and_body.scan(JIRA_ISSUE_URL).each do |match|
+      jira_issue_map[match.match(JIRA_CODE)[0]] = match
+    end
+    jira_issue_map.values.map { |x| Changeset::JiraIssue.new(x) }
   end
 
   def body
