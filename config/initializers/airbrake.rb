95920588a41ff6cb86a7634627659b3c4b09239d
Shane Hender <henders@gmail.com>
2015-08-27 17:40:00 +0100

    Make it a new module.

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -1,16 +1,15 @@
-require 'airbrake'
-
 if !defined?(Rails) || Rails.env.staging? || Rails.env.production?
+  require 'airbrake'
   Airbrake.configure do |config|
     config.api_key = ENV['AIRBRAKE_API_KEY']
   end
 else
   module Airbrake
-    def self.notify(ex, *args)
-      logger.error "AIRBRAKE: #{ex.message} - #{ex.backtrace[0..5].join("\n")}"
+    def self.notify(ex, *_args)
+      Rails.logger.error "AIRBRAKE: #{ex.message} - #{ex.backtrace[0..5].join("\n")}"
     end
 
-    def self.notify_or_ignore(ex, *args)
+    def self.notify_or_ignore(ex, *_args)
       notify(ex)
     end
   end
