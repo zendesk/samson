f0f05aa641139ca1bba9ef1d717d253f06864c2b
Maciek Sufa <msufa@zendesk.com>
2015-09-21 14:09:43 +0100

    add separate authorization_error flash

--- app/helpers/flash_helper.rb
+++ app/helpers/flash_helper.rb
@@ -1,6 +1,6 @@
 module FlashHelper
   # Flash type -> Bootstrap alert class
   def flash_mapping
-    { notice: :info, error: :danger, success: :success }
+    { notice: :info, error: :danger, authorization_error: :danger, success: :success }
   end
 end
