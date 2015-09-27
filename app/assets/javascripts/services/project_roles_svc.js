79d2d69867285fe8350a9646d6ce11f9ace9eae9
Sergio Nunes <snunes@zendesk.com>
2015-09-27 22:43:08 +0100

    Refactoring some code.

--- app/assets/javascripts/services/project_roles_svc.js
+++ app/assets/javascripts/services/project_roles_svc.js
@@ -1,4 +1,4 @@
-samson.service('projectRolesService', function ($http, $q) {
+samson.service('projectRolesService', function ($http) {
 
     this.loadProjectRoles = function () {
         return $http.get('/project_roles');
@@ -6,19 +6,23 @@ samson.service('projectRolesService', function ($http, $q) {
 
     this.createProjectRole = function (project_role) {
         var data = {
-            user_id: project_role.user_id,
-            project_id: project_role.project_id,
-            role_id: project_role.role_id
+            project_role: {
+                user_id: project_role.user_id,
+                project_id: project_role.project_id,
+                role_id: project_role.role_id
+            }
         };
 
-        return $http.post('/projects/' + project_role.project_id + '/project_roles', {project_role: data});
+        return $http.post('/projects/' + project_role.project_id + '/project_roles', data);
     };
 
     this.updateProjectRole = function (project_role) {
         var data = {
-            role_id: project_role.role_id
+            project_role: {
+                role_id: project_role.role_id
+            }
         };
 
-        return $http.put('/projects/' + project_role.project_id + '/project_roles/' + project_role.id, {project_role: data});
+        return $http.put('/projects/' + project_role.project_id + '/project_roles/' + project_role.id, data);
     };
 });
