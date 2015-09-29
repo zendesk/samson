5ffb755e4438f8914332798ac0d4c238d1470d9f
Sergio Nunes <snunes@zendesk.com>
2015-09-29 12:05:00 +0100

    Updates to LocksController to allow project level authorisation (project deployers should be allowed to create and delete locks).

--- test/controllers/locks_controller_test.rb
+++ test/controllers/locks_controller_test.rb
@@ -1,21 +1,37 @@
 require_relative '../test_helper'
 
 describe LocksController do
+  let(:stage) { stages(:test_staging) }
+  let(:lock) { stage.create_lock! user: users(:deployer) }
   let(:global_lock) { Lock.create! user: users(:deployer) }
 
   before { request.headers['HTTP_REFERER'] = '/back' }
 
   as_a_viewer do
     unauthorized :post, :create
-    unauthorized :post, :create, lock: {stage_id: 1}
-    unauthorized :delete, :destroy, id: 1
+
+    it('responds with unauthorized when doing a post to create a local lock') do
+      post :create, lock: {stage_id: stage.id}
+      @unauthorized.must_equal true, "Request was not marked unauthorized"
+    end
+
+    it('is unauthorized when doing a delete to destroy a local lock') do
+      delete :destroy, id: lock.id
+      @unauthorized.must_equal true, "Request was not marked unauthorized"
+    end
+
+    it('is unauthorized when doing a delete to destroy a global lock') do
+      delete :destroy, id: global_lock.id
+    end
   end
 
   as_a_deployer do
     unauthorized :post, :create
-    it("unauthorized") { post :create, id: global_lock.id }
 
-    let(:stage) { stages(:test_staging) }
+    it("responds with unauthorized when doing a post to create a global lock") do
+      post :create, lock: {stage_id: '', description: "DESC"}
+      @unauthorized.must_equal true, "Request was not marked unauthorized"
+    end
 
     describe 'POST to #create' do
       it "creates a lock" do
@@ -62,7 +78,7 @@ describe LocksController do
   as_a_admin do
     describe 'POST to #create' do
       it "creates a global lock" do
-        post :create, lock: {description: "DESC"}
+        post :create, lock: {stage_id: '', description: "DESC"}
         assert_redirected_to "/back"
         assert flash[:notice]
 
@@ -78,6 +94,53 @@ describe LocksController do
         assert_redirected_to "/back"
         assert flash[:notice]
 
+        Lock.count.must_equal 0
+      end
+    end
+  end
+
+  as_a_viewer_project_deployer do
+    unauthorized :post, :create
+
+    it("responds with unauthorized when doing a post to create a global lock") { post :create, id: global_lock.id }
+
+    describe 'POST to #create' do
+      it "creates a lock" do
+        post :create, lock: {stage_id: stage.id, description: "DESC"}
+        assert_redirected_to "/back"
+        assert flash[:notice]
+
+        stage.reload
+
+        stage.warning?.must_equal(false)
+        stage.locked?.must_equal(true)
+        stage.lock.description.must_equal "DESC"
+      end
+
+      it "creates a warning" do
+        post :create, lock: {stage_id: stage.id, description: "DESC", warning: true}
+        assert_redirected_to "/back"
+        assert flash[:notice]
+
+        stage.reload
+
+        stage.warning?.must_equal(true)
+        stage.locked?.must_equal(false)
+        stage.lock.description.must_equal "DESC"
+      end
+    end
+
+    describe 'DELETE to #destroy' do
+      it "destroys the lock" do
+        lock = stage.create_lock!(user: users(:deployer))
+        delete :destroy, id: lock.id
+
+        assert_redirected_to "/back"
+        assert flash[:notice]
+
+        stage.reload
+
+        stage.locked?.must_equal(false)
         Lock.count.must_equal 0
       end
     end
