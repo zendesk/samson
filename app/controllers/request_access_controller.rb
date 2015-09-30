31def6c9107a38becd94bc1725c3619c803f4061
Maciek Sufa <msufa@zendesk.com>
2015-09-30 14:26:22 +0100

    configuration flag to enable the feature

--- app/controllers/request_access_controller.rb
+++ app/controllers/request_access_controller.rb
@@ -1,7 +1,11 @@
 class RequestAccessController < ApplicationController
   def send_email
-    RequestAccessMailer.request_access_email(request.base_url, current_user).deliver_now
-    flash[:success] = 'Access request email sent.'
-    redirect_to :back
+    if ENV['REQUEST_ACCESS_FEATURE'].present?
+      RequestAccessMailer.request_access_email(request.base_url, current_user).deliver_now
+      flash[:success] = 'Access request email sent.'
+      redirect_to :back
+    else
+      raise ActionController::RoutingError.new('Not Found')
+    end
   end
 end
