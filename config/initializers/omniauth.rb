7da6b823d2c858f2d85f24639e8071a9f5091039
Steven Davidovitz <sdavidovitz@zendesk.com>
2015-10-05 13:33:53 -0700

    actually disable authorization strategies
    Right now, they're just hidden in the UI.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -7,31 +7,37 @@ require 'omniauth-google-oauth2'
 require 'omniauth-ldap'
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :github,
-    ENV["GITHUB_CLIENT_ID"],
-    ENV["GITHUB_SECRET"],
-    scope: "user:email",
-    client_options: {
-      site:          "https://#{Rails.application.config.samson.github.api_url}",
-      authorize_url: "https://#{Rails.application.config.samson.github.web_url}/login/oauth/authorize",
-      token_url:     "https://#{Rails.application.config.samson.github.web_url}/login/oauth/access_token",
-    }
+  if Rails.application.config.samson.auth.github
+    provider :github,
+      ENV["GITHUB_CLIENT_ID"],
+      ENV["GITHUB_SECRET"],
+      scope: "user:email",
+      client_options: {
+        site:          "https://#{Rails.application.config.samson.github.api_url}",
+        authorize_url: "https://#{Rails.application.config.samson.github.web_url}/login/oauth/authorize",
+        token_url:     "https://#{Rails.application.config.samson.github.web_url}/login/oauth/access_token",
+      }
+  end
 
-  provider OmniAuth::Strategies::GoogleOauth2,
-    ENV["GOOGLE_CLIENT_ID"],
-    ENV["GOOGLE_CLIENT_SECRET"],
-    {
-      name:   "google",
-      scope:  "email,profile",
-      prompt: "select_account",
-    }
+  if Rails.application.config.samson.auth.google
+    provider OmniAuth::Strategies::GoogleOauth2,
+      ENV["GOOGLE_CLIENT_ID"],
+      ENV["GOOGLE_CLIENT_SECRET"],
+      {
+        name:   "google",
+        scope:  "email,profile",
+        prompt: "select_account",
+      }
+  end
 
-  provider OmniAuth::Strategies::LDAP,
-    title: Rails.application.config.samson.ldap.title,
-    host: Rails.application.config.samson.ldap.host,
-    port: Rails.application.config.samson.ldap.port,
-    method: 'plain',
-    base: Rails.application.config.samson.ldap.base,
-    uid: Rails.application.config.samson.ldap.uid,
-    password: Rails.application.config.samson.ldap.password
+  if Rails.application.config.samson.auth.ldap
+    provider OmniAuth::Strategies::LDAP,
+      title: Rails.application.config.samson.ldap.title,
+      host: Rails.application.config.samson.ldap.host,
+      port: Rails.application.config.samson.ldap.port,
+      method: 'plain',
+      base: Rails.application.config.samson.ldap.base,
+      uid: Rails.application.config.samson.ldap.uid,
+      password: Rails.application.config.samson.ldap.password
+  end
 end
