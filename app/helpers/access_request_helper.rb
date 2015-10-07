ae2030f300e641cbb999ec3f29637b6803b50fca
Maciek Sufa <msufa@zendesk.com>
2015-10-07 17:17:39 +0100

    request additional access rights from profile page

--- app/helpers/access_request_helper.rb
+++ app/helpers/access_request_helper.rb
@@ -1,11 +1,13 @@
 module AccessRequestHelper
-  def display_access_request_link?(flash_type)
-    flash_type == :authorization_error && AccessRequestsController.feature_enabled?
+  def display_access_request_link?(flash_type = :authorization_error)
+    flash_type == :authorization_error &&
+        AccessRequestsController.feature_enabled? &&
+        !current_user.is_super_admin?
   end
 
   def link_to_request_access
     current_user.access_request_pending ?
         'Access request pending.' :
-        (link_to 'Request access.', new_access_request_path)
+        (link_to 'Request additional access rights', new_access_request_path)
   end
 end
