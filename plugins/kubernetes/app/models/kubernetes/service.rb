887310aade148b588e5b76d979ee382cb8f9e20f
Jon Moter <jmoter@zendesk.com>
2015-10-22 11:49:55 -0700

    refactored some Kubernetes::Service code based on PR feedback

--- plugins/kubernetes/app/models/kubernetes/service.rb
+++ plugins/kubernetes/app/models/kubernetes/service.rb
@@ -1,13 +1,19 @@
 module Kubernetes
   # Convenience wrapper around the 'Service' object we can get from the K8S API
   class Service
-    attr_reader :deploy_group, :role, :name, :ip_address, :port_info
+    attr_reader :deploy_group, :role
 
-    def initialize(deploy_group: nil, role: nil)
+    def initialize(deploy_group:, role:)
       @deploy_group = deploy_group
       @role = role
-      @port_info = []
-      @name = role.try(:service_name)
+    end
+
+    def name
+      if defined?(@service_object)
+        service_object.metadata.name
+      else
+        role.try(:service_name)
+      end
     end
 
     def namespace
@@ -18,24 +24,35 @@ module Kubernetes
       service_object.present?
     end
 
+    def ip_address
+      service_object.spec.clusterIP if running?
+    end
+
+    def port_info
+      (service_object.spec.ports if running?) || []
+    end
+
     def proxy_address
       "#{example_node_ip}:#{node_ports.first}" if running?
     end
 
+    # In Kubernetes, each minion acts as a proxy. So there's no single server
+    # that acts as a proxy/load-balancer. This method returns the IP address
+    # of one of the nodes that could act as a proxy.
     def example_node_ip
       node = client.get_nodes.first
       node.spec.externalID if node
     end
 
     # The set of ports that are exposed on each Kubernetes minion. Traffic sent
-    # to this port on any minion will be proxied to the Pods in this service.
+    # to these ports on any minion will be proxied to the Pods in this service.
     def node_ports
-      fetch_service if @service_object.nil?
-      @port_info.map(&:nodePort)
+      fetch_service
+      port_info.map(&:nodePort)
     end
 
     # Returns an array of OpenStructs containing the list of Pods that this
-    # service routes traffic to. Each object return has the following attributes:
+    # service routes traffic to. Each object returned has the following attributes:
     #   `pod_ip` : IP address of the Pod
     #   `pod_name` : name of the Pod
     #   `ready?` : true if the Pod is ready and accepting traffic
@@ -46,14 +63,14 @@ module Kubernetes
 
       # https://htmlpreview.github.io/?https://github.com/kubernetes/kubernetes/HEAD/docs/api-reference/v1/definitions.html#_v1_endpoints
       endpoint_object.subsets.each do |subset|
-        [:addresses, :notReadyAddresses].each do |attr|
-          next if subset[attr].blank?
+        [:addresses, :notReadyAddresses].each do |key|
+          next if subset[key].blank?
 
-          subset[attr].each do |address|
+          subset[key].each do |address|
             if address.targetRef.kind == 'Pod'
               results << OpenStruct.new(pod_ip: address.ip,
                                         pod_name: address.targetRef.name,
-                                        ready?: (attr == :addresses))
+                                        ready?: (key == :addresses))
             end
           end
         end
@@ -63,21 +80,17 @@ module Kubernetes
     end
 
     def service_object
-      @service_object || fetch_service
+      defined?(@service_object) ? @service_object : fetch_service
     end
 
     def endpoint_object
-      @endpoint_object || fetch_endpoint
+      defined?(@endpoint_object) ? @endpoint_object : fetch_endpoint
     end
 
     private
 
     def fetch_service
       @service_object = client.get_service(name, namespace)
-      @name = @service_object.metadata.name
-      @ip_address = @service_object.spec.clusterIP
-      @port_info = @service_object.spec.ports || []
-      @service_object
     rescue KubeException => e
       raise e unless e.error_code == 404
       nil
