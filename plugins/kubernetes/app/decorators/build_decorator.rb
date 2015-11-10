b11ec39dc200e86023ca26777602b48c4cf7b8e5
Shane Hender <henders@gmail.com>
2015-11-10 12:11:39 +0000

    Fix build decorator.

--- plugins/kubernetes/app/decorators/build_decorator.rb
+++ plugins/kubernetes/app/decorators/build_decorator.rb
@@ -1,6 +1,6 @@
 Build.class_eval do
 
-  has_many :kubernetes_releases
+  has_many :kubernetes_releases, class_name: 'Kubernetes::Release'
 
   def project_name
     @project_name ||= project.name.parameterize('-')
