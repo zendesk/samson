b785b064a445cef3deda59cb04380bae752fb115
Akash Manohar J <akash@akash.im>
2015-11-22 20:03:15 +0530

    Add migration to create slack_webhook_urls

---
+++ plugins/slack/db/migrate/20151122194738_create_slack_webhook_urls.rb
@@ -0,0 +1,12 @@
+class CreateSlackWebhookUrls < ActiveRecord::Migration
+  def change
+    create_table :slack_webhook_urls do |t|
+      t.text :webhook_url, null: false
+      t.integer :stage_id, null: false
+
+      t.timestamps
+    end
+
+    add_index :slack_webhook_urls, :stage_id
+  end
+end
