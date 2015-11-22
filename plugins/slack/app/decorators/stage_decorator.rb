7490967d551b69aa689860b15c17bb146f5f2ad6
Akash Manohar J <akash@akash.im>
2015-11-23 00:37:24 +0530

    Remove name field from being mandatory

--- plugins/slack/app/decorators/stage_decorator.rb
+++ plugins/slack/app/decorators/stage_decorator.rb
@@ -1,14 +1,14 @@
 Stage.class_eval do
 
   has_many :slack_webhooks
-  accepts_nested_attributes_for :slack_webhooks, allow_destroy: true, reject_if: :no_name_or_webhook_url?
+  accepts_nested_attributes_for :slack_webhooks, allow_destroy: true, reject_if: :no_webhook_url?
 
   def send_slack_notifications?
     slack_webhooks.any?
   end
 
-  def no_name_or_webhook_url?(slack_webhook_attrs)
-    slack_webhook_attrs['name'].blank? || slack_webhook_attrs['webhook_url'].blank?
+  def no_webhook_url?(slack_webhook_attrs)
+    slack_webhook_attrs['webhook_url'].blank?
   end
 
 end
