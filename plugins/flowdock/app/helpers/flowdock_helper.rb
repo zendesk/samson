9d90cd5e42114023d04a9eced926690a252db34b
Roman Sandler <rsandler@zendesk.com>
2015-11-24 11:35:20 +1100

    put the project name into the flowdock message

--- plugins/flowdock/app/helpers/flowdock_helper.rb
+++ plugins/flowdock/app/helpers/flowdock_helper.rb
@@ -1,5 +1,6 @@
 module FlowdockHelper
   def default_flowdock_message(deploy)
-    ":pray: @team #{deploy.user.name} is requesting approval to deploy **#{deploy.reference}** to production. [Approve this deploy](#{project_deploy_url(deploy.project, deploy)})."
+    ":pray: @team #{deploy.user.name} is requesting approval to deploy #{deploy.project.name} **#{deploy.reference}** to production."\
+    " [Approve this deploy](#{project_deploy_url(deploy.project, deploy)})."
   end
 end
