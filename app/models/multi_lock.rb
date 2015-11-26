03726265e2cbaee16b914f3efbb956d148cae980
Jon Moter <jmoter@zendesk.com>
2015-11-26 11:15:11 -0800

    add some more NewRelic custom metrics to hunt down Samson slowness

--- app/models/multi_lock.rb
+++ app/models/multi_lock.rb
@@ -3,14 +3,10 @@ class MultiLock
   cattr_accessor(:locks) { {} }
 
   class << self
+    include ::NewRelic::Agent::MethodTracer
+
     def lock(id, holder, options)
-      locked = false
-      end_time = Time.now + options.fetch(:timeout)
-      until Time.now > end_time
-        break if locked = try_lock(id, holder)
-        options.fetch(:failed_to_lock).call(locks[id])
-        sleep 1
-      end
+      locked = wait_for_lock(id, holder, options)
       yield if locked
       locked
     ensure
@@ -18,6 +14,17 @@ class MultiLock
     end
 
     private
+
+    def wait_for_lock(id, holder, options)
+      end_time = Time.now + options.fetch(:timeout)
+      until Time.now > end_time
+        return true if try_lock(id, holder)
+        options.fetch(:failed_to_lock).call(locks[id])
+        sleep 1
+      end
+      false
+    end
+    add_method_tracer :wait_for_lock
 
     def try_lock(id, holder)
       mutex.synchronize do
