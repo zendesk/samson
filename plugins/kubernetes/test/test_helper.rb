28efd86cc4b217bbcdca15907c16ea7a4e2522ac
Maciek Sufa <msufa@zendesk.com>
2015-12-10 15:25:47 +0000

    kubernetes dashboard controller tests

--- plugins/kubernetes/test/test_helper.rb
+++ plugins/kubernetes/test/test_helper.rb
@@ -6,8 +6,20 @@ class ActiveSupport::TestCase
   self.set_fixture_class kubernetes_releases: Kubernetes::Release
   self.set_fixture_class kubernetes_roles: Kubernetes::Role
   self.set_fixture_class kubernetes_clusters: Kubernetes::Cluster
+  self.set_fixture_class kubernetes_cluster_deploy_groups: Kubernetes::ClusterDeployGroup
+  self.set_fixture_class kubernetes_release_docs: Kubernetes::ReleaseDoc
 
   def parse_role_config_file(file_name)
-    File.read("#{Rails.root}/plugins/kubernetes/test/samples/#{file_name}.yml")
+    read_file "#{file_name}.yml"
+  end
+
+  def parse_json_response_file(file_name)
+    read_file "#{file_name}.json"
+  end
+
+  private
+
+  def read_file(file_name)
+    File.read("#{Rails.root}/plugins/kubernetes/test/samples/#{file_name}")
   end
 end
