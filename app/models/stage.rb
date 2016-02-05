bc54950c11a322cc10a1bff5350c5d16e66becaa
Michael Grosser <michael@grosser.it>
2016-02-04 20:36:37 -0800

    warn when doing weird settings

--- app/models/stage.rb
+++ app/models/stage.rb
@@ -22,6 +22,7 @@ class Stage < ActiveRecord::Base
   default_scope { order(:order) }
 
   validates :name, presence: true, uniqueness: { scope: [:project, :deleted_at] }
+  validate :validate_bypass_used_correctly
 
   accepts_nested_attributes_for :new_relic_applications, allow_destroy: true, reject_if: :no_newrelic_name?
 
@@ -218,5 +219,11 @@ class Stage < ActiveRecord::Base
   def ensure_ordering
     return unless project
     self.order = project.stages.maximum(:order).to_i + 1
+  end
+
+  def validate_bypass_used_correctly
+    if bypass_buddy_check? && !production?
+      errors.add(:bypass_buddy_check, 'makes no sense when set but not being in production')
+    end
   end
 end
