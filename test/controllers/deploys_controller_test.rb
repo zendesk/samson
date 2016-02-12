32d231de2f627e7f38718787018ae871c8c84664
Robert Ikeoka <rikeoka@gmail.com>
2016-02-11 21:20:31 -0800

    Add unit tests for csv reports
    Added completeness and accuracy test for users and deploys csv reports.
    Added content type check for csv reports.

--- test/controllers/deploys_controller_test.rb
+++ test/controllers/deploys_controller_test.rb
@@ -70,6 +70,35 @@ describe DeploysController do
           assert_response :ok
         end
       end
+
+      describe "as csv" do
+        let(:format) { :csv }
+
+        it "renders csv" do
+          assert_equal "text/csv", @response.content_type
+          assert_response :ok
+        end
+        
+        it "outputs csv accurately and completely" do
+          csv_response = CSV.parse(response.body)
+          csv_headers = csv_response.shift
+          deploycount = csv_headers.pop.to_i
+          Deploy.count.must_equal deploycount
+          deploycount.must_equal csv_response.length
+          assert_not_nil csv_response
+          csv_response.each do |d|
+            deploy_info = Deploy.find_by(id: d[0])
+            deploy_info.wont_be_nil
+            deploy_info.project.name.must_equal d[1]
+            deploy_info.summary.must_equal d[2]
+            deploy_info.updated_at.to_s.must_equal d[3]
+            deploy_info.start_time.to_s.must_equal d[4]
+            deploy_info.job.user.name.must_equal d[5]
+            deploy_info.csv_buddy.must_equal d[6]
+            deploy_info.stage.production.to_s.must_equal d[7]
+          end
+        end
+      end
     end
 
     describe "a GET to :recent with a project_id" do
