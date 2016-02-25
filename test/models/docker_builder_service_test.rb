154100befd3640635e81429baa99b79f4c44ed1b
Shane Hender <henders@gmail.com>
2016-02-25 09:42:35 +0000

    Fix tests.

--- test/models/docker_builder_service_test.rb
+++ test/models/docker_builder_service_test.rb
@@ -40,20 +40,22 @@ describe DeployService do
 
   describe '#push_image' do
     let(:repo_digest) { 'sha256:5f1d7c7381b2e45ca73216d7b06004fdb0908ed7bb8786b62f2cdfa5035fde2c' }
+    let(:push_output) { [
+      [{status: "pushing image to repo..."}.to_json],
+      [{status: "completed push."}.to_json],
+      [{status: "Frobinating..."}.to_json],
+      [{status: "Digest: #{repo_digest}"}.to_json],
+      [{status: "Done"}.to_json]
+    ] }
 
     before do
-      mock_docker_image.expects(:push).multiple_yields(
-        [{ status: "pushing image to repo..." }.to_json],
-        [{ status: "completed push." }.to_json],
-        [{ status: "Frobinating..." }.to_json],
-        [{ status: "Digest: #{repo_digest}" }.to_json],
-        [{ status: "Done" }.to_json]
-      )
-      mock_docker_image.expects(:tag).returns(true)
+      mock_docker_image.stubs(:push)
+      mock_docker_image.stubs(:tag)
       build.docker_image = mock_docker_image
     end
 
     it 'sets the values on the build' do
+      mock_docker_image.expects(:push).multiple_yields(*push_output).twice
       build.label = "Version 123"
       service.push_image(nil)
       assert_equal('version-123', build.docker_ref)
@@ -61,18 +63,21 @@ describe DeployService do
     end
 
     it 'saves docker output to the buffer' do
+      mock_docker_image.expects(:push).multiple_yields(*push_output).once
+      mock_docker_image.expects(:tag).once
       service.push_image(nil)
       assert_includes(service.output_buffer.to_s, 'Frobinating...')
       assert_equal('latest', build.docker_ref)
     end
 
     it 'uses the tag passed in' do
+      mock_docker_image.expects(:tag).twice
       service.push_image('my-test')
       assert_equal('my-test', build.docker_ref)
     end
 
     it 'always adds the latest tag on top of the one specified' do
-      mock_docker_image.expects(:tag).with(has_entry(tag: 'latest'))
+      mock_docker_image.expects(:tag).with(has_entry(tag: 'my-test')).with(has_entry(tag: 'latest'))
       mock_docker_image.expects(:push).with(service.send(:registry_credentials), tag: 'latest', force: true)
       service.push_image('my-test')
     end
