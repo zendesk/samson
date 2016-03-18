456fba582b08dbd45ecd270dbb6c1894ba1087e6
Ian Waters <iwaters@zendesk.com>
2016-03-18 12:45:03 -0700

    added testing for job.pid

--- test/models/job_test.rb
+++ test/models/job_test.rb
@@ -1,6 +1,15 @@
 require_relative '../test_helper'
+require 'byebug'
 
 describe Job do
+  include GitRepoTestHelper
+
+  before { Project.any_instance.stubs(:valid_repository_url).returns(true) }
+  let(:url) { "git://foo.com:hello/world.git" }
+  let(:user) { User.create!(name: 'test') }
+  let(:project) { Project.create!(name: 'jobtest', repository_url: url) }
+  let(:job) { project.jobs.create!(command: 'cat foo', user: user, project: project) }
+
   describe 'when project is globally locked' do
     before do
       Lock.create!(user: users(:admin))
@@ -10,4 +19,20 @@ describe Job do
       Job.create.errors[:project].must_equal(['is locked'])
     end
   end
+
+  describe "looking for the pid of a job" do
+    before do
+      JobExecution.start_job(JobExecution.new('master', job))
+      JobExecution.any_instance.stubs(:pid).returns(1234)
+    end
+    it "has a pid when running" do
+      job.run!
+      job.pid.wont_be_nil
+    end
+    it "has no pid when stopped" do
+      job.stop!
+      job.pid.must_be_nil
+    end
+  end
+
 end
