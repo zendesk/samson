f1f4709e5598ae7e2c12630f8c3cc1d5197c961d
Ian Waters <iwaters@zendesk.com>
2016-03-30 11:28:04 -0700

    removing the updating of all users

--- db/migrate/20160328221058_add_time_format_to_user.rb
+++ db/migrate/20160328221058_add_time_format_to_user.rb
@@ -1,8 +1,5 @@
 class AddTimeFormatToUser < ActiveRecord::Migration
   def change
     add_column :users, :time_format, :string, :default => 'relative', :null => false
-    User.all.each do |user|
-      user.update_attributes!(:time_format => 'relative')
-    end
   end
 end
