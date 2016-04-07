7ba0e0d3cd55dc543f026fa9d28ccc9529700667
Michael Grosser <michael@grosser.it>
2016-04-07 11:13:34 -0700

    macros have command associations

--- app/models/macro.rb
+++ app/models/macro.rb
@@ -1,21 +1,22 @@
 class Macro < ActiveRecord::Base
   has_soft_deletion default_scope: true
 
+  attr_writer :command
+
   belongs_to :project
   belongs_to :user
 
   has_many :macro_commands, autosave: true
-  has_many :commands,
-    -> { order('macro_commands.position ASC') },
+  has_many :commands, -> { order('macro_commands.position ASC') },
     through: :macro_commands, auto_include: false
 
   validates :name, presence: true, uniqueness: { scope: [:project, :deleted_at] }
-  validates :reference, :command, presence: true
+  validates :reference, presence: true
 
-  def macro_command
-    commands.map(&:command).
-      push(command).
-      join("\n")
+  before_save :build_new_project_command
+
+  def command
+    commands.map(&:command).join("\n")
   end
 
   def command_ids=(new_command_ids)
@@ -25,6 +26,15 @@ class Macro < ActiveRecord::Base
   end
 
   private
+
+  def build_new_project_command
+    return unless @command.present?
+
+    new_command = project.commands.build(command: @command)
+    macro_commands.build(command: new_command).tap do
+      reorder_commands
+    end
+  end
 
   def reorder_commands(command_ids = self.command_ids)
     macro_commands.each do |macro_command|
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160405103253) do
+ActiveRecord::Schema.define(version: 20160407181257) do
 
   create_table "build_statuses", force: :cascade do |t|
     t.integer  "build_id",                                     null: false
@@ -245,9 +245,8 @@ ActiveRecord::Schema.define(version: 20160405103253) do
   end
 
   create_table "macros", force: :cascade do |t|
-    t.string   "name",       limit: 255,   null: false
-    t.string   "reference",  limit: 255,   null: false
-    t.text     "command",    limit: 65535, null: false
+    t.string   "name",       limit: 255, null: false
+    t.string   "reference",  limit: 255, null: false
     t.integer  "project_id", limit: 4
     t.integer  "user_id",    limit: 4
     t.datetime "deleted_at"
