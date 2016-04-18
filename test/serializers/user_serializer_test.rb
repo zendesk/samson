34c96d780259e15772f37d9cd559081eb00e00cc
Michael Grosser <michael@grosser.it>
2016-04-18 11:05:43 -0700

    more coverage and change project url to include a actual url

---
+++ test/serializers/user_serializer_test.rb
@@ -0,0 +1,12 @@
+require_relative '../test_helper'
+
+SingleCov.covered!
+
+describe UserSerializer do
+  let(:user) { users(:admin) }
+  let(:parsed) { JSON.parse(UserSerializer.new(user).to_json) }
+
+  it 'serializes' do
+    parsed['user']['gravatar_url'].must_equal "https://www.gravatar.com/avatar/e64c7d89f26bd1972efa854d13d7dd61"
+  end
+end
