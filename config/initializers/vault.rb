486878a395724882cc2f4386a914e9473cb0b20b
Ian Waters <iwaters@zendesk.com>
2016-04-21 09:13:21 -0700

    more reasonable setting of defaults

--- config/initializers/vault.rb
+++ config/initializers/vault.rb
@@ -1,11 +1,11 @@
 VAULT_ENABLED = ENV.fetch('SECRET_STORAGE_BACKEND', false)
 if VAULT_ENABLED == 'SecretStorage::HashicorpVault'
   require 'vault'
-  Rails.logger.info("Enabling the vault client")
+  Rails.logger.info("Vault Client enabled")
   Vault.configure do |config|
     config.ssl_pem_file = ENV.fetch("VAULT_SSL_CERT")
-    config.ssl_verify = ActiveRecord::Type::Boolean.new.type_cast_from_user(ENV.fetch("VAULT_SSL_VERIFY"))
-    config.address = ENV.fetch("VAULT_ADDR") || 'https://127.0.0.1:8200'
+    config.ssl_verify = ActiveRecord::Type::Boolean.new.type_cast_from_user(ENV.fetch("VAULT_SSL_VERIFY"), true)
+    config.address = ENV.fetch("VAULT_ADDR", 'https://127.0.0.1:8200')
 
     # Timeout the connection after a certain amount of time (seconds), also read
     config.timeout = 30
