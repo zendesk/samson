082d6ad2861ac60b15238ef20e793e9600f90c44
Michael Grosser <michael@grosser.it>
2016-05-02 22:13:31 -0700

    docs

--- app/assets/javascripts/directives/filter_by_directive.js
+++ app/assets/javascripts/directives/filter_by_directive.js
@@ -1,3 +1,8 @@
+// filter elements in a list when typing
+// use by adding filter-by to an li's
+// <li filter-by="fooBar">
+// and having an input with that model
+// <input type="search" ng-model="fooBar">
 samson.directive('filterBy', function($parse) {
   return {
     restrict: 'A',
