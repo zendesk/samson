796439a6c01ab3750678a01520a995b7804e025d
Jon Moter <jmoter@zendesk.com>
2016-05-03 09:37:09 -0700

    add new migration to fix one that had a line commented out

--- plugins/kubernetes/db/migrate/20160426223230_add_kuberetes_to_stage.rb
+++ plugins/kubernetes/db/migrate/20160426223230_add_kuberetes_to_stage.rb
@@ -1,6 +1,5 @@
 class AddKuberetesToStage < ActiveRecord::Migration
   def change
-    # add_column :stages, :kubernetes, :boolean, default: false, null: false
     add_column :deploys, :kubernetes, :boolean, default: false, null: false
   end
 end
