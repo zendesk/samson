1fb35893956dac51f8d53eb4546270b1126a62e7
Michael Grosser <michael@grosser.it>
2016-05-10 18:46:00 -0700

    fix length again

--- plugins/kubernetes/db/migrate/20160509225637_make_kubernetes_services_unique.rb
+++ plugins/kubernetes/db/migrate/20160509225637_make_kubernetes_services_unique.rb
@@ -1,5 +1,5 @@
 class MakeKubernetesServicesUnique < ActiveRecord::Migration
   def change
-    add_index :kubernetes_roles, [:service_name, :deleted_at], unique: true, length: {"service_name"=>191}
+    add_index :kubernetes_roles, [:service_name, :deleted_at], unique: true, length: {service_name: 191}
   end
 end
