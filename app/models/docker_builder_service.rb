zendesk/samson

commit e3594b032196991b3c076545ecd03838ca2d5b8b

do not override latest with branch builds

app/models/docker_builder_service.rb
@@ -10,7 +10,7 @@ class DockerBuilderService
     @build = build
   end
 
-  def run!(image_name: nil, push: false)
+  def run!(image_name: nil, push: false, tag_as_latest: false)
     build.docker_build_job.try(:destroy) # if there's an old build job, delete it
 
     job = build.create_docker_job
@@ -22,7 +22,7 @@ class DockerBuilderService
       repository.executor = execution.executor
 
       if build_image(tmp_dir)
-        push_image(image_name) if push
+        push_image(image_name, push_to_latest: tag_as_latest) if push
         build.docker_image.remove(force: true)
       end
     end
@@ -50,7 +50,7 @@ class DockerBuilderService
   end
   add_method_tracer :build_image
 
-  def push_image(tag)
+  def push_image(tag, tag_as_latest: false)
     build.docker_ref = tag.presence || build.label.try(:parameterize).presence || 'latest'
     build.docker_repo_digest = nil
     output.puts("### Tagging and pushing Docker image to #{project.docker_repo}:#{build.docker_ref}")
@@ -65,7 +65,8 @@ class DockerBuilderService
         build.docker_repo_digest = "#{project.docker_repo}@#{matches[1]}"
       end
     end
-    push_latest unless build.docker_ref == 'latest'
+
+    push_latest if tag_as_latest && build.docker_ref != 'latest'
 
     build.save!
     build

app/controllers/integrations/base_controller.rb
@@ -83,7 +83,7 @@ class Integrations::BaseController < ApplicationController
   end
 
   def create_docker_image
-    DockerBuilderService.new(@build).run!(push: true)
+    DockerBuilderService.new(@build).run!(push: true, tag_as_latest: true)
   end
 
   def latest_release

test/models/docker_builder_service_test.rb
@@ -1,6 +1,6 @@
 require_relative '../test_helper'
 
-SingleCov.covered! uncovered: 21
+SingleCov.covered! uncovered: 20
 
 describe DeployService do
   include GitRepoTestHelper
@@ -26,17 +26,25 @@ describe DeployService do
   describe '#build_image' do
     before do
       Docker::Image.expects(:build_from_dir).returns(mock_docker_image)
-      service.build_image(tmp_dir)
     end
 
     it 'writes the REVISION file' do
+      service.build_image(tmp_dir)
       revision_filepath = File.join(tmp_dir, 'REVISION')
       assert File.exists?(revision_filepath)
       assert_equal(build.git_sha, File.read(revision_filepath))
     end
 
     it 'updates the Build object' do
+      service.build_image(tmp_dir)
       assert_equal(docker_image_id, build.docker_image_id)
+    end
+
+    it 'catches docker errors' do
+      Docker::Image.unstub(:build_from_dir)
+      Docker::Image.expects(:build_from_dir).raises(Docker::Error::DockerError.new("XYZ"))
+      service.build_image(tmp_dir).must_equal nil
+      build.docker_image_id.must_equal nil
     end
   end
 
@@ -57,7 +65,7 @@ describe DeployService do
     end
 
     it 'sets the values on the build' do
-      mock_docker_image.expects(:push).multiple_yields(*push_output).twice
+      mock_docker_image.expects(:push).multiple_yields(*push_output)
       build.label = "Version 123"
       service.push_image(nil)
       assert_equal('version-123', build.docker_ref)
@@ -73,15 +81,29 @@ describe DeployService do
     end
 
     it 'uses the tag passed in' do
-      mock_docker_image.expects(:tag).twice
+      mock_docker_image.expects(:tag)
       service.push_image('my-test')
       assert_equal('my-test', build.docker_ref)
     end
 
-    it 'always adds the latest tag on top of the one specified' do
-      mock_docker_image.expects(:tag).with(has_entry(tag: 'my-test')).with(has_entry(tag: 'latest'))
-      mock_docker_image.expects(:push).with(service.send(:registry_credentials), tag: 'latest', force: true)
-      service.push_image('my-test')
+    describe 'pushing latest' do
+      it 'adds the latest tag on top of the one specified when latest is true' do
+        mock_docker_image.expects(:tag).with(has_entry(tag: 'my-test')).with(has_entry(tag: 'latest'))
+        mock_docker_image.expects(:push).with(service.send(:registry_credentials), tag: 'latest', force: true)
+        service.push_image('my-test', tag_as_latest: true)
+      end
+
+      it 'does not add the latest tag on top of the one specified when that tag is latest' do
+        mock_docker_image.expects(:tag).never
+        mock_docker_image.expects(:push).never
+        service.push_image('latest', tag_as_latest: true)
+      end
+
+      it 'does not add the latest tag on top of the one specified when latest is false' do
+        mock_docker_image.expects(:tag).never
+        mock_docker_image.expects(:push).never
+        service.push_image('my-test')
+      end
     end
   end
 end

commit 61c7252095e6ac9460a4cb75189a4c270af3a68f

Fix typo
Commit e3594b032196991b3c076545ecd03838ca2d5b8b introduced a
push_to_latest param in push_image() callers, but when defining it in
push_image() it was called "tag_as_latest".

This is clearly a typo, and without having any preference, I choose to
make the param in push_image() "push_to_latest". Please feel free to
change it, just let's make sure it's used with the same name :)

app/models/docker_builder_service.rb
@@ -50,7 +50,7 @@ class DockerBuilderService
   end
   add_method_tracer :build_image
 
-  def push_image(tag, tag_as_latest: false)
+  def push_image(tag, push_to_latest: false)
     build.docker_ref = tag.presence || build.label.try(:parameterize).presence || 'latest'
     build.docker_repo_digest = nil
     output.puts("### Tagging and pushing Docker image to #{project.docker_repo}:#{build.docker_ref}")
@@ -66,7 +66,7 @@ class DockerBuilderService
       end
     end
 
-    push_latest if tag_as_latest && build.docker_ref != 'latest'
+    push_latest if push_as_latest && build.docker_ref != 'latest'
 
     build.save!
     build