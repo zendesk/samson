c89b7f816549031752228f6be0ff6acc1c9cf774
Ian Waters <iwaters@zendesk.com>
2016-05-18 11:05:41 -0700

    some js issues

--- app/assets/javascripts/deploy_groups.js
+++ app/assets/javascripts/deploy_groups.js
@@ -3,17 +3,17 @@
 
 var populateProjectSelectList = function() {
   $( document).ready(function(){
-    var environment = $("#secret_environment_permalink").val()
-    var groups = JSON.parse($('#deployGroupList').attr('imbededData'))
+    var environment = $("#secret_environment_permalink").val();
+    var groups = JSON.parse($('#deployGroupList').attr('imbededData'));
     // re-initialize the select list
     $('#secret_deploy_group_permalink').empty().append('');
     // Create and append the options that match what
     // we stored in the DOM
     _.each(groups, function(group) {
       if (group[environment] !== undefined) {
-        let value = group[environment];
+        var value = group[environment];
         $('#secret_deploy_group_permalink').append(new Option(value, value));
       }
-    })
+    });
   });
 };
