f84ea745a05e65d03b05a025487b86d986af911f
Michael Grosser <michael@grosser.it>
2016-05-27 16:18:05 -0700

    cleanup deploy group config so deploy group index does not crash when a project was deleted

--- plugins/kubernetes/app/decorators/project_decorator.rb
+++ plugins/kubernetes/app/decorators/project_decorator.rb
@@ -1,6 +1,7 @@
 Project.class_eval do
   has_many :kubernetes_releases, class_name: 'Kubernetes::Release'
-  has_many :kubernetes_roles, class_name: 'Kubernetes::Role'
+  has_many :kubernetes_roles, class_name: 'Kubernetes::Role', dependent: :destroy
+  has_many :kubernetes_deploy_group_roles, class_name: 'Kubernetes::DeployGroupRole'
 
   def name_for_label
     name.parameterize('-')
