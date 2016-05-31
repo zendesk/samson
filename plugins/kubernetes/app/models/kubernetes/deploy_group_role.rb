410229c2bb1eaa06ed93371d15be1b0cedd817d0
Michael Grosser <michael@grosser.it>
2016-05-31 16:07:19 -0700

    add seed button to deploy group matrix for easy seedin with default vlaues

--- plugins/kubernetes/app/models/kubernetes/deploy_group_role.rb
+++ plugins/kubernetes/app/models/kubernetes/deploy_group_role.rb
@@ -5,5 +5,52 @@ module Kubernetes
     belongs_to :deploy_group
     belongs_to :kubernetes_role, class_name: 'Kubernetes::Role'
     validates :ram, :cpu, :replicas, presence: true
+
+    # The matrix is a list of deploy group and its roles + deploy-group-roles
+    def self.matrix(stage)
+      project_dg_roles = Kubernetes::DeployGroupRole.where(
+        project_id: stage.project_id,
+        deploy_group_id: stage.deploy_groups.map(&:id)
+      ).to_a
+      roles = stage.project.kubernetes_roles.sort_by(&:name)
+
+      stage.deploy_groups.sort_by(&:natural_order).map do |deploy_group|
+        dg_roles = project_dg_roles.select { |r| r.deploy_group_id == deploy_group.id }
+        role_pairs = roles.map do |role|
+          [role, dg_roles.detect { |r| r.kubernetes_role_id == role.id }]
+        end
+        [deploy_group, role_pairs]
+      end
+    end
+
+    # add deploy group roles for everything missing from the matrix
+    def self.seed!(stage)
+      missing = matrix(stage).each_with_object([]) do |(deploy_group, roles), missing|
+        roles.each do |role, dg_role|
+          missing << [deploy_group, role] unless dg_role
+        end
+      end
+
+      missing.map do |deploy_group, role|
+        next unless raw_template = stage.project.repository.file_content(role.config_file, 'HEAD')
+        objects = Array.wrap(Kubernetes::Util.parse_file(raw_template, role.config_file))
+        next unless deploy = objects.detect { |o| ['Deployment', 'DaemonSet'].include?(o.fetch('kind')) }
+
+        replicas = deploy['spec']['replicas']
+
+        next unless limits = deploy['spec']['template']['spec']['containers'].first['resources'].try(:[], 'limits')
+        cpu = limits['cpu'].to_i / 1000.0 # 250m -> 0.25
+        ram = limits['ram'].to_i # 200Mi -> 200
+
+        create!(
+          project: stage.project,
+          deploy_group: deploy_group,
+          kubernetes_role: role,
+          replicas: replicas,
+          cpu: cpu,
+          ram: ram
+        )
+      end.all?
+    end
   end
 end
--- plugins/kubernetes/test/models/kubernetes/deploy_group_role_test.rb
+++ plugins/kubernetes/test/models/kubernetes/deploy_group_role_test.rb
@@ -3,5 +3,78 @@ require_relative "../../test_helper"
 SingleCov.covered!
 
 describe Kubernetes::DeployGroupRole do
-  let(:doc) { kubernetes_deploy_group_roles(:test_pod_1_app_server) }
+  let(:stage) { stages(:test_staging) }
+
+  describe ".matrix" do
+    it "builds a complete matrix" do
+      Kubernetes::DeployGroupRole.matrix(stage).must_equal(
+        [[
+          stage.deploy_groups.first,
+          [
+            [kubernetes_roles(:app_server), kubernetes_deploy_group_roles(:test_pod100_app_server)],
+            [kubernetes_roles(:resque_worker), kubernetes_deploy_group_roles(:test_pod100_resque_worker)]
+          ]
+        ]]
+      )
+    end
+
+    it "shows missing roles with nil" do
+      kubernetes_deploy_group_roles(:test_pod100_app_server).delete
+      Kubernetes::DeployGroupRole.matrix(stage).must_equal(
+        [[
+          stage.deploy_groups.first,
+          [
+            [kubernetes_roles(:app_server), nil],
+            [kubernetes_roles(:resque_worker), kubernetes_deploy_group_roles(:test_pod100_resque_worker)]
+          ]
+        ]]
+      )
+    end
+  end
+
+  describe "#seed!" do
+    describe "with missing role" do
+      let(:created_role) do
+        Kubernetes::DeployGroupRole.where(
+          deploy_group: deploy_groups(:pod100),
+          project: stage.project,
+          kubernetes_role: kubernetes_roles(:app_server)
+        ).first
+      end
+      let(:content) do
+        {
+          'kind' => 'Deployment',
+          'spec' => {
+            'replicas' => 3,
+            'template' =>
+              {'spec' => {'containers' => [{'resources' => {'limits' => {'ram' => '200Mi', 'cpu' => '250m'}}}]}}
+          }
+        }.to_yaml
+      end
+
+      before do
+        kubernetes_deploy_group_roles(:test_pod100_app_server).delete
+        GitRepository.any_instance.stubs(file_content: content)
+      end
+
+      it "fills in missing roles" do
+        assert Kubernetes::DeployGroupRole.seed!(stage)
+        created_role.cpu.must_equal 0.25
+        created_role.ram.must_equal 200
+        created_role.replicas.must_equal 3
+      end
+
+      it "does nothing when there is no deployment" do
+        Kubernetes::Util.expects(:parse_file).returns([])
+        refute Kubernetes::DeployGroupRole.seed!(stage)
+        refute created_role
+      end
+
+      it "does nothing when there are no limits" do
+        assert content.sub!('resources', 'no_resources')
+        refute Kubernetes::DeployGroupRole.seed!(stage)
+        refute created_role
+      end
+    end
+  end
 end
