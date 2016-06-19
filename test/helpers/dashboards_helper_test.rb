dd3fe5cc044a757bfe3e5939f55357f7277b5a97
Robert Ikeoka <rikeoka@gmail.com>
2016-06-19 14:22:45 -0700

    Move dashboard out of angular -Rails renders the dashboard static table -jQuery DataTables is used to make table fit in display window -Removed angular for dashboard -add tests for helper

--- test/helpers/dashboards_helper_test.rb
+++ test/helpers/dashboards_helper_test.rb
@@ -5,21 +5,57 @@ SingleCov.covered!
 describe DashboardsHelper do
   describe '#project_multi_deploys?' do
     it 'has no warnings for same deploy across all deploy groups' do
-      deploys = Hashie::Mash.new(
-        '1' => { reference: 'v1.0' },
-        '2' => { reference: 'v1.0' },
-        '3' => { reference: 'v1.0' }
-      )
-      project_has_different_deploys?(deploys).must_equal false
+      prep_versions('v1.0')
+      project_has_different_deploys?(@versions['1']).must_equal false
     end
 
     it 'has warnings for different deploy across all deploy groups' do
-      deploys = Hashie::Mash.new(
-        '1' => { reference: 'v1.0' },
-        '2' => { reference: 'v1.1' },
-        '3' => { reference: 'v1.0' }
-      )
-      project_has_different_deploys?(deploys).must_equal true
+      prep_versions('v1.1')
+      project_has_different_deploys?(@versions['1']).must_equal true
     end
+  end
+
+  describe '#project_style' do
+    it 'returns blank style for same versions' do
+      prep_versions('v1.0')
+      project_style(1).must_equal ''
+    end
+
+    it 'returns warning for different versions' do
+      prep_versions('v1.1')
+      project_style(1).must_equal 'class=warning'
+    end
+
+    it 'returns no-deploys for no versions' do
+      prep_versions('v1.0')
+      project_style(0).must_equal 'class=no-deploys'
+    end
+  end
+
+  describe '#display_version' do
+    before do
+      prep_versions('v2.0')
+      Project.first.update_attribute('id', 1)
+      Deploy.where(reference: 'v1.0').first.update_attribute('id', 1)
+    end
+
+    it 'returns blank for empty version' do
+      display_version(0, 1).must_equal ''
+    end
+
+    it 'returns correct link' do
+      display_version(1, 1).must_equal link_to('v2.0', project_deploy_path(1, 1))
+    end
+  end
+
+  def prep_versions(ref)
+    @versions = Hashie::Mash.new(
+      0 => {},
+      1 => {
+        '1' => { reference: ref, id: 1 },
+        '2' => { reference: 'v1.0' },
+        '3' => { reference: 'v1.0' }
+      }
+    )
   end
 end
