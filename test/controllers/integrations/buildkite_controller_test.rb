8da108c97aa449a2577b21ba1bf754bbe9856425
yufei.chen <yufei.chen@redbubble.com>
2016-07-04 15:27:39 +1000

    using block args in block as args are not necessary

--- test/controllers/integrations/buildkite_controller_test.rb
+++ test/controllers/integrations/buildkite_controller_test.rb
@@ -76,7 +76,7 @@ describe Integrations::BuildkiteController do
       Build.any_instance.stubs(:validate_git_reference).returns(true)
       stub_request(:post, "https://api.github.com/repos/bar/foo/releases").
         to_return(status: 200, body: "", headers: {})
-      Samson::Hooks.callback :release_params do |project, build_param|
+      Samson::Hooks.callback :release_params do |_|
         [[:number, 9]]
       end
     end
