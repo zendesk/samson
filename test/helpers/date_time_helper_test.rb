1f8485d2ad07652827666e4501eb0b39a1beb1b5
Adam Malcontenti-Wilson <amalcontenti-wilson@zendesk.com>
2016-07-15 16:50:24 +1000

    Placate the cop

--- test/helpers/date_time_helper_test.rb
+++ test/helpers/date_time_helper_test.rb
@@ -16,29 +16,41 @@ describe DateTimeHelper do
     let(:current_user) { users(:admin) }
 
     it "formats time in the uesrs default preference" do
-      render_time(ts, nil).must_equal "<span data-time=\"1461001570000\" class=\"mouseover\">Mon, 18 Apr 2016 17:46:10 +0000</span>"
+      render_time(ts, nil).must_equal(
+        "<span data-time=\"1461001570000\" class=\"mouseover\">Mon, 18 Apr 2016 17:46:10 +0000</span>"
+      )
     end
 
     it "formats time in utc" do
-      render_time(ts, 'utc').must_equal "<time datetime=\"2016-04-18 17:46:10 UTC\">2016-04-18 17:46:10 UTC</time>"
+      render_time(ts, 'utc').must_equal(
+        "<time datetime=\"2016-04-18 17:46:10 UTC\">2016-04-18 17:46:10 UTC</time>"
+      )
     end
 
     it "formats time in utc if no timezone cookie is set" do
-      render_time(ts, 'local').must_equal "<time datetime=\"2016-04-18 17:46:10 UTC\">2016-04-18 17:46:10 UTC</time>"
+      render_time(ts, 'local').must_equal(
+        "<time datetime=\"2016-04-18 17:46:10 UTC\">2016-04-18 17:46:10 UTC</time>"
+      )
     end
 
     it "formats local time in America/Los_Angeles via cookie set by JS" do
       cookies[:timezone] = 'America/Los_Angeles'
-      render_time(ts, 'local').must_equal "<time datetime=\"2016-04-18 10:46:10 -0700\">2016-04-18 10:46:10 -0700</time>"
+      render_time(ts, 'local').must_equal(
+        "<time datetime=\"2016-04-18 10:46:10 -0700\">2016-04-18 10:46:10 -0700</time>"
+      )
     end
 
     it "formats local time in America/New_York via cookie set by JS" do
       cookies[:timezone] = 'America/New_York'
-      render_time(ts, 'local').must_equal "<time datetime=\"2016-04-18 13:46:10 -0400\">2016-04-18 13:46:10 -0400</time>"
+      render_time(ts, 'local').must_equal(
+        "<time datetime=\"2016-04-18 13:46:10 -0400\">2016-04-18 13:46:10 -0400</time>"
+      )
     end
 
     it "formats time relative" do
-      render_time(ts, 'foobar').must_equal "<span data-time=\"1461001570000\" class=\"mouseover\">Mon, 18 Apr 2016 17:46:10 +0000</span>"
+      render_time(ts, 'foobar').must_equal(
+        "<span data-time=\"1461001570000\" class=\"mouseover\">Mon, 18 Apr 2016 17:46:10 +0000</span>"
+      )
     end
   end
 end
