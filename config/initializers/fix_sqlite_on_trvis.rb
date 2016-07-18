2826b4c697e943d014175ebab60b9cbb5df3049e
Michael Grosser <michael@grosser.it>
2016-07-17 22:00:15 -0700

    sqlite has issues auto-loading the schema on rails 5

---
+++ config/initializers/fix_sqlite_on_trvis.rb
@@ -0,0 +1,7 @@
+# sqlite on travis always thinks it needs to be migrated
+if ENV['CI'] && ENV['DATABASE_URL'].to_s.include?('sqlite')
+  class << ActiveRecord::Migration
+    def check_pending!
+    end
+  end
+end
