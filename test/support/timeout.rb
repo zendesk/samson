bac3ba213d18469b2323b9222f7d188cc58d5fd2
Michael Grosser <michael@grosser.it>
2016-07-27 16:45:43 -0700

    make timeout errors easier to debug
    also makes it less likely to be caught by and unrelated rescue

--- test/support/timeout.rb
+++ test/support/timeout.rb
@@ -1,11 +1,17 @@
 # tests sometimes hang locally or on ci and with this we can actually debug the cause instead of just hanging forever
 module TimeoutEveryTestCase
-  def capture_exceptions(*args, &block)
+  class TestCaseTimeout < StandardError
+    def message
+      "Test took too long to finish, aborting. To use a debugger: disable timeouts in #{__FILE__}."
+    end
+  end
+
+  def capture_exceptions(*, &block)
     super do
       rescued = false
       begin
-        Timeout.timeout(5, &block)
-      rescue Timeout::Error => e
+        Timeout.timeout(5, TestCaseTimeout, &block)
+      rescue TestCaseTimeout => e
         raise e if rescued
         rescued = true
         retry
