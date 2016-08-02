a837fad574c617a3823acdbc8df5f9050ecac6cc
Michael Grosser <michael@grosser.it>
2016-08-02 11:09:12 -0700

    show release docs after the deploy has generated them for debugging

--- plugins/kubernetes/app/decorators/deploy_decorator.rb
+++ plugins/kubernetes/app/decorators/deploy_decorator.rb
@@ -1,4 +1,6 @@
 Deploy.class_eval do
+  has_one :kubernetes_release, class_name: 'Kubernetes::Release'
+
   before_create :copy_kubernetes_from_stage
 
   private
--- plugins/kubernetes/lib/samson_kubernetes/samson_plugin.rb
+++ plugins/kubernetes/lib/samson_kubernetes/samson_plugin.rb
@@ -12,6 +12,8 @@ Samson::Hooks.view :project_tabs_view, 'samson_kubernetes/project_tab'
 Samson::Hooks.view :admin_menu, 'samson_kubernetes/admin_menu'
 Samson::Hooks.view :stage_form, "samson_kubernetes/stage_form"
 Samson::Hooks.view :stage_show, "samson_kubernetes/stage_show"
+Samson::Hooks.view :deploy_tab_nav, "samson_kubernetes/deploy_tab_nav"
+Samson::Hooks.view :deploy_tab_body, "samson_kubernetes/deploy_tab_body"
 Samson::Hooks.view :deploy_group_show, "samson_kubernetes/deploy_group_show"
 Samson::Hooks.view :deploy_group_form, "samson_kubernetes/deploy_group_form"
 Samson::Hooks.view :deploy_group_table_header, "samson_kubernetes/deploy_group_table_header"
