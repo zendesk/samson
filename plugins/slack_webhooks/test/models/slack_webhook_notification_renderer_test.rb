6a8c4b83f659cd72335b9b709fe8498c7571f9e0
Ben Straub <bstraub@zendesk.com>
2016-08-16 10:35:22 -0700

    Cover emoji with tests

--- plugins/slack_webhooks/test/models/slack_webhook_notification_renderer_test.rb
+++ plugins/slack_webhooks/test/models/slack_webhook_notification_renderer_test.rb
@@ -4,23 +4,21 @@ require_relative '../test_helper'
 SingleCov.covered!
 
 describe SlackWebhookNotificationRenderer do
+  let(:subject) { 'Deploy starting' }
+  let(:changeset) do
+    stub "changeset",
+      commits: stub("commits", count: 3),
+      github_url: "https://github.com/url",
+      pull_requests: stub("pull_requests", count: 2),
+      author_names: ['author1', 'author2']
+  end
+
   it "renders a nicely formatted notification" do
-    changeset = stub("changeset")
     deploy = stub("deploy",
       short_reference: "xyz",
       changeset: changeset,
       pending?: true,
       url: "http://sams.on/url")
-
-    changeset.stubs(:commits).returns(stub("commits", count: 3))
-    changeset.stubs(:github_url).returns("https://github.com/url")
-    changeset.stubs(:pull_requests).returns(stub("pull_requests", count: 2))
-
-    author1 = "author1"
-    author2 = "author2"
-    changeset.stubs(:author_names).returns([author1, author2])
-
-    subject = "Deploy starting"
 
     result = SlackWebhookNotificationRenderer.render(deploy, subject)
 
@@ -28,5 +26,59 @@ describe SlackWebhookNotificationRenderer do
       :stopwatch: *Deploy starting* (<http://sams.on/url|view the deploy>)
       _<https://github.com/url|3 commits> and 2 pull requests by author1 and author2._
     RESULT
+  end
+
+  it 'uses a truck emoji for a running deploy' do
+    deploy = stub("deploy",
+      short_reference: "xyz",
+      changeset: changeset,
+      pending?: false,
+      running?: true,
+      url: "http://sams.on/url")
+
+    result = SlackWebhookNotificationRenderer.render(deploy, subject)
+    result.must_include ':truck::dash:'
+  end
+
+  it 'uses an X emoji for an errored deploy' do
+    deploy = stub("deploy",
+      short_reference: "xyz",
+      changeset: changeset,
+      pending?: false,
+      running?: false,
+      errored?: true,
+      url: "http://sams.on/url")
+
+    result = SlackWebhookNotificationRenderer.render(deploy, subject)
+    result.must_include ':x:'
+  end
+
+  it 'uses an X emoji for a failed deploy' do
+    deploy = stub("deploy",
+      short_reference: "xyz",
+      changeset: changeset,
+      pending?: false,
+      running?: false,
+      errored?: false,
+      failed?: true,
+      url: "http://sams.on/url")
+
+    result = SlackWebhookNotificationRenderer.render(deploy, subject)
+    result.must_include ':x:'
+  end
+
+  it 'uses a checkmark emoji for a successful deploy' do
+    deploy = stub("deploy",
+      short_reference: "xyz",
+      changeset: changeset,
+      pending?: false,
+      running?: false,
+      errored?: false,
+      failed?: false,
+      succeeded?: true,
+      url: "http://sams.on/url")
+
+    result = SlackWebhookNotificationRenderer.render(deploy, subject)
+    result.must_include ':white_check_mark:'
   end
 end
