a1cbe152432f9458d43895707cb3c311a73a5d91
Michael Grosser <michael@grosser.it>
2016-08-19 15:36:27 -0700

    show current versions first

--- app/controllers/versions_controller.rb
+++ app/controllers/versions_controller.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 class VersionsController < ApplicationController
   def index
-    @versions = PaperTrail::Version.where(item_id: params.require(:item_id), item_type: params.require(:item_type))
+    @versions = PaperTrail::Version.where(
+      item_id: params.require(:item_id),
+      item_type: params.require(:item_type)
+    ).order('created_at desc')
   end
 end
