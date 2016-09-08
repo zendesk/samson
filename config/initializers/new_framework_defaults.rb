bfa03b145effd5132a3e00923e715d449e0cc5ab
Michael Grosser <michael@grosser.it>
2016-09-07 21:01:35 -0700

    remove deprecated features

--- config/initializers/new_framework_defaults.rb
+++ config/initializers/new_framework_defaults.rb
@@ -16,7 +16,7 @@ Rails.application.config.action_controller.forgery_protection_origin_check = tru
 ActiveSupport.to_time_preserves_timezone = true
 
 # Require `belongs_to` associations by default. Previous versions had false.
-# Rails.application.config.active_record.belongs_to_required_by_default = true
+Rails.application.config.active_record.belongs_to_required_by_default = true
 
 # Do not halt callback chains when a callback returns false. Previous versions had true.
 ActiveSupport.halt_callback_chains_on_return_false = false
