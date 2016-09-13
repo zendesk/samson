5a0390adf2ac5258bc93fddd9793c567193f9c5d
Travers McInerney <travers@mcinerney.me>
2016-09-13 16:24:22 -0700

    Add Rails version to migration

--- db/migrate/20160827172550_add_deploy_to_new_pods_to_projects.rb
+++ db/migrate/20160827172550_add_deploy_to_new_pods_to_projects.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
-class AddDeployToNewPodsToProjects < ActiveRecord::Migration
+class AddDeployToNewPodsToProjects < ActiveRecord::Migration[4.2]
   def change
     add_column :projects, :include_new_deploy_groups, :boolean, default: false, null: false
   end
--- db/migrate/20160831151133_add_is_template_to_stage.rb
+++ db/migrate/20160831151133_add_is_template_to_stage.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
-class AddIsTemplateToStage < ActiveRecord::Migration
+class AddIsTemplateToStage < ActiveRecord::Migration[4.2]
   def change
     add_column :stages, :is_template, :boolean, default: false, null: false
   end
