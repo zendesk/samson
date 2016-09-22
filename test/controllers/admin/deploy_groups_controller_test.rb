37d1cac66c6c2821586f06338b48c6b9bf6f0f24
Jason Stillwell <jstillwell@zendesk.com>
2016-09-22 14:17:02 -0700

    Add tests for the new code.

--- test/controllers/admin/deploy_groups_controller_test.rb
+++ test/controllers/admin/deploy_groups_controller_test.rb
@@ -207,5 +207,31 @@ describe Admin::DeployGroupsController do
         template_stage.next_stage_ids.must_equal([stage.id])
       end
     end
+
+    describe "#create_all_stages_verify" do
+      let(:env) { environments(:staging) }
+      let(:deploy_group) { DeployGroup.create!(name: 'Pod 101', environment: env) }
+      let(:template_stage) { stages(:test_staging) }
+
+      it "finds stages to create" do
+        get :create_all_stages_verify, params: {id: deploy_group}
+
+        refute @controller.instance_variable_get(:@stages_to_create).empty?
+      end
+
+      it "finds precreated stages" do
+
+        # clone the stage
+        stage = Stage.build_clone(template_stage)
+        stage.deploy_groups << deploy_group
+        stage.name = "foo"
+        stage.is_template = false
+        stage.save!
+
+        get :create_all_stages_verify, params: {id: deploy_group}
+
+        refute @controller.instance_variable_get(:@stages_preexisting).empty?
+      end
+    end
   end
 end
