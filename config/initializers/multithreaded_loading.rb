dc0dc19b611893b806a29203cc8055603c4aeb35
Michael Grosser <michael@grosser.it>
2016-09-22 11:25:01 -0700

    solve eager load vs threads issue
    rails 5 thinks it needs to lock while doing autoload ...
    then somehow runs into deadlocks because some threads sleep ...
    we could spend a bunch of time debugging that or just go back to the way rails 4 handled it
    which worked fine for many years ...
    
    https://github.com/rails/rails/issues/24028

---
+++ config/initializers/multithreaded_loading.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+# https://github.com/rails/rails/issues/24028
+# can be removed if a deploy on osx in development mode without eager load works
+ActiveSupport::Dependencies::Interlock.class_eval do
+  def loading
+    yield
+  end
+end
