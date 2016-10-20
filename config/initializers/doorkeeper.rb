f277bd128a08e89271a15a7998e8ad612b676ac7
Adam Panzer <apanzer@zendesk.com>
2016-10-20 12:20:50 -0700

    Add comment

--- config/initializers/doorkeeper.rb
+++ config/initializers/doorkeeper.rb
@@ -1,4 +1,13 @@
 # frozen_string_literal: true
+
+# Doorkeeper (https://github.com/doorkeeper-gem/doorkeeper) is the engine
+# that secures /api. It uses OAuth and the OAuth2 gem can be used to interact
+# with the samson api.
+
+
+
+
+
 # do not auto initialize all the models
 # will make rails runner fail because the models were preloaded
 module Doorkeeper
