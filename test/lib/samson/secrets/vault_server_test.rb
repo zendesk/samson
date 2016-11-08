27144dfa4048535413b6761fb3758cbfcbfec067
Michael Grosser <michael@grosser.it>
2016-11-08 15:19:53 -0800

    validate vault connection works when saving a server

--- test/lib/samson/secrets/vault_server_test.rb
+++ test/lib/samson/secrets/vault_server_test.rb
@@ -7,6 +7,11 @@ describe Samson::Secrets::VaultServer do
   describe "validations" do
     let(:server) { Samson::Secrets::VaultServer.new(name: 'abc', address: 'http://vault-land.com', token: "TOKEN") }
 
+    before do
+      stub_request(:get, "http://vault-land.com/v1/secret/apps/?list=true").
+        to_return(headers: {content_type: 'application/json'}, body: {data: {keys: ['abc']}}.to_json)
+    end
+
     it "is valid" do
       assert_valid server
     end
@@ -30,6 +35,17 @@ describe Samson::Secrets::VaultServer do
     it "is invalid with duplicate name" do
       server.save!
       refute_valid server.dup
+    end
+
+    it "is invalid when it cannot connect to vault" do
+      stub_request(:get, "http://vault-land.com/v1/secret/apps/?list=true").to_timeout
+      refute_valid server
+    end
+
+    it "is invalid when vault connection fails" do
+      stub_request(:get, "http://vault-land.com/v1/secret/apps/?list=true").
+        to_raise(Vault::HTTPError.new("address", stub(code: '200')))
+      refute_valid server
     end
   end
 end
