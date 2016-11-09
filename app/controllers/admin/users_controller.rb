b9ac5aff22d0579bebde5abaac04308a3cd0996e
Robert Ikeoka <rikeoka@zendesk.com>
2016-11-09 09:14:58 -0800

    Cleanup args usage in AuditLog concerm

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -5,7 +5,7 @@ class Admin::UsersController < ApplicationController
   before_action :authorize_admin!
   before_action :authorize_super_admin!, only: [:update, :destroy]
 
-  include AuditLog
+  include AuditLog::UserLog
 
   def index
     @users = User.search_by_criteria(params)
@@ -28,7 +28,7 @@ class Admin::UsersController < ApplicationController
       Rails.logger.info(
         "#{current_user.name_and_email} changed the role of #{user.name_and_email} to #{user.role.name}"
       )
-      Audit.info(current_user, 'updated', user, user.role)
+      #Audit.info(current_user, 'updated', user, user.role)
       head :ok
     else
       head :bad_request
@@ -38,7 +38,7 @@ class Admin::UsersController < ApplicationController
   def destroy
     user.soft_delete!
     Rails.logger.info("#{current_user.name_and_email} just deleted #{user.name_and_email})")
-    Audit.warn(current_user, 'destroyed', user)
+    #Audit.warn(current_user, 'destroyed', user)
     redirect_to admin_users_path
   end
 
--- app/controllers/concerns/audit_log.rb
+++ app/controllers/concerns/audit_log.rb
@@ -8,23 +8,23 @@ module AuditLog
     VALID_METHODS = [:info, :warn, :debug, :error]
 
     def self.log(user, action, *args)
-      logger(:info, user, action, args)
+      logger(:info, user, action, *args)
     end
 
     def self.info(user, action, *args)
-      logger(:info, user, action, args)
+      logger(:info, user, action, *args)
     end
 
     def self.warn(user, action, *args)
-      logger(:warn, user, action, args)
+      logger(:warn, user, action, *args)
     end
 
     def self.debug(user, action, *args)
-      logger(:debug, user, action, args)
+      logger(:debug, user, action, *args)
     end
 
     def self.error(user, action, *args)
-      logger(:error, user, action, args)
+      logger(:error, user, action, *args)
     end
 
     def self.logger(level, user, action, *args)
@@ -33,7 +33,7 @@ module AuditLog
         message[:user] = AuditPresenter.present(user)
         message[:time] = Time.now
         message[:action] = action
-        args[0].each_with_index do |arg, i|
+        args.each_with_index do |arg, i|
           message['subject' + i.to_s] = AuditPresenter.present(arg)
         end
         AUDIT_LOGGER.send(level, message)
