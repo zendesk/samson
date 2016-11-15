d57c2740390f9c048a74a07fad79077515d60d3e
Robert Ikeoka <rikeoka@zendesk.com>
2016-11-15 09:52:00 -0800

    Use PaperTrail_whodunnit_user #1485

--- app/models/concerns/audit_log.rb
+++ app/models/concerns/audit_log.rb
@@ -2,16 +2,16 @@
 module AuditLog
   def self.included(base)
     base.after_create_commit do |object|
-      SamsonAuditLog::Audit.log(:info, PaperTrail.whodunnit, "created #{object.class.name}", object)
+      SamsonAuditLog::Audit.log(:info, PaperTrail.whodunnit_user, "created #{object.class.name}", object)
     end
     base.after_destroy_commit do |object|
-      SamsonAuditLog::Audit.log(:info, PaperTrail.whodunnit, "deleted #{object.class.name}", object)
+      SamsonAuditLog::Audit.log(:info, PaperTrail.whodunnit_user, "deleted #{object.class.name}", object)
     end
     base.after_update_commit do |object|
       if object.try(:deleted_at)
-        SamsonAuditLog::Audit.log(:info, PaperTrail.whodunnit, "deleted #{object.class.name}", object)
+        SamsonAuditLog::Audit.log(:info, PaperTrail.whodunnit_user, "deleted #{object.class.name}", object)
       else
-        SamsonAuditLog::Audit.log(:info, PaperTrail.whodunnit, "updated #{object.class.name}", object)
+        SamsonAuditLog::Audit.log(:info, PaperTrail.whodunnit_user, "updated #{object.class.name}", object)
       end
     end
   end
--- test/models/concerns/audit_log_test.rb
+++ test/models/concerns/audit_log_test.rb
@@ -5,22 +5,22 @@ SingleCov.covered!
 
 describe AuditLog do
   it 'calls SamsonAuditLog::Audit.log on create action' do
-    SamsonAuditLog::Audit.expects(:log).with(:info, PaperTrail.whodunnit, "created User", instance_of(User)).once
+    SamsonAuditLog::Audit.expects(:log).with(:info, PaperTrail.whodunnit_user, 'created User', instance_of(User)).once
     User.create(name: 'foo')
   end
 
   it 'calls SamsonAuditLog::Audit.log on destroy action' do
-    SamsonAuditLog::Audit.expects(:log).with(:info, PaperTrail.whodunnit, "deleted User", instance_of(User)).once
+    SamsonAuditLog::Audit.expects(:log).with(:info, PaperTrail.whodunnit_user, 'deleted User', instance_of(User)).once
     User.last.destroy
   end
 
   it 'calls SamsonAuditLog::Audit.log on soft_deletion action' do
-    SamsonAuditLog::Audit.expects(:log).with(:info, PaperTrail.whodunnit, "deleted User", instance_of(User)).once
+    SamsonAuditLog::Audit.expects(:log).with(:info, PaperTrail.whodunnit_user, 'deleted User', instance_of(User)).once
     User.last.soft_delete
   end
 
   it 'calls SamsonAuditLog::Audit.log on update action' do
-    SamsonAuditLog::Audit.expects(:log).with(:info, PaperTrail.whodunnit, "updated User", instance_of(User)).once
+    SamsonAuditLog::Audit.expects(:log).with(:info, PaperTrail.whodunnit_user, 'updated User', instance_of(User)).once
     User.last.update_attribute(:role_id, 2)
   end
 end
