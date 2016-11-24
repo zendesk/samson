7f7c31efe5817ac0f41360ccd88bd2010c871279
Nitin Chadha <nchadha@zendesk.com>
2016-11-24 11:06:55 +0800

    resolve db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161118215353) do
+ActiveRecord::Schema.define(version: 20161124215353) do
 
   create_table "builds", force: :cascade do |t|
     t.integer  "project_id",                                       null: false
