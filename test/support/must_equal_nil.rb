8c1c5c42c3fa5fa026c46bbc8a899650681b3087
Michael Grosser <michael@grosser.it>
2016-12-13 13:37:04 -0800

    silence minitest deprecation
    minitest/issues/666
    Use assert_nil if expecting nil from lib/minitest/spec.rb:23:in `must_equal'. This will fail in MT6.

---
+++ test/support/must_equal_nil.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+# https://github.com/seattlerb/minitest/issues/666
+Object.prepend(Module.new do
+  def must_equal(*args)
+    if args.first.nil?
+      must_be_nil(*args[1..-1])
+    else
+      super
+    end
+  end
+end)
