5ab6898cc3c4ff891be13839c79a1f3b53bdeb01
Michael Grosser <michael@grosser.it>
2016-12-13 07:59:45 -0800

    support UNSTABLE for jenkins jobs
    https://zendesk.airbrake.io/projects/95346/groups/1786680229285535838/notices/1839942896368086033

--- plugins/jenkins/app/helpers/jenkins_helper.rb
+++ plugins/jenkins/app/helpers/jenkins_helper.rb
@@ -1,24 +1,26 @@
 # frozen_string_literal: true
 module JenkinsHelper
+  JENKINS_MAPPING = {
+    "SUCCESS"  => "success",
+    "FAILURE"  => "danger",
+    "CANCELED" => "warning",
+    "ABORTED"  => "warning",
+    "UNSTABLE" => "warning",
+    "STARTUP_ERROR" => "warning",
+    nil        => "info"
+  }.freeze
+
+  JENKINS_RESULT = {
+    "SUCCESS"  => "has Passed.",
+    "FAILURE"  => "has Failed.",
+    "CANCELED" => "was canceled, Please check Jenkins job for more details.",
+    "STARTUP_ERROR" => "failed to start, Please check Jenkins job to see what went wrong.",
+    "ABORTED" => "was aborted, Please go to Jenkins job to start it manually.",
+    "UNSTABLE" => "was unstable.",
+    nil => "is running. This can take a few minutes to finish, Please reload this page to check latest status."
+  }.freeze
+
   def jenkins_status_panel(deploy, jenkins_job)
-    mapping = {
-      "SUCCESS"  => "success",
-      "FAILURE"  => "danger",
-      "CANCELED" => "warning",
-      "ABORTED"  => "warning",
-      "STARTUP_ERROR" => "warning",
-      nil        => "info"
-    }
-
-    result = {
-      "SUCCESS"  => "has Passed.",
-      "FAILURE"  => "has Failed.",
-      "CANCELED" => "was canceled, Please check Jenkins job for more details.",
-      "STARTUP_ERROR" => "failed to start, Please check Jenkins job to see what went wrong.",
-      "ABORTED" => "was aborted, Please go to Jenkins job to start it manually.",
-      nil => "is running. This can take a few minutes to finish, Please reload this page to check latest status."
-    }
-
     jenkins_job_status = jenkins_job.status
     jenkins_job_url = jenkins_job.url
     unless jenkins_job_status
@@ -33,8 +35,8 @@ module JenkinsHelper
       status = 'warning'
       status_message = jenkins_job_status
     else
-      status = mapping.fetch(jenkins_job_status)
-      status_message = result.fetch(jenkins_job_status)
+      status = JENKINS_MAPPING.fetch(jenkins_job_status)
+      status_message = JENKINS_RESULT.fetch(jenkins_job_status)
     end
 
     content = "Jenkins build #{jenkins_job.name} for #{deploy.stage.name} #{status_message}"
