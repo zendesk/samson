6dd74ed5a268ab6e5a53435cb83d8cb1a34a3d53
Michael Grosser <michael@grosser.it>
2017-01-06 13:26:06 -0800

    make visible settable in vault
    rails forms send 'true' or 'false' ... need to cast that
    other fields are fine as string since they are ids

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170102212707) do
+ActiveRecord::Schema.define(version: 20170106212807) do
 
   create_table "builds", force: :cascade do |t|
     t.integer  "project_id",                                       null: false
