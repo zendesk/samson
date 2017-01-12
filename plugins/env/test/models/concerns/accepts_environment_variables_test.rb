f774bc028b82407f73261fb4ea4d7cf9676a446a
Michael Grosser <michael@grosser.it>
2017-01-12 08:25:13 -0800

    fix missing tests

--- plugins/env/test/models/concerns/accepts_environment_variables_test.rb
+++ plugins/env/test/models/concerns/accepts_environment_variables_test.rb
@@ -1,7 +1,20 @@
 # frozen_string_literal: true
 require_relative "../../test_helper"
 
-SingleCov.covered! uncovered: 3
+SingleCov.covered!
 
 describe AcceptsEnvironmentVariables do
+  it "adds environment_variables" do
+    EnvironmentVariableGroup.new.environment_variables.must_equal []
+  end
+
+  it "accepts attributes for environment_variables" do
+    group = EnvironmentVariableGroup.new(environment_variables_attributes: {0 => {name: 'X'}})
+    group.environment_variables.map(&:name).must_equal ['X']
+  end
+
+  it "does not accept attributes for environment_variables without name" do
+    group = EnvironmentVariableGroup.new(environment_variables_attributes: {0 => {name: ''}})
+    group.environment_variables.map(&:name).must_equal []
+  end
 end
--- test/models/macro_test.rb
+++ test/models/macro_test.rb
@@ -5,35 +5,4 @@ SingleCov.covered!
 
 describe Macro do
   subject { macros(:test) }
-
-  describe '#command' do
-    it 'joins all commands based on position' do
-      command = Command.create!(command: 'test')
-
-      subject.command_ids = [command.id, commands(:echo).id]
-      subject.save!
-      subject.reload
-
-      subject.script.must_equal("test\n#{commands(:echo).command}")
-    end
-
-    it "is empty without commands" do
-      subject.command_associations.clear
-      subject.script.must_equal('')
-    end
-  end
-
-  describe "#command=" do
-    it "adds a new comand" do
-      subject.command = "HELLOOOO"
-      subject.save!
-      subject.reload.commands.map(&:command).must_equal ["echo hello", "HELLOOOO"]
-    end
-
-    it "does not add a empty command" do
-      subject.command = "   "
-      subject.save!
-      subject.reload.commands.map(&:command).must_equal ["echo hello"]
-    end
-  end
 end
