6c51cc94591d7569dbb3d84bbf7e3087def60d3a
Michael Grosser <michael@grosser.it>
2017-02-15 19:14:28 -0800

    remove direct job execution

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -4,31 +4,11 @@ class JobsController < ApplicationController
 
   skip_before_action :require_project, only: [:enabled]
 
-  before_action :authorize_project_admin!, only: [:new, :create]
-  before_action :authorize_project_deployer!, only: [:new, :create, :destroy]
+  before_action :authorize_project_deployer!, only: [:destroy]
   before_action :find_job, only: [:show, :destroy]
 
   def index
     @jobs = @project.jobs.non_deploy.page(params[:page])
-  end
-
-  def new
-    @job = Job.new
-  end
-
-  def create
-    @job = current_project.jobs.build(
-      user: current_user,
-      command: command,
-      commit: job_params[:commit]
-    )
-
-    if @job.save
-      JobExecution.start_job(JobExecution.new(@job.commit, @job))
-      redirect_to [@project, @job]
-    else
-      render :new
-    end
   end
 
   def show
@@ -63,27 +43,6 @@ class JobsController < ApplicationController
   end
 
   private
-
-  def job_params
-    params.require(:job).permit(:commit, :command)
-  end
-
-  def command_params
-    if commands = params[:commands]
-      commands.permit(ids: [])
-    else
-      {ids: []}
-    end
-  end
-
-  def command
-    command_ids = command_params[:ids].select(&:present?).map(&:to_i)
-    commands = Command.find(command_ids).sort_by { |c| command_ids.index(c.id) }.map(&:command)
-    if command = job_params[:command].strip.presence
-      commands << command
-    end
-    commands.join("\n")
-  end
 
   def find_job
     @job = current_project.jobs.find(params[:id])
