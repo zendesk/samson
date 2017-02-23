faf4ca8e4fe265b71c8a91e2717064f897df2611
Jon Moter <jmoter@zendesk.com>
2017-02-23 13:40:27 -0800

    allow exporting of projects to a CSV file

--- app/serializers/project_serializer.rb
+++ app/serializers/project_serializer.rb
@@ -1,8 +1,16 @@
 # frozen_string_literal: true
 class ProjectSerializer < ActiveModel::Serializer
-  attributes :id, :name, :url, :permalink, :repository_url
+  attributes :id, :name, :url, :permalink, :repository_url, :owner, :created_at
+
+  def self.csv_header
+    ['ID', 'Name', 'URL', 'Permalink', 'Repository URL', 'Owner', 'Created At']
+  end
 
   def url
     Rails.application.routes.url_helpers.project_url(object)
+  end
+
+  def csv_line
+    to_h.values
   end
 end
--- test/serializers/project_serializer_test.rb
+++ test/serializers/project_serializer_test.rb
@@ -5,9 +5,26 @@ SingleCov.covered!
 
 describe ProjectSerializer do
   let(:project) { projects(:test) }
-  let(:parsed) { JSON.parse(ProjectSerializer.new(project).to_json) }
+  let(:serializer) { ProjectSerializer.new(project) }
+  let(:parsed) { JSON.parse(serializer.to_json) }
 
   it 'serializes url' do
     parsed['url'].must_equal "http://www.test-url.com/projects/foo"
+  end
+
+  describe '.csv_header' do
+    it 'returns an array of strings' do
+      header = ProjectSerializer.csv_header
+      header.must_be_kind_of Array
+      header.first.must_equal 'ID'
+    end
+  end
+
+  describe '#csv_line' do
+    it 'returns a CSV line' do
+      line = serializer.csv_line
+      line.must_be_kind_of Array
+      line.first.must_equal project.id
+    end
   end
 end
