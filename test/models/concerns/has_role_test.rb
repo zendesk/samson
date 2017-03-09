67ce0b26610710afd8436270b314ed14e46304dc
Tam Grosser <tamgrosser@gmail.com>
2017-03-08 17:37:21 -0800

    test for has_role

--- test/models/concerns/has_role_test.rb
+++ test/models/concerns/has_role_test.rb
@@ -1,7 +1,30 @@
 # frozen_string_literal: true
 require_relative '../../test_helper'
 
-SingleCov.covered! uncovered: 2
+SingleCov.covered!
 
 describe HasRole do
+  describe "#role" do
+    it "finds the role" do
+      User.new(role_id: Role::ADMIN.id).role.must_equal Role::ADMIN
+    end
+
+    it "returns viewer when nil" do
+      User.new.role.must_equal Role::VIEWER
+    end
+  end
+
+  describe "dynamic role method" do
+    it "is true when role_id is greater than requested" do
+      assert User.new(role_id: Role::SUPER_ADMIN.id).admin?
+    end
+
+    it "is true when role_id is equal" do
+      assert User.new(role_id: Role::ADMIN.id).admin?
+    end
+
+    it "is false when role_id is lower than requested" do
+      refute User.new(role_id: Role::VIEWER.id).admin?
+    end
+  end
 end
