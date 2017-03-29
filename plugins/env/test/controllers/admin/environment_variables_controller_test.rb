92272d7892a3dac963d8f19042c08605582dfa5b
Michael Grosser <michael@grosser.it>
2017-03-28 21:43:42 -0700

    add environment variable view

---
+++ plugins/env/test/controllers/admin/environment_variables_controller_test.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+require_relative "../../test_helper"
+
+SingleCov.covered!
+
+describe Admin::EnvironmentVariablesController do
+  let!(:env_var) { EnvironmentVariable.create!(name: 'FOO', value: 'bar', parent: projects(:test)) }
+
+  as_a_viewer do
+    unauthorized :delete, :destroy, id: 1
+
+    describe "#index" do
+      before { EnvironmentVariable.create!(name: 'BAR', value: 'baz', parent: projects(:test)) }
+
+      it "renders" do
+        get :index
+        assert_response :success
+        assigns[:environment_variables].size.must_equal 2
+      end
+
+      it "can filter by name" do
+        get :index, params: {search: {name: 'FOO'}}
+        assert_response :success
+        assigns[:environment_variables].size.must_equal 1
+      end
+
+      it "can filter by value" do
+        get :index, params: {search: {value: 'bar'}}
+        assert_response :success
+        assigns[:environment_variables].size.must_equal 1
+      end
+    end
+  end
+
+  as_a_admin do
+    describe "#destroy" do
+      it "destroy" do
+        assert_difference "EnvironmentVariable.count", -1 do
+          delete :destroy, params: {id: env_var.id}
+        end
+        assert_response :success
+      end
+    end
+  end
+end
