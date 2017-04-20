182b396e9f6fafccaab4805b07127451b8a21856
Michael Grosser <michael@grosser.it>
2017-04-19 18:44:07 -0700

    everyone can see commands, so everyone should get links to them
    fixes bug introduced by https://github.com/zendesk/samson/pull/1780

--- test/helpers/stages_helper_test.rb
+++ test/helpers/stages_helper_test.rb
@@ -8,41 +8,18 @@ describe StagesHelper do
   include ApplicationHelper
 
   describe "#edit_command_link" do
-    describe "as admin" do
-      let(:current_user) { users(:admin) }
+    let(:current_user) { users(:admin) }
 
-      it "links to global edit" do
-        command = commands(:global)
-        html = edit_command_link(command)
-        html.must_equal "<a title=\"Edit global command\" class=\"edit-command glyphicon glyphicon-globe no-hover\" href=\"/admin/commands/#{command.id}\"></a>"
-      end
-
-      it "links to local edit" do
-        command = commands(:echo)
-        html = edit_command_link(command)
-        html.must_equal "<a title=\"Edit in admin UI\" class=\"edit-command glyphicon glyphicon-edit no-hover\" href=\"/admin/commands/#{command.id}\"></a>"
-      end
+    it "links to global edit" do
+      command = commands(:global)
+      html = edit_command_link(command)
+      html.must_equal "<a title=\"Edit global command\" class=\"edit-command glyphicon glyphicon-globe no-hover\" href=\"/admin/commands/#{command.id}\"></a>"
     end
 
-    describe "as other user" do
-      let(:current_user) { users(:deployer) }
-
-      it "explains global commands" do
-        html = edit_command_link(commands(:global))
-        html.must_equal "<a title=\"Global command, can only be edited via Admin UI\" class=\"edit-command glyphicon glyphicon-globe no-hover\" href=\"#\"></a>"
-      end
-
-      it "does not show local edit" do
-        edit_command_link(commands(:echo)).must_be_nil
-      end
-
-      it "links to local edit if command is local and user is project admin" do
-        command = commands(:echo)
-        project = projects(:test)
-        current_user.user_project_roles.create!(project: project, user: current_user, role_id: Role::ADMIN.id)
-        html = edit_command_link(command)
-        html.must_equal "<a title=\"Edit in admin UI\" class=\"edit-command glyphicon glyphicon-edit no-hover\" href=\"/admin/commands/#{command.id}\"></a>"
-      end
+    it "links to local edit" do
+      command = commands(:echo)
+      html = edit_command_link(command)
+      html.must_equal "<a title=\"Edit\" class=\"edit-command glyphicon glyphicon-edit no-hover\" href=\"/admin/commands/#{command.id}\"></a>"
     end
   end
 
