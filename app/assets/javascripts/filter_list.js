707fb015da1581d8e3d744afbd886ae1278d79c3
Michael Grosser <michael@grosser.it>
2017-04-22 09:26:43 -0700

    use live for header

--- app/assets/javascripts/filter_list.js
+++ app/assets/javascripts/filter_list.js
@@ -3,14 +3,12 @@
 // <li class="filtered-projects">
 // and having an input targetting it
 // <input type="search" class="filter-list" data-target=".filtered-projects">
-$(function(){
-  $('.filter-list').on('keyup', function(){
-    var $list = $($(this).data('target'));
-    var typed = $(this).val();
-    $list.each(function(i, element){
-      var $element = $(element);
-      var matches = !typed || (new RegExp(typed, 'i')).test($element.text());
-      $element.toggle(matches);
-    });
+$(document).on('keyup', '.filter-list', function(){
+  var $list = $($(this).data('target'));
+  var typed = $(this).val();
+  $list.each(function(i, element){
+    var $element = $(element);
+    var matches = !typed || (new RegExp(typed, 'i')).test($element.text());
+    $element.toggle(matches);
   });
 });
