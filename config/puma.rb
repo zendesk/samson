005fc65a9803cefb5eccfcd140346b9dd164afec
Michael Grosser <michael@grosser.it>
2017-05-04 09:10:59 -0700

    make puma boot on the correct port in dev
    see
     - https://github.com/puma/puma/pull/1277/files
     - https://github.com/rails/rails/issues/28971

--- config/puma.rb
+++ config/puma.rb
@@ -4,4 +4,10 @@ require_relative "boot"
 threads ENV.fetch('RAILS_MIN_THREADS', 8), ENV.fetch('RAILS_MAX_THREADS', 250)
 preload_app!
 
-bind 'tcp://0.0.0.0:9080'
+port = 9080
+
+# make dev puma boot on port 3000
+# remove once https://github.com/puma/puma/pull/1277 is released
+port = 3000 if (ENV["RAILS_ENV"] || "development") == "development"
+
+bind "tcp://0.0.0.0:#{port}"
