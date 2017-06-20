b2a5e32ab9504f3712faf6ddd4fe6aeb2af6a280
Michael Grosser <michael@grosser.it>
2017-06-20 11:38:26 -0700

    run all migrations on travis so we see if they still work or not

--- plugins/env/db/migrate/20170320213836_delete_abandomed_env_vars.rb
+++ plugins/env/db/migrate/20170320213836_delete_abandomed_env_vars.rb
@@ -23,24 +23,24 @@ class DeleteAbandomedEnvVars < ActiveRecord::Migration[5.0]
     [Project, EnvironmentVariableGroup].each do |klass|
       existing = klass.pluck(:id)
       deleted = EnvironmentVariable.where(parent_type: klass.name).where('parent_id NOT IN(?)', existing).delete_all
-      puts "DELETED #{deleted} #{klass.name} EnvironmentVariables by parent"
+      write "DELETED #{deleted} #{klass.name} EnvironmentVariables by parent"
     end
 
     # cleanup EnvironmentVariable by scope
     [DeployGroup, Environment].each do |klass|
       existing = klass.pluck(:id)
       deleted = EnvironmentVariable.where(scope_type: klass.name).where('scope_id NOT IN(?)', existing).delete_all
-      puts "DELETED #{deleted} #{klass.name} EnvironmentVariables by scope"
+      write "DELETED #{deleted} #{klass.name} EnvironmentVariables by scope"
     end
 
     # cleanup ProjectEnvironmentVariableGroup by project and group
     project_ids = Project.pluck(:id)
     deleted = ProjectEnvironmentVariableGroup.where('project_id NOT IN(?)', project_ids).delete_all
-    puts "DELETED #{deleted} ProjectEnvironmentVariableGroup by project"
+    write "DELETED #{deleted} ProjectEnvironmentVariableGroup by project"
 
     group_ids = EnvironmentVariableGroup.pluck(:id)
     deleted = ProjectEnvironmentVariableGroup.where('environment_variable_group_id NOT IN(?)', group_ids).delete_all
-    puts "DELETED #{deleted} ProjectEnvironmentVariableGroup by environment_variable_group"
+    write "DELETED #{deleted} ProjectEnvironmentVariableGroup by environment_variable_group"
   end
 
   def down
