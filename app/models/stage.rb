6ded9e539951fc7bf0489348bb1eeb1b088ed334
Michael Grosser <michael@grosser.it>
2017-06-22 18:31:20 -0400

    do not show build stages as interfering with deploy stages

--- app/models/stage.rb
+++ app/models/stage.rb
@@ -178,7 +178,7 @@ class Stage < ActiveRecord::Base
   def influencing_stage_ids
     deploy_group_ids = deploy_groups_stages.reorder(nil).pluck(:deploy_group_id)
     stage_ids = DeployGroupsStage.reorder(nil).where(deploy_group_id: deploy_group_ids).pluck('distinct stage_id')
-    Stage.reorder(nil).where(id: stage_ids, project_id: project_id).pluck(:id)
+    Stage.reorder(nil).where(no_code_deployed: false, id: stage_ids, project_id: project_id).pluck(:id)
   end
 
   def direct?
