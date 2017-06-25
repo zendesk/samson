d0bdb80171ebde4ccc22f452cf4fa5d003ce7a90
Michael Grosser <michael@grosser.it>
2017-06-25 15:31:37 +0200

    diff for large text blobs

--- test/helpers/audits_helper_test.rb
+++ test/helpers/audits_helper_test.rb
@@ -13,4 +13,13 @@ describe AuditsHelper do
       readable_ruby_value(BigDecimal.new(1.2, 2)).to_s.must_equal '1.2'
     end
   end
+
+  describe "#text_diff" do
+    it "produces a safe diff" do
+      diff = text_diff("a", "<script>alert(1)</script>")
+      diff.must_include "<del>a</del>"
+      diff.must_include "<ins><strong>&lt;script&gt;</strong>a<strong>lert(1)&lt;/script&gt;</strong></ins>"
+      assert diff.html_safe?
+    end
+  end
 end
