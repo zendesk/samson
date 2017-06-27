bf025850df43ccf58df570da39afe08c346768fa
Michael Grosser <michael@grosser.it>
2017-06-27 10:47:57 +0200

    better fix for test-env loading, also need to unset keys that are not listed in .env.test

--- config/dotenv.rb
+++ config/dotenv.rb
@@ -1,8 +1,13 @@
 # frozen_string_literal: true
 # Fill ENV from .env with Dotenv, to configure puma/RAILS_ENV/gems with .env
 require 'dotenv'
-Dotenv.load(Bundler.root.join('.env'))
+ENV["SAMSON_ENV_KEYS_BEFORE_DOTENV"] ||= ENV.keys.join(",")
 
 # when we run rake we load the dev environment first and fork to load the test env
-# so we need to overload what .env changed, load .env.test, and ignore system ENV
-Dotenv.overload(Bundler.root.join('.env.test')) if ENV['RAILS_ENV'] == 'test'
+# so we need to unset what .env changed and then load .env.test
+if ENV['RAILS_ENV'] == 'test'
+  (ENV.keys - ENV["SAMSON_ENV_KEYS_BEFORE_DOTENV"].split(",")).each { |k| ENV.delete(k) }
+  Dotenv.load(Bundler.root.join('.env.test'))
+else
+  Dotenv.load(Bundler.root.join('.env'))
+end
