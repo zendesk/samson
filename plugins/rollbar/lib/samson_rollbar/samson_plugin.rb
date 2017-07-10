ad085a2d3b0507bbecd21fe947a3acf9f778a00e
Henrik Jorgensen <henrikj@me.com>
2017-07-10 16:25:24 -0700

    Boilerplate plugin code

---
+++ plugins/rollbar/lib/samson_rollbar/samson_plugin.rb
@@ -0,0 +1,122 @@
+module SamsonRollbar
+  class Engine < Rails::Engine
+  end
+end
+
+# To add in your own UI for a hook, e.g., for the stage edit page:
+# - Create app/views/samson_rollbar/_my_fields.html.erb
+# - Add this line to this file, note the lack of leading underscore and extension:
+#   Samson::Hooks.view :stage_form, 'samson_rollbar/my_fields'
+
+
+#Samson::Hooks.view :stage_form, '<view path>'
+
+#Samson::Hooks.view :stage_show, '<view path>'
+
+#Samson::Hooks.view :project_form, '<view path>'
+
+#Samson::Hooks.view :build_new, '<view path>'
+
+#Samson::Hooks.view :deploy_group_show, '<view path>'
+
+#Samson::Hooks.view :deploy_group_form, '<view path>'
+
+#Samson::Hooks.view :deploy_group_table_header, '<view path>'
+
+#Samson::Hooks.view :deploy_group_table_cell, '<view path>'
+
+#Samson::Hooks.view :deploys_header, '<view path>'
+
+#Samson::Hooks.view :deploy_tab_nav, '<view path>'
+
+#Samson::Hooks.view :deploy_tab_body, '<view path>'
+
+#Samson::Hooks.view :deploy_view, '<view path>'
+
+#Samson::Hooks.view :deploy_form, '<view path>'
+
+#Samson::Hooks.view :admin_menu, '<view path>'
+
+#Samson::Hooks.view :manage_menu, '<view path>'
+
+#Samson::Hooks.view :project_tabs_view, '<view path>'
+
+
+# Possible callbacks are listed below, delete any unused ones.
+
+#Samson::Hooks.callback :stage_clone do
+  # Do stuff in here
+#end
+
+#Samson::Hooks.callback :stage_permitted_params do
+  # Do stuff in here
+#end
+
+#Samson::Hooks.callback :deploy_permitted_params do
+  # Do stuff in here
+#end
+
+#Samson::Hooks.callback :project_permitted_params do
+  # Do stuff in here
+#end
+
+#Samson::Hooks.callback :deploy_group_permitted_params do
+  # Do stuff in here
+#end
+
+#Samson::Hooks.callback :build_permitted_params do
+  # Do stuff in here
+#end
+
+#Samson::Hooks.callback :buddy_request do
+  # Do stuff in here
+#end
+
+#Samson::Hooks.callback :before_deploy do
+  # Do stuff in here
+#end
+
+#Samson::Hooks.callback :after_deploy_setup do
+  # Do stuff in here
+#end
+
+#Samson::Hooks.callback :after_deploy do
+  # Do stuff in here
+#end
+
+#Samson::Hooks.callback :before_docker_repository_usage do
+  # Do stuff in here
+#end
+
+#Samson::Hooks.callback :before_docker_build do
+  # Do stuff in here
+#end
+
+#Samson::Hooks.callback :after_docker_build do
+  # Do stuff in here
+#end
+
+#Samson::Hooks.callback :after_job_execution do
+  # Do stuff in here
+#end
+
+#Samson::Hooks.callback :job_additional_vars do
+  # Do stuff in here
+#end
+
+#Samson::Hooks.callback :buildkite_release_params do
+  # Do stuff in here
+#end
+
+#Samson::Hooks.callback :release_deploy_conditions do
+  # Do stuff in here
+#end
+
+#Samson::Hooks.callback :deploy_group_env do
+  # Do stuff in here
+#end
+
+#Samson::Hooks.callback :link_parts_for_resource do
+  # Do stuff in here
+#end
+
---
+++ plugins/rollbar/samson_rollbar.gemspec
@@ -0,0 +1,6 @@
+Gem::Specification.new 'samson_rollbar', '0.0.0' do |s|
+  s.summary = 'Samson Rollbar plugin'
+  s.authors = ['Henrik Jorgensen']
+  s.email = ['henrikj@me.com']
+  s.files = Dir['{app,config,db,lib}/**/*']
+end
