d0408ec590a7428fa02bc56af3635fc8aa3d7966
Henrik Jorgensen <henrikj@me.com>
2017-07-14 12:06:26 -0700

    More test for when it doesnt succeed

--- plugins/rollbar/test/models/rollbar_notification_test.rb
+++ plugins/rollbar/test/models/rollbar_notification_test.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 require_relative '../test_helper'
 
-SingleCov.covered! uncovered: 2
+SingleCov.covered!
 
 describe RollbarNotification do
   let(:endpoint) { 'https://rollbar.com' }
@@ -26,6 +26,18 @@ describe RollbarNotification do
 
       notification.deliver
       assert_requested request
+    end
+
+    it 'shows an error message' do
+      stub_request(:post, endpoint).to_return(
+        status: [500, "Internal Server Error"],
+        body: 'oops..'
+      )
+
+      Rails.logger.expects(:info).with("Sending Rollbar notification...")
+      Rails.logger.expects(:info).with("Failed to send Rollbar notification. HTTP 500: oops..")
+
+      notification.deliver
     end
   end
 end
