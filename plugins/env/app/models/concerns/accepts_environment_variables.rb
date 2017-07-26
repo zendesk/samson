b717a7e559563d2d4d7946898e620749941f631b
Michael Grosser <michael@grosser.it>
2017-07-25 21:09:28 -0700

    validate that environment variables are unique to prevent sql errors from showing to users
    could not be done on the model itself since at the save-time each row is validated,
    against the current DB state ... which passes ... but adding 2 non-unique rows still fails when
    saving ... so using a inline check ... and trying to prevent queries when nothing was assigned
    and the association is not even loaded.

--- plugins/env/app/models/concerns/accepts_environment_variables.rb
+++ plugins/env/app/models/concerns/accepts_environment_variables.rb
@@ -6,6 +6,19 @@ module AcceptsEnvironmentVariables
     base.class_eval do
       has_many :environment_variables, as: :parent, dependent: :destroy, inverse_of: :parent
       accepts_nested_attributes_for :environment_variables, allow_destroy: true, reject_if: ->(a) { a[:name].blank? }
+      validate :validate_unique_environment_variables
+
+      private
+
+      def validate_unique_environment_variables
+        return if persisted? && !environment_variables.proxy_association.loaded?
+
+        grouped = environment_variables.group_by { |e| [e.name, e.scope_type, e.scope_id, e.parent_type, e.parent_id] }
+        return if grouped.size == environment_variables.size
+
+        bad = grouped.select { |_, g| g.size > 1 }
+        errors.add :base, "Non-Unique environment variables found for: #{bad.map(&:first).map(&:first).join(", ")}"
+      end
     end
   end
 end
