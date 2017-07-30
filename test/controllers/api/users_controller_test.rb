16cca40f0111f37337099fecc1e43a970d9433d8
Ash McKenzie <ash@the-rebellion.net>
2017-07-30 17:16:29 +1000

    Show users via API with email support

--- test/controllers/api/users_controller_test.rb
+++ test/controllers/api/users_controller_test.rb
@@ -8,11 +8,45 @@ describe Api::UsersController do
 
   before { @request_format = :json }
 
+  as_a_deployer do
+    unauthorized :get, :show, id: 1, format: :json
+    unauthorized :get, :show_via_resource, email: 'no-matter@example.com', format: :json
+    unauthorized :delete, :destroy, id: 1, format: :json
+  end
+
   as_an_admin do
+    describe "#show" do
+      it "shows" do
+        get :show, params: {id: users(:admin)}, format: :json
+        assert_response :success
+      end
+    end
+
+    unauthorized :get, :show_via_resource, email: 'no-matter@example.com', format: :json
     unauthorized :delete, :destroy, id: 1, format: :json
   end
 
   as_a_super_admin do
+    describe "#show" do
+      it "shows" do
+        get :show, params: {id: users(:admin)}, format: :json
+        data = JSON.parse(response.body)
+
+        assert_response :success
+        assert_equal data['user']['id'], 135138680
+      end
+    end
+
+    describe "#show_via_resource" do
+      it "shows" do
+        get :show_via_resource, params: {email: users(:admin).email}, format: :json
+        data = JSON.parse(response.body)
+
+        assert_response :success
+        assert_equal data['user']['email'], 'admin@example.com'
+      end
+    end
+
     describe "#destroy" do
       it "deletes" do
         delete :destroy, params: {id: users(:admin)}, format: :json
