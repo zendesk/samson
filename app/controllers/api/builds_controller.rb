41553cfaf27d6319146e40ddb9c523338a3083b1
Michael Grosser <michael@grosser.it>
2017-08-31 16:26:48 -0700

    no need to ensure repos when we do not interact with them
    fixes bug introduced in https://github.com/zendesk/samson/pull/2213
    where the hook expected a build and not a project to get passed

--- app/controllers/api/builds_controller.rb
+++ app/controllers/api/builds_controller.rb
@@ -8,19 +8,13 @@ class Api::BuildsController < Api::BaseController
     build_params = params.require(:build)
     digest = extract_param(build_params, :docker_repo_digest)
     sha = extract_param(build_params, :git_sha)
-
-    Samson::Hooks.fire(:before_docker_repository_usage, current_project)
-
-    build = current_project.builds.create!(
+    current_project.builds.create!(
       build_params.permit(*Build::ASSIGNABLE_KEYS).merge(
         creator: current_user,
         docker_repo_digest: digest,
         git_sha: sha
       )
     )
-
-    Samson::Hooks.fire(:after_docker_build, build)
-
     head :created
   end
 
