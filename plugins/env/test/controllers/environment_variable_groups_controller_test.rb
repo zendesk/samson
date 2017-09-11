fc55fd3e10dd6a94d3f71f62d184674fb0f16aed
Michael Grosser <michael@grosser.it>
2017-09-11 09:49:46 -0700

    TOOLS-1870 allow deployers to create env var groups .. allow project admins to edit

--- plugins/env/test/controllers/environment_variable_groups_controller_test.rb
+++ plugins/env/test/controllers/environment_variable_groups_controller_test.rb
@@ -4,6 +4,36 @@ require_relative "../test_helper"
 SingleCov.covered!
 
 describe EnvironmentVariableGroupsController do
+  def self.it_updates
+    it "updates" do
+      variable = env_group.environment_variables.first
+      refute_difference "EnvironmentVariable.count" do
+        put :update, params: {
+          id: env_group.id,
+          environment_variable_group: {
+            environment_variables_attributes: {
+              "0" => {name: "N1", value: "V2", scope_type_and_id: "DeployGroup-#{deploy_group.id}", id: variable.id}
+            }
+          }
+        }
+      end
+
+      assert_redirected_to "/environment_variable_groups"
+      variable.reload.value.must_equal "V2"
+      variable.reload.scope.must_equal deploy_group
+    end
+  end
+
+  def self.it_destroys
+    it "destroy" do
+      env_group
+      assert_difference "EnvironmentVariableGroup.count", -1 do
+        delete :destroy, params: {id: env_group.id}
+      end
+      assert_redirected_to "/environment_variable_groups"
+    end
+  end
+
   let(:stage) { stages(:test_staging) }
   let(:project) { stage.project }
   let(:deploy_group) { stage.deploy_groups.first }
@@ -16,8 +46,20 @@ describe EnvironmentVariableGroupsController do
       }
     )
   end
+  let(:other_project) do
+    p = project.dup
+    p.name = 'xxxxx'
+    p.permalink = 'xxxxx'
+    p.save!(validate: false)
+    p
+  end
 
   as_a_viewer do
+    before do
+      env_group
+      env_group.update_column :id, 1 # need static id
+    end
+
     unauthorized :get, :new
     unauthorized :post, :create
     unauthorized :patch, :update, id: 1
@@ -56,7 +98,7 @@ describe EnvironmentVariableGroupsController do
     end
   end
 
-  as_an_admin do
+  as_a_project_admin do
     describe "#new" do
       it "renders" do
         get :new
@@ -81,20 +123,24 @@ describe EnvironmentVariableGroupsController do
     end
 
     describe "#update" do
+      let(:params) do
+        {
+          id: env_group.id,
+          environment_variable_group: {
+            name: "G2",
+            comment: "COOMMMENT",
+            environment_variables_attributes: {
+              "0" => {name: "N1", value: "V1"}
+            }
+          }
+        }
+      end
+
       before { env_group }
 
       it "adds" do
         assert_difference "EnvironmentVariable.count", +1 do
-          put :update, params: {
-            id: env_group.id,
-            environment_variable_group: {
-              name: "G2",
-              comment: "COOMMMENT",
-              environment_variables_attributes: {
-                "0" => {name: "N1", value: "V1"}
-              }
-            }
-          }
+          put :update, params: params
         end
 
         assert_redirected_to "/environment_variable_groups"
@@ -103,23 +149,7 @@ describe EnvironmentVariableGroupsController do
         env_group.comment.must_equal "COOMMMENT"
       end
 
-      it "updates" do
-        variable = env_group.environment_variables.first
-        refute_difference "EnvironmentVariable.count" do
-          put :update, params: {
-            id: env_group.id,
-            environment_variable_group: {
-              environment_variables_attributes: {
-                "0" => {name: "N1", value: "V2", scope_type_and_id: "DeployGroup-#{deploy_group.id}", id: variable.id}
-              }
-            }
-          }
-        end
-
-        assert_redirected_to "/environment_variable_groups"
-        variable.reload.value.must_equal "V2"
-        variable.reload.scope.must_equal deploy_group
-      end
+      it_updates
 
       it "destoys variables" do
         variable = env_group.environment_variables.first
@@ -135,16 +165,40 @@ describe EnvironmentVariableGroupsController do
 
         assert_redirected_to "/environment_variable_groups"
       end
+
+      it "cannot update when not an admin for any used projects" do
+        ProjectEnvironmentVariableGroup.create!(environment_variable_group: env_group, project: other_project)
+        put :update, params: params
+        assert_response :unauthorized
+      end
+
+      it "cannot update when not an admin for some used projects" do
+        ProjectEnvironmentVariableGroup.create!(environment_variable_group: env_group, project: project)
+        ProjectEnvironmentVariableGroup.create!(environment_variable_group: env_group, project: other_project)
+        put :update, params: params
+        assert_response :unauthorized
+      end
     end
 
     describe "#destroy" do
-      it "destroy" do
-        env_group
-        assert_difference "EnvironmentVariableGroup.count", -1 do
-          delete :destroy, params: {id: env_group.id}
-        end
-        assert_redirected_to "/environment_variable_groups"
+      it_destroys
+
+      it "cannot destroy when not an admin for all used projects" do
+        ProjectEnvironmentVariableGroup.create!(environment_variable_group: env_group, project: other_project)
+        delete :destroy, params: {id: env_group.id}
+        assert_response :unauthorized
       end
+    end
+  end
+
+  as_an_admin do
+    describe "#update" do
+      before { env_group }
+      it_updates
+    end
+
+    describe "#destroy" do
+      it_destroys
     end
   end
 end
