1f5fa5143f2c6f771119c725bacfa88905793af2
Michael Grosser <michael@grosser.it>
2017-10-30 13:22:48 +0100

    merge users api into regular controllers

--- app/controllers/api/users_controller.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-class Api::UsersController < Api::BaseController
-  before_action :authorize_resource!
-
-  def destroy
-    User.find(params.require(:id)).soft_delete!
-    head :ok
-  end
-end
