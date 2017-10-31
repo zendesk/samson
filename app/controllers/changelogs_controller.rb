b204306fc191f758141a888b3da89d7b80209d54
Anthony Woo <anthonywoo88@gmail.com>
2017-10-31 10:04:42 -0700

    configurable branch when generating changelog

--- app/controllers/changelogs_controller.rb
+++ app/controllers/changelogs_controller.rb
@@ -4,13 +4,13 @@ class ChangelogsController < ApplicationController
 
   def show
     if params[:start_date].blank? || params[:end_date].blank?
-      params[:start_date] = (Date.today.beginning_of_week - 3.days).to_s
+      params[:start_date] = (Date.today.beginning_of_week - 7.days).to_s
       params[:end_date] = Date.today.to_s
     end
 
+    @branch = params[:branch] || 'master'
     @start_date = Date.strptime(params[:start_date], '%Y-%m-%d')
     @end_date = Date.strptime(params[:end_date], '%Y-%m-%d')
-
-    @changeset = Changeset.new(current_project.repository_path, "master@{#{@start_date}}", "master@{#{@end_date}}")
+    @changeset = Changeset.new(current_project.repository_path, "#{@branch}@{#{@start_date}}", "#{@branch}@{#{@end_date}}")
   end
 end
