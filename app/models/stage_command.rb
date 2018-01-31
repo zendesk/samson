96675293a1e29c3160eebd7a5235c085f63ec33f
Ryan Gurney <rygurney@zendesk.com>
2018-01-30 16:25:24 -0800

    DEVEX-70 Prevent 'orphaned' records on soft_delete

--- app/models/stage_command.rb
+++ app/models/stage_command.rb
@@ -2,6 +2,8 @@
 class StageCommand < ActiveRecord::Base
   has_soft_deletion default_scope: true
 
-  belongs_to :stage
-  belongs_to :command, autosave: true
+  belongs_to :stage, optional: true
+  belongs_to :command, autosave: true, optional: true
+
+  validates :command, :stage, presence: true, if: :new_record?
 end
