f4bc07b5baf3f12e9847a0d2ba3fede4f9ec1403
Ryan Gurney <rygurney@zendesk.com>
2018-02-02 14:11:06 -0800

    Add logging to show when initializers are being loaded

---
+++ lib/samson/initializer_logging.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+# We saw initializers hanging boot so we are logging before every initializer is called to easily debug
+# Test: boot up rails and look at the logs
+if ENV['SERVER_MODE']
+  Rails::Engine.prepend(Module.new do
+    def load(file, *)
+      Rails.logger.info "Loading initializer #{file.sub("#{Rails.root}/", "")}"
+      super
+    end
+  end)
+end
