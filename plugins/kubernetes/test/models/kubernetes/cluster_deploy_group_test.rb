9a15d4d831bc655e451768d62a10d12d55fc26f5
Michael Grosser <michael@grosser.it>
2018-02-08 13:16:34 -0800

    show what namespaces are available during cluster validation

--- plugins/kubernetes/test/models/kubernetes/cluster_deploy_group_test.rb
+++ plugins/kubernetes/test/models/kubernetes/cluster_deploy_group_test.rb
@@ -1,7 +1,30 @@
 # frozen_string_literal: true
 require_relative "../../test_helper"
 
-SingleCov.covered! uncovered: 2
+SingleCov.covered!
 
 describe Kubernetes::ClusterDeployGroup do
+  let(:group) { kubernetes_cluster_deploy_groups(:pod1) }
+
+  describe "validations" do
+    it "is valid" do
+      group.cluster.expects(:namespaces).returns([group.namespace])
+      assert_valid group
+    end
+  end
+
+  describe "#validate_namespace_exists" do
+    it "is invalid when namespace lookup fails" do
+      assert_request(:get, "http://foobar.server/api/v1/namespaces", to_return: {status: 404}) do
+        refute_valid group
+        group.errors.full_messages.must_equal ["Namespace error looking up namespaces"]
+      end
+    end
+
+    it "is invalid when namespace does not exist" do
+      group.cluster.expects(:namespaces).returns(['foo', 'bar'])
+      refute_valid group
+      group.errors.full_messages.must_equal ["Namespace named 'pod1' does not exist, found: foo, bar"]
+    end
+  end
 end
