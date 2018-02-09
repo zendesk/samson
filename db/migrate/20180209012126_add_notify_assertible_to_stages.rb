33dbc4ea9e44ead0dbbeee22edfbc8e12674d70e
Zachary Wright <zachwr@zillowgroup.com>
2018-02-09 07:49:25 -0500

    Add # frozen_string_literal: true magic comment.

--- db/migrate/20180209012126_add_notify_assertible_to_stages.rb
+++ db/migrate/20180209012126_add_notify_assertible_to_stages.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddNotifyAssertibleToStages < ActiveRecord::Migration[5.1]
   def change
     add_column :stages, :notify_assertible, :boolean, default: false, null: false
--- plugins/assertible/samson_assertible.gemspec
+++ plugins/assertible/samson_assertible.gemspec
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 Gem::Specification.new 'samson_assertible', '0.0.0' do |s|
   s.summary = 'Samson Assertible plugin'
   s.authors = ['Zachary Wright']
