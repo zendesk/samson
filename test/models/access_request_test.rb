101d116b54c0199046ec925d5ca8e11306e23139
Ryan Gurney <rygurney@zendesk.com>
2018-02-15 13:41:47 -0800

    Refactor refute_valid_on to use includes for error messages

--- test/models/access_request_test.rb
+++ test/models/access_request_test.rb
@@ -18,19 +18,19 @@ describe AccessRequest do
     end
 
     it 'is invalid if it is missing a manager email' do
-      refute_valid_on access_request(manager_email: nil), :manager_email, ["Manager email can't be blank"]
+      refute_valid_on access_request(manager_email: nil), :manager_email, "Manager email can't be blank"
     end
 
     it 'is invalid if it is missing a reason' do
-      refute_valid_on access_request(reason: nil), :reason, ["Reason can't be blank"]
+      refute_valid_on access_request(reason: nil), :reason, "Reason can't be blank"
     end
 
     it 'is invalid if it is missing a project id' do
-      refute_valid_on access_request(project_ids: nil), :project_ids, ["Project ids can't be blank"]
+      refute_valid_on access_request(project_ids: nil), :project_ids, "Project ids can't be blank"
     end
 
     it 'is invalid if it is missing a role id' do
-      refute_valid_on access_request(role_id: nil), :role_id, ["Role can't be blank"]
+      refute_valid_on access_request(role_id: nil), :role_id, "Role can't be blank"
     end
   end
 end
