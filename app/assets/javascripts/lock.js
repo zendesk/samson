d66eb33ad78dc9f6fb5dcab1f3b6dc72e1d0a80d
Ryan Gurney <rygurney@zendesk.com>
2018-02-16 10:13:28 -0800

    Add buttons for quick lock expiration time selection

--- app/assets/javascripts/lock.js
+++ app/assets/javascripts/lock.js
@@ -1,14 +1,44 @@
+// Governs the lock/warning form. Users can use either the buttons for quickly setting the
+// lock/warning duration or a datetime picker for setting a custom value.
+
 $(function () {
+  var datetimeFormat = 'MM/DD/YYYY h:mm A';
+
+  function formatLockExpirationTime(form, lockDeleteAtInput) {
+    form.submit(function (e) {
+      var lockDeleteAtVal = lockDeleteAtInput.val();
+
+      if (lockDeleteAtVal !== '') {
+        var lockExpireMoment = moment(lockDeleteAtVal, datetimeFormat);
+        lockDeleteAtInput.val(lockExpireMoment.utc().format());
+      }
+    });
+  }
+
+  function handleLockTimeButtons(lockExpirationTimeButtons, lockDeleteAtInput) {
+    // user clicks on pre-defined lock time buttons
+    lockExpirationTimeButtons.click(function (e) {
+      var buttonData = e.target.dataset;
+      var unit = buttonData.unit;
+      var quantity = parseInt(buttonData.num, 10);
+
+      if (quantity === 0) {
+        lockDeleteAtInput.val('');
+      } else {
+        var newVal = moment().add(quantity, unit).format(datetimeFormat);
+        lockDeleteAtInput.val(newVal);
+      }
+    });
+  }
+
   $('.datetimepicker').datetimepicker();
 
-  $('.new-lock-form').submit(function (e) {
-    var lockDeleteAtInput = $(e.target).find('#lock_delete_at');
-    var lockDeleteAtVal = lockDeleteAtInput.val();
+  $('.new-lock-form').each(function() {
+    var form = $(this);
+    var lockDeleteAtInput = form.find('#lock_delete_at');
+    var lockExpirationTimeButtons = form.find('.lock-times');
 
-    if (lockDeleteAtVal !== '') {
-      var lockExpireMoment = moment(lockDeleteAtVal);
-
-      lockDeleteAtInput.val(moment(lockDeleteAtVal).utc().format());
-    }
+    formatLockExpirationTime(form, lockDeleteAtInput);
+    handleLockTimeButtons(lockExpirationTimeButtons, lockDeleteAtInput);
   });
 });
