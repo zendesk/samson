988363b19dc1e7c55779916e8f536685a5451eee
Michael Grosser <michael@grosser.it>
2018-03-02 17:10:13 -0800

    rename revision -> version ... since it can be a sha or a tag

--- config/initializers/console.rb
+++ config/initializers/console.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 Rails.application.console do
   Rails::ConsoleMethods.send(:prepend, Samson::ConsoleExtensions)
-  puts "Samson version: #{Rails.application.config.samson.revision.to_s.first(7)}"
+  puts "Samson version: #{Rails.application.config.samson.version&.first(7)}"
   ActiveRecord::Base.logger = Rails.logger = ActiveSupport::TaggedLogging.new(ActiveSupport::Logger.new(STDOUT))
   Audited.store[:audited_user] = "rails console"
 end
