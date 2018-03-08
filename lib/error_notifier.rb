90ec78fba44d222e1abaa34b117e560b8199c0d5
Ryan Gurney <rygurney@zendesk.com>
2018-03-08 13:53:24 -0800

    Allow ErrorNotifier to accept strings

--- lib/error_notifier.rb
+++ lib/error_notifier.rb
@@ -10,9 +10,14 @@ module ErrorNotifier
         raise exception # tests have to use ErrorNotifier.expects(:notify) to silence intended errors
       else
         # TODO: Don't spam logs twice if any exception plugin is enabled
-        exception_message = "ErrorNotifier: #{exception.class} - #{exception.message} " \
-          "- #{exception.backtrace[0..5].join("\n")}"
-        ::Rails.logger.error exception_message
+        message_body =
+          if exception.is_a?(String)
+            exception
+          else
+            "#{exception.class} - #{exception.message} - #{exception.backtrace[0..5].join("\n")}"
+          end
+
+        ::Rails.logger.error "ErrorNotifier: #{message_body}"
       end
       debug_info
     end
