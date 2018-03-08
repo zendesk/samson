dc8a13b6884b657adc5bf48811d05b9bd54d0d31
Michael Grosser <michael@grosser.it>
2018-03-07 17:29:24 -0800

    run dev in pura puma so we can reproduce restart issues better

--- lib/samson/initializer_logging.rb
+++ lib/samson/initializer_logging.rb
@@ -2,7 +2,7 @@
 
 # We saw initializers hanging boot so we are logging before every initializer is called to easily debug
 # Test: boot up rails and look at the logs
-if ENV['SERVER_MODE']
+if ENV['SERVER_MODE'] && !Rails.env.development?
   Rails::Engine.prepend(Module.new do
     def load(file, *)
       Rails.logger.info "Loading initializer #{file.sub("#{Rails.root}/", "")}"
