2bbeeab7cf086acf5e86c84edd2923882ff1bee6
Hai Dang Tran <tranhaidangtn10609@gmail.com>
2018-04-03 18:28:25 +0000

    Shellescape secret values in terminal executor

--- app/models/terminal_executor.rb
+++ app/models/terminal_executor.rb
@@ -133,7 +133,7 @@ class TerminalExecutor
       key = $1
       if expanded = resolver.expand('unused', key).first&.last
         key.replace(expanded)
-        Samson::Secrets::Manager.read(key, include_value: true).fetch(:value)
+        Samson::Secrets::Manager.read(key, include_value: true).fetch(:value).shellescape
       end
     end
 
