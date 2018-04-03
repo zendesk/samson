cf94c7afa4f76af82adf7f6a53c134e55042db14
nogates <david@singularities.org>
2018-04-03 11:12:22 +0200

    add ability to use env variables in deploys

---
+++ plugins/deploy_env_vars/app/helpers/deploys_helper.rb
@@ -0,0 +1,63 @@
+DeploysHelper.class_eval do
+  def redeploy_button
+    return if @deploy.job.active?
+
+    html_options = {method: :post}
+    if @deploy.succeeded?
+      html_options[:class] = 'btn btn-default'
+      html_options[:data] = {
+        toggle: 'tooltip',
+        placement: 'auto bottom'
+      }
+      html_options[:title] = 'Why? This deploy succeeded.'
+    else
+      html_options[:class] = 'btn btn-danger'
+    end
+
+    link_to "Redeploy",
+      project_stage_deploys_path(
+        @project,
+        @deploy.stage,
+        deploy: redeploy_params
+      ),
+      html_options
+  end
+
+  private
+
+  # Applies different logic depending on the class of each of the deploy
+  # parameters, so it supports nested paramaters based on object relations
+  def redeploy_params
+    params = Samson::Hooks.fire(:deploy_permitted_params).flatten(1) || []
+    params.each_with_object(reference: @deploy.reference) do |param, collection|
+      case param
+      when String,Symbol
+        collection.merge!(param => @deploy.public_send(param))
+      when Hash
+        nested_redeploy_params(collection, param)
+      else
+        collection
+      end
+    end
+  end
+
+  def nested_redeploy_params(collection, params)
+    params.each_with_object(collection) do |(key, attributes), nested|
+      if key.to_s.ends_with?('attributes')
+        nested.merge!(key => deploy_relation_attributes(key, attributes))
+      else
+        nested
+      end
+    end
+  end
+
+  # currently, one has_many relations are supported
+  def deploy_relation_attributes(key, attributes)
+    relation_name = key.to_s.gsub(/_attributes$/, '')
+    @deploy.public_send(relation_name).map do |item|
+      (attributes - [ :id, :_destroy ]).each_with_object({}) do |attribute, hash|
+        hash.merge!(attribute => item.public_send(attribute))
+      end
+    end
+  end
+end
---
+++ plugins/deploy_env_vars/lib/samson_deploy_env_vars/samson_plugin.rb
@@ -0,0 +1,24 @@
+module SamsonDeployEnvVars
+  class Engine < Rails::Engine
+    config.after_initialize do
+      # Decorates default deploys helper. Maybe there is a better way to do this?
+      require_relative '../../app/helpers/deploys_helper'
+    end
+  end
+end
+
+# Adds the deploy env vars view to the deploy form in order to add
+# specific environment vars per deploy
+Samson::Hooks.view :deploy_form, 'samson_deploy_env_vars/deploy_form'
+
+# Allows environment vars as valid parameters for the deploy model
+Samson::Hooks.callback :deploy_permitted_params do
+  AcceptsEnvironmentVariables::ASSIGNABLE_ATTRIBUTES
+end
+
+# Injects specific environment variables for the deploy if any
+Samson::Hooks.callback :job_additional_vars do |job|
+  job.deploy.environment_variables.each_with_object({}) do |var, collection|
+    collection.merge!(var.name => var.value)
+  end
+end
