2259b1e05c442a07e8ee6caa1f6b93f62e7bf599
Stanisław Pitucha <stan.pitucha@envato.com>
2018-04-26 16:27:38 +1000

    Use the Ruby time not SQL one
    CURRENT_TIMESTAMP is local while Rails may be configured to use another
    timezone. These shouldn't be mixed in one place.

--- app/models/lock.rb
+++ app/models/lock.rb
@@ -54,7 +54,7 @@ class Lock < ActiveRecord::Base
   end
 
   def self.remove_expired_locks
-    Lock.where("delete_at IS NOT NULL and delete_at < CURRENT_TIMESTAMP").find_each(&:soft_delete!)
+    Lock.where("delete_at IS NOT NULL and delete_at < ?", Time.now).find_each(&:soft_delete!)
   end
 
   def delete_in=(seconds)
