a1983ccfc11885fc7a553105741c9b21972e9ad2
Michael Grosser <michael@grosser.it>
2018-04-30 09:34:12 -0700

    bundle update in preparation for rails 5.2

--- test/support/query_counter.rb
+++ test/support/query_counter.rb
@@ -4,7 +4,9 @@ class ActiveSupport::TestCase
   def sql_queries(&block)
     queries = []
     counter = ->(*, payload) do
-      queries << payload.fetch(:sql) unless ["CACHE", "SCHEMA"].include?(payload.fetch(:name))
+      sql = payload.fetch(:sql)
+      # NOTE: it used to have a useful :name property, maybe that will come back in rails 5.2 or 6
+      queries << sql unless sql.match?(/\ARELEASE SAVEPOINT|SAVEPOINT /)
     end
 
     ActiveSupport::Notifications.subscribed(counter, "sql.active_record", &block)
