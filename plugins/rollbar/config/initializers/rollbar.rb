13f8a4282c98ce4523535b553f3df841bb50741f
Stanisław Pitucha <stan.pitucha@envato.com>
2018-05-03 12:43:52 +1000

    DRY the config logic

--- plugins/rollbar/config/initializers/rollbar.rb
+++ plugins/rollbar/config/initializers/rollbar.rb
@@ -4,8 +4,12 @@ if token = ENV['ROLLBAR_ACCESS_TOKEN']
   Rollbar.configure do |config|
     config.access_token = token
     config.environment = Rails.env
-    config.endpoint = ENV.fetch('ROLLBAR_URL') + '/api/1/item/' if ENV.key?('ROLLBAR_URL')
-    config.web_base = ENV.fetch('ROLLBAR_WEB_BASE') if ENV.key?('ROLLBAR_WEB_BASE')
+    if url = ENV['ROLLBAR_URL']
+      config.endpoint = url + '/api/1/item/'
+    end
+    if web_base = ENV['ROLLBAR_WEB_BASE']
+      config.web_base = web_base
+    end
     config.use_thread # use threads for async notifications (waits for them at_exit)
     config.code_version = Rails.application.config.samson.version&.first(7)
     config.populate_empty_backtraces = true
