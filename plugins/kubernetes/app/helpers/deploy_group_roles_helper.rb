0004a49f04303afbf59e199ecc72aabd01f4a9dd
Michael Grosser <michael@grosser.it>
2018-07-18 11:19:35 -0700

    make deleted deploy group roles more obvious

---
+++ plugins/kubernetes/app/helpers/deploy_group_roles_helper.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+# NOTE: using kubernetes namespace resulted in a strange error on every page
+module DeployGroupRolesHelper
+  def kubernetes_deploy_group_role_replica(role, dgr)
+    if dgr.delete_resource?
+      icon_tag :remove, title: "Marked for deletion"
+    else
+      html = "".html_safe
+      html << dgr.replicas.to_s
+      if role.autoscaled?
+        html << icon_tag(:scale, title: "Replicas managed externally, actual count might not match this")
+      end
+      html
+    end
+  end
+end
