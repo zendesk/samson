34d574019a7d43ff51a0d7003b290fb1ffa37815
Michael Grosser <michael@grosser.it>
2018-07-23 10:11:37 -0700

    also retry on connection resets

--- plugins/kubernetes/lib/samson_kubernetes/samson_plugin.rb
+++ plugins/kubernetes/lib/samson_kubernetes/samson_plugin.rb
@@ -13,7 +13,7 @@ module SamsonKubernetes
   # see https://github.com/abonas/kubeclient/issues/240
   # using a method to avoid loading kubeclient on every boot ~0.1s
   def self.connection_errors
-    [OpenSSL::SSL::SSLError, Kubeclient::HttpError, Errno::ECONNREFUSED].freeze
+    [OpenSSL::SSL::SSLError, Kubeclient::HttpError, Errno::ECONNREFUSED, Errno::ECONNRESET].freeze
   end
 
   def self.retry_on_connection_errors(&block)
