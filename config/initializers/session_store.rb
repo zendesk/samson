6384b30a8536b2bd2ba01fd6905cfda56b76e334
Adam Malcontenti-Wilson <amalcontenti-wilson@zendesk.com>
2018-07-23 11:08:38 -0700

    Scrub session from rollbar

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,4 @@
 # frozen_string_literal: true
 # Restart your server when you modify this file.
 
-# enable multiple samson instances on the same base domain (samson-staging.foo.com + samson-production.foo.com)
-Samson::Application.config.session_store :cookie_store, key: "_samson_session_#{Rails.env}"
+Samson::Application.config.session_store :cookie_store, key: Samson::Application.config.session_key
