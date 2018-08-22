f1fc7a94e7d3b9bef25a718d3eda837e1381d8ca
Daniel Schierbeck <daniel.schierbeck@gmail.com>
2018-08-22 10:41:43 +0200

    Memoize the statuses

--- app/models/github_status.rb
+++ app/models/github_status.rb
@@ -48,12 +48,14 @@ class GithubStatus
   end
 
   def statuses
-    statuses = @github.combined_status(@repo, @ref).statuses
+    @statuses ||= begin
+      statuses = @github.combined_status(@repo, @ref).statuses
 
-    return [] if statuses.nil?
+      return [] if statuses.nil?
 
-    statuses.group_by(&:context).map do |context, statuses|
-      Status.new(context, statuses.max_by {|status| status.created_at.to_i })
+      statuses.group_by(&:context).map do |context, statuses|
+        Status.new(context, statuses.max_by {|status| status.created_at.to_i })
+      end
     end
   rescue Octokit::Error
     # In case of error, fall back to not listing the statuses.
