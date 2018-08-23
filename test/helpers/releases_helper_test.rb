d25be1ccc3675b652018586666e21bd29eddf256
Daniel Schierbeck <daniel.schierbeck@gmail.com>
2018-08-23 15:03:01 +0200

    Fix helper name

--- test/helpers/releases_helper_test.rb
+++ test/helpers/releases_helper_test.rb
@@ -15,32 +15,32 @@ describe ReleasesHelper do
     end
   end
 
-  describe "#status_glyphicon" do
+  describe "#github_commit_status_icon" do
     include ApplicationHelper
 
     it "renders an icon for success" do
-      html = status_glyphicon("success")
+      html = github_commit_status_icon("success")
       html.must_include "glyphicon-ok"
       html.must_include "text-success"
       html.must_include "Github status: success"
     end
 
     it "renders an icon for failure" do
-      html = status_glyphicon("failure")
+      html = github_commit_status_icon("failure")
       html.must_include "glyphicon-remove"
       html.must_include "text-danger"
       html.must_include "Github status: failure"
     end
 
     it "renders an icon for missing status" do
-      html = status_glyphicon("missing")
+      html = github_commit_status_icon("missing")
       html.must_include "glyphicon-minus"
       html.must_include "text-muted"
       html.must_include "Github status: missing"
     end
 
     it "renders an icon for pending status" do
-      html = status_glyphicon("pending")
+      html = github_commit_status_icon("pending")
       html.must_include "glyphicon-hourglass"
       html.must_include "text-primary"
       html.must_include "Github status: pending"
