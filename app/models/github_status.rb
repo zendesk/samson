ac80490fb882fb21c53a26cf3ad781d9c3f599fb
Daniel Schierbeck <daniel.schierbeck@gmail.com>
2018-08-23 13:09:02 +0200

    Refactor GithubStatus
    * Add testing.
    * Isolate network related code into a procedural method.

--- app/models/github_status.rb
+++ app/models/github_status.rb
@@ -26,16 +26,38 @@ class GithubStatus
     end
   end
 
-  def initialize(repo, ref, github: GITHUB, cache: Rails.cache)
-    @github = github
-    @cache = cache
-    @repo = repo
-    @ref = ref
-    @state = nil
+  attr_reader :state, :statuses
+
+  def initialize(state, statuses)
+    @state = state
+    @statuses = statuses
   end
 
-  def state
-    status_response ? status_response.state : "missing"
+  def self.fetch(repo, ref)
+    cache_key = [name, repo, ref]
+
+    response = Rails.cache.read(cache_key)
+
+    # Fetch the data if the cache returned nil.
+    response ||= begin
+      GITHUB.combined_status(repo, ref)
+    rescue Octokit::Error
+      nil
+    end
+
+    # Fall back to a "missing" status.
+    return new("missing", []) if response.nil?
+
+    # Don't cache pending statuses, since we expect updates soon.
+    unless response&.state == "pending"
+      Rails.cache.write(cache_key, response, expires_in: 1.hour)
+    end
+
+    statuses = response.statuses.group_by(&:context).map do |context, statuses|
+      Status.new(context, statuses.max_by { |status| status.created_at.to_i })
+    end
+
+    new(response.state, statuses)
   end
 
   def success?
@@ -52,39 +74,5 @@ class GithubStatus
 
   def missing?
     state == "missing"
-  end
-
-  def statuses
-    @statuses ||= begin
-      return [] if status_response.nil?
-
-      status_response.statuses.group_by(&:context).map do |context, statuses|
-        Status.new(context, statuses.max_by { |status| status.created_at.to_i })
-      end
-    end
-  end
-
-  private
-
-  def status_response
-    return @status_response if defined?(@status_response)
-
-    cache_key = [self.class.to_s, @repo, @ref]
-
-    @status_response = @cache.read(cache_key)
-
-    # Fetch the data if the cache returned nil.
-    @status_response ||= begin
-      @github.combined_status(@repo, @ref)
-    rescue Octokit::Error
-      nil
-    end
-
-    # Don't cache pending statuses, since we expect updates soon.
-    unless @status_response&.state == "pending"
-      @cache.write(cache_key, @status_response, expires_in: 1.hour)
-    end
-
-    @status_response
   end
 end
