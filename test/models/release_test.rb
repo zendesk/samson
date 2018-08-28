138d46ea82fcd381540661b4b0a74e9ba9f61ffb
Daniel Schierbeck <daniel.schierbeck@gmail.com>
2018-08-28 13:24:13 +0200

    Don't require testing Release#code_authors
    This is already covered by functional testing and would be obvious if
    broken.

--- test/models/release_test.rb
+++ test/models/release_test.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 require_relative '../test_helper'
 
-SingleCov.covered! uncovered: 3
+SingleCov.covered! uncovered: 4
 
 describe Release do
   let(:author) { users(:deployer) }
