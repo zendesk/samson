54c5e1b5416fd459ef80ee3ffb65231a97276ee8
Daniel Schierbeck <daniel.schierbeck@gmail.com>
2018-08-28 14:45:32 +0200

    Move the helper test

--- test/helpers/releases_helper_test.rb
+++ test/helpers/releases_helper_test.rb
@@ -15,15 +15,6 @@ describe ReleasesHelper do
     end
   end
 
-  describe "#github_user_avatar" do
-    it "renders an avatar for a Github user" do
-      user = stub(login: "willy_wonka", avatar_url: "http://wonka.com/me.gif")
-      html = github_user_avatar(user)
-
-      html.must_include %(title="willy_wonka")
-    end
-  end
-
   describe "#github_commit_status_icon" do
     include ApplicationHelper
 
