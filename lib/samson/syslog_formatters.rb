67a5effaf8d535f73f97dbd636719aacf705d8b7
sathish <sathishavm@gmail.com>
2018-10-04 01:06:20 -0700

    Re-formatted log format

--- lib/samson/syslog_formatters.rb
+++ lib/samson/syslog_formatters.rb
@@ -4,11 +4,19 @@ require 'syslog/logger'
 module Samson
   class SyslogFormatter < Syslog::Logger::Formatter
     def call(severity, timestamp, _progname, message)
+      message_h = begin
+        message = message.to_json unless message.is_a?(String)
+        # Fix for Integer and Array as a return object
+        (parsed = JSON.parse(message)).is_a?(Hash) ? parsed : raise(JSON::ParserError)
+      rescue JSON::ParserError
+        {"message": message&.squish}
+      end
       {
-        severity: severity,
-        time: timestamp,
-        message: message
-      }.to_json
+        "level": severity,
+        "@timestamp": timestamp,
+        "application": "samson",
+        "host": Rails.application.config.samson.uri.host
+      }.merge(message_h).to_json
     end
   end
 end
--- test/lib/samson/syslog_formatters_test.rb
+++ test/lib/samson/syslog_formatters_test.rb
@@ -6,14 +6,40 @@ SingleCov.covered!
 
 describe Samson::SyslogFormatter do
   describe '.json' do
+    before do
+      @output = StringIO.new
+      @logger = Logger.new(@output)
+      @logger.formatter = Samson::SyslogFormatter.new
+      @log_template = {level: "INFO", "@timestamp": Time.now, application: "samson", host: "www.test-url.com"}
+    end
+
     it 'returns a json formatted log' do
       travel_to Time.parse("2017-05-01 01:00 +0000").utc do
-        output = StringIO.new
-        logger = Logger.new(output)
-        logger.formatter = Samson::SyslogFormatter.new
-        logger.info('test')
+        message = 'test'
+        @log_template[:"@timestamp"] = Time.now
+        @logger.info(message)
+        expected = @log_template.merge(message: message)
+        @output.string.must_equal(expected.to_json)
+      end
+    end
 
-        output.string.must_equal({severity: "INFO", time: Time.now, message: "test"}.to_json)
+    it 'parse and format json messages' do
+      travel_to Time.parse("2017-05-01 01:00 +0000").utc do
+        message = {content: "xyz", data: "123"}
+        @log_template[:"@timestamp"] = Time.now
+        @logger.info(message)
+        expected = @log_template.merge(message)
+        @output.string.must_equal(expected.to_json)
+      end
+    end
+
+    it 'skips parser when message isn`t hash' do
+      travel_to Time.parse("2017-05-01 01:00 +0000").utc do
+        message = "[1,2]"
+        @log_template[:"@timestamp"] = Time.now
+        @logger.info(message)
+        expected = @log_template.merge(message: message)
+        @output.string.must_equal(expected.to_json)
       end
     end
   end
