2d32a7e32808eb5c11233f7c2ca111077e0baa3e
sathish <sathishavm@gmail.com>
2018-10-04 11:45:32 -0700

    Added more comments

--- lib/samson/syslog_formatters.rb
+++ lib/samson/syslog_formatters.rb
@@ -7,16 +7,17 @@ module Samson
       message_h =
         begin
           message = message.to_json unless message.is_a?(String)
-          # Fix for Integer and Array as a return object
+          # We support only string and hash in log.
+          # JSON also parse's Integer and Arrays, we skip those.
           (parsed = JSON.parse(message)).is_a?(Hash) ? parsed : raise(JSON::ParserError)
         rescue JSON::ParserError
           {"message": message&.squish}
         end
       {
-        "level": severity,
+        level: severity,
         "@timestamp": timestamp,
-        "application": "samson",
-        "host": Rails.application.config.samson.uri.host
+        application: "samson",
+        host: Rails.application.config.samson.uri.host
       }.merge(message_h).to_json
     end
   end
