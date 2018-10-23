1712fc012be947e397e5f3c44d7aec166599c780
Ryan Gurney <rygurney@zendesk.com>
2018-10-23 15:05:21 -0700

    Add checkbox for use_env_repo setting on Project edit page

--- plugins/env/lib/samson_env/samson_plugin.rb
+++ plugins/env/lib/samson_env/samson_plugin.rb
@@ -55,7 +55,12 @@ Samson::Hooks.view :deploy_tab_nav, "samson_env/deploy_tab_nav"
 Samson::Hooks.view :deploy_tab_body, "samson_env/deploy_tab_body"
 
 Samson::Hooks.callback :project_permitted_params do
-  AcceptsEnvironmentVariables::ASSIGNABLE_ATTRIBUTES.merge(environment_variable_group_ids: [])
+  [
+    AcceptsEnvironmentVariables::ASSIGNABLE_ATTRIBUTES.merge(
+      environment_variable_group_ids: []
+    ),
+    :use_env_repo
+  ]
 end
 
 Samson::Hooks.callback :after_deploy_setup do |dir, job|
