0eb38f3ab638e2ee679d6d779f23f51246631993
Christopher Thilgen <cthilgen@zendesk.com>
2019-03-10 00:26:33 -0800

    adjust static class definitions based on pr comments

--- app/presenters/deploy_group_usage_csv_presenter.rb
+++ app/presenters/deploy_group_usage_csv_presenter.rb
@@ -2,47 +2,51 @@
 require 'csv'
 
 class DeployGroupUsageCsvPresenter
-  def self.to_csv
-    CSV.generate do |csv|
-      csv << csv_header
-      Project.all.each do |project|
-        if project.stages.empty?
-          csv << csv_line(project)
-        else
-          project.stages.each do |stage|
-            stage.deploy_groups.each do |deploy_group|
-              csv << csv_line(project, stage, deploy_group)
+  class << self
+    def to_csv
+      CSV.generate do |csv|
+        csv << csv_header
+        Project.all.each do |project|
+          if project.stages.empty?
+            csv << csv_line(project)
+          else
+            project.stages.each do |stage|
+              stage.deploy_groups.each do |deploy_group|
+                csv << csv_line(project, stage, deploy_group)
+              end
             end
           end
         end
       end
     end
-  end
 
-  def self.csv_header
-    [
-      "project_name",
-      "stage_name",
-      "deploy_group_name",
-      "deploy_group_environment_id",
-      "deploy_group_environment_name",
-      "deploy_group_env_value",
-      "deploy_group_permalink"
-    ]
-  end
+    private
 
-  def self.csv_line(project, stage = nil, deploy_group = nil)
-    result = [project.name]
-    if stage && deploy_group
-      result.concat [
-        stage.name,
-        deploy_group.name,
-        deploy_group.environment_id,
-        deploy_group.environment.name,
-        deploy_group.env_value,
-        deploy_group.permalink
+    def csv_header
+      [
+        "project_name",
+        "stage_name",
+        "deploy_group_name",
+        "deploy_group_environment_id",
+        "deploy_group_environment_name",
+        "deploy_group_env_value",
+        "deploy_group_permalink"
       ]
     end
-    result
+
+    def csv_line(project, stage = nil, deploy_group = nil)
+      result = [project.name]
+      if stage && deploy_group
+        result.concat [
+          stage.name,
+          deploy_group.name,
+          deploy_group.environment_id,
+          deploy_group.environment.name,
+          deploy_group.env_value,
+          deploy_group.permalink
+        ]
+      end
+      result
+    end
   end
 end
