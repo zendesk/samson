f3a9ca6b6df212b4f746c347d0714e07e6b7e9c7
Michael Grosser <michael@grosser.it>
2019-03-26 16:47:59 -0700

    separate log formatter and log location options

--- lib/samson/logging.rb
+++ lib/samson/logging.rb
@@ -1,33 +1,19 @@
 # frozen_string_literal: true
+
 config = Rails.application.config
 
 if Samson::EnvCheck.set?("RAILS_LOG_TO_STDOUT")
-  # heroku and docker: dump everything to stdout
+  # good for heroku or docker
   config.logger = ActiveSupport::TaggedLogging.new(ActiveSupport::Logger.new(STDOUT))
 elsif Samson::EnvCheck.set?("RAILS_LOG_TO_SYSLOG")
-  require 'lograge'
-  require 'logstash-event'
-  # log 1 message per request to syslog in json format
-  config.lograge.enabled = true
-
-  config.lograge.custom_options = ->(event) do
-    # show params for every request
-    unwanted_keys = %w[format action controller]
-    params = event.payload[:params].reject { |key, _| unwanted_keys.include? key }
-    params['commits'] = '... truncated ...' if params['commits'] # lots of metadata from github we don't need
-    request = event.payload[:headers].instance_variable_get(:@req)
-    {
-      params: params,
-      user_id: request.env['warden']&.user&.id,
-      client_ip: request.remote_ip
-    }
-  end
-
+  # good for production hosts with syslog
+  # TODO: add Syslog::Logger#silence to support dev mode
+  # TODO: add tagged logging so we get request-ids
   config.logger = Syslog::Logger.new('samson')
-  config.action_cable.logger = Syslog::Logger.new('samson')
-  config.lograge.formatter = Lograge::Formatters::Logstash.new
-  config.logger.formatter = config.action_cable.logger.formatter = Samson::SyslogFormatter.new
-elsif ENV["SERVER_MODE"] # regular file logger that needs rotating
+  config.logger.formatter = Samson::SyslogFormatter.new
+elsif ENV["SERVER_MODE"]
+  # good for development or servers without syslog
+  # regular file logger that needs rotating
   # Reopen logfile when we receive a SIGHUP for log-rotation
   # puma normally kills itself with INT if it receives a SIGHUP
   # see https://github.com/puma/puma/pull/1527 and https://github.com/puma/puma/blob/master/docs/signals.md
@@ -46,7 +32,25 @@ elsif ENV["SERVER_MODE"] # regular file logger that needs rotating
   end
 end
 
-# workaround for our server ... always gets SIGHUP even though it does not do file logging
-if ENV["IGNORE_SIGHUP"]
-  Signal.trap(:SIGHUP) {}
+# log 1 message per request to in json/syslog format
+if ENV["RAILS_LOG_WITH_LOGRAGE"]
+  require 'lograge'
+  require 'logstash-event'
+  config.lograge.enabled = true
+  config.lograge.custom_options = ->(event) do
+    # show params for every request
+    unwanted_keys = %w[format action controller]
+    params = event.payload[:params].reject { |key, _| unwanted_keys.include? key }
+    params['commits'] = '... truncated ...' if params['commits'] # lots of metadata from github we don't need
+    request = event.payload[:headers].instance_variable_get(:@req)
+    {
+      params: params,
+      user_id: request.env['warden']&.user&.id,
+      client_ip: request.remote_ip
+    }
+  end
+  config.lograge.formatter = Lograge::Formatters::Logstash.new
 end
+
+# workaround for our server ... always gets SIGHUP even though it does not do file logging
+Signal.trap(:SIGHUP) {} if ENV["IGNORE_SIGHUP"]
