95e69f98ff0168990885af558f5491acb293f295
Cameron Dutro <camertron@gmail.com>
2019-06-27 16:05:30 -0700

    Pull allowed request origins from ENV

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -34,7 +34,7 @@ Samson::Application.configure do
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
-  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  config.action_cable.allowed_request_origins = ENV['RAILS_ALLOWED_REQUEST_ORIGINS'].split(',').map(&:strip)
 
   # Set to :debug to see everything in the log.
   config.log_level = :info
