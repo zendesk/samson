bdf5b14a2af48103dcee4bbb4871860794350089
Jon Moter <jmoter@zendesk.com>
2019-10-22 09:02:31 -0700

    DB migration for istio inject annotation column

---
+++ plugins/kubernetes/db/migrate/20191022023140_add_istio_inject_column.rb
@@ -0,0 +1,5 @@
+class AddIstioInjectColumn < ActiveRecord::Migration[5.2]
+  def change
+    add_column :kubernetes_deploy_group_roles, :inject_istio_annotation, :boolean, default: false, null: false
+  end
+end
