5fdd7ad5a657f42eee8e60df4051c2fe0d84a61d
Michael Grosser <michael@grosser.it>
2019-11-29 15:47:56 -0800

    bring schema dumping back

--- db/migrate/20140804204455_add_soft_deletion_to_deploys.rb
+++ db/migrate/20140804204455_add_soft_deletion_to_deploys.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 class AddSoftDeletionToDeploys < ActiveRecord::Migration[4.2]
   def change
-    add_column :deploys, :deleted_at, :timestamp
+    add_column :deploys, :deleted_at, :datetime
   end
 end
--- db/migrate/20161102201243_add_metadata_to_access_tokens.rb
+++ db/migrate/20161102201243_add_metadata_to_access_tokens.rb
@@ -2,6 +2,6 @@
 class AddMetadataToAccessTokens < ActiveRecord::Migration[5.0]
   def change
     add_column :oauth_access_tokens, :description, :string
-    add_column :oauth_access_tokens, :last_used_at, :timestamp
+    add_column :oauth_access_tokens, :last_used_at, :datetime
   end
 end
