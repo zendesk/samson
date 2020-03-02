4ecd8e4cb08bc1919f18430167ae2d11d0cdb772
Patrick Robinson <patrick.robinson@envato.com>
2020-03-02 11:34:43 +1100

    Cleanup orphaned webhooks

---
+++ db/migrate/20200219043627_delete_orphaned_outbound_webhooks.rb
@@ -0,0 +1,14 @@
+class DeleteOrphanedOutboundWebhooks < ActiveRecord::Migration[6.0]
+  class OutboundWebhook < ActiveRecord::Base
+  end
+
+  class OutboundWebhookStage < ActiveRecord::Base
+  end
+
+  def up
+    OutboundWebhookStage.where('outbound_webhook_id not IN (?)', OutboundWebhook.pluck(:id)).delete_all
+  end
+
+  def down
+  end
+end
