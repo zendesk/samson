105a1866f315d78f4caa4d620ffc95b442316c57
Michael Grosser <michael@grosser.it>
2020-06-26 16:55:50 -0700

    re-run time formatter when updating the deploys active page

--- app/assets/javascripts/channels/deploy_notifications_channel.js
+++ app/assets/javascripts/channels/deploy_notifications_channel.js
@@ -1,6 +1,7 @@
 // Stream count of current deploys to users that keep a page open via app/channels/deploy_notifications_channel.rb
 // - update deploy badge
 // - reload deploys table
+// - do not immediately start to avoid server overhead when user is just clicking through the UI
 $(function(){
   setTimeout(function(){
     App.cable.subscriptions.create("DeployNotificationsChannel", {
@@ -9,7 +10,9 @@ $(function(){
         $("#current-deploys").toggle(data.count > 0).text(data.count);
 
         // reload active deploys page (noop when id is not found)
-        $('#deploys-active').load('/deploys/active?partial=true');
+        $('#deploys-active').load('/deploys/active?partial=true', function(){
+          timeAgoFormat();
+        });
       }
     });
   }, 5000);
