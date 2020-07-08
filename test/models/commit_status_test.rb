aa9105531b86b8fd712650a51a5441977c66c400
sathish <sathishavm@gmail.com>
2020-07-08 11:29:49 -0700

    Added Checks State stale

--- test/models/commit_status_test.rb
+++ test/models/commit_status_test.rb
@@ -285,6 +285,12 @@ describe CommitStatus do
         status.state.must_equal 'pending'
       end
 
+      it 'returns error if github stale the check suites' do
+        stub_github_api(check_suite_url, check_suites: [{conclusion: 'stale', id: 1}])
+
+        status.state.must_equal 'error'
+      end
+
       it 'maps check status to state equivalent' do
         stub_github_api(check_suite_url, check_suites: [{conclusion: 'action_required', id: 1}])
 
