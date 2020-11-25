65fe0c46f4c82e65216d46ea78f6c85f2c4fee58
Michael Grosser <michael@grosser.it>
2020-11-25 14:53:59 -0800

    allow searching for changes by what attribute and value changed

--- app/controllers/audits_controller.rb
+++ app/controllers/audits_controller.rb
@@ -2,7 +2,17 @@
 class AuditsController < ApplicationController
   def index
     query = params[:search]&.to_unsafe_h&.select { |_, v| v.present? }
-    @pagy, @audits = pagy(audit_scope.where(query).order(id: :desc), page: params[:page], items: 25)
+    key = query&.delete(:key)
+    value = query&.delete(:value)
+    scope = audit_scope.where(query)
+
+    # find by changed key, by matching the yaml in audited_changes
+    scope = scope.where(query_changes("%\n#{ActiveRecord::Base.send(:sanitize_sql_like, key)}:\n%")) if key
+
+    # find by changed value ('from' or 'to'), by matching the yaml in audited_changes
+    scope = scope.where(query_changes("%\n- #{ActiveRecord::Base.send(:sanitize_sql_like, value)}\n%")) if value
+
+    @pagy, @audits = pagy(scope.order(id: :desc), page: params[:page], items: 25)
   end
 
   def show
@@ -10,6 +20,11 @@ class AuditsController < ApplicationController
   end
 
   private
+
+  # redoing what Audited::Audit.arel_table[:audited_changes].matches does without converting the value into yaml
+  def query_changes(query)
+    Arel::Nodes::Matches.new Audited::Audit.arel_table[:audited_changes], Arel::Nodes.build_quoted(query), nil, false
+  end
 
   def audit_scope
     scope = Audited::Audit
