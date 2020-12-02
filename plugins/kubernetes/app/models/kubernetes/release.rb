222dd8b8162444bd3d196c4118afdf1e0c3388b4
Craig Day <cday@zendesk.com>
2020-12-02 13:04:51 -0800

    [k8s] always set blue/green on release to give unique deploy refs if needed

--- plugins/kubernetes/app/models/kubernetes/release.rb
+++ plugins/kubernetes/app/models/kubernetes/release.rb
@@ -25,11 +25,10 @@ module Kubernetes
     def self.build_release_with_docs(params)
       roles = params.delete(:grouped_deploy_group_roles).to_a
       release = new(params) do |release|
-        if roles.flatten(1).any? { |dgr| dgr.kubernetes_role.blue_green? }
-          release.blue_green_color = begin
-            release.previous_succeeded_release&.blue_green_color == "blue" ? "green" : "blue"
-          end
-        end
+        # We always set the blue green color but the template filler might not always use it.
+        # This allows us to support deploy-specific features like env vars from config maps.
+        release.blue_green_color =
+          release.previous_succeeded_release&.blue_green_color == "blue" ? "green" : "blue"
       end
       release.send :build_release_docs, roles if release.valid?
       release
