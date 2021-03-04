68c6da201490ccd4b780747c5c0ca541f01689e2
Michael Grosser <michael@grosser.it>
2021-03-04 10:42:26 -0800

    sanitize description/context to avoid XSS

--- app/controllers/commit_statuses_controller.rb
+++ app/controllers/commit_statuses_controller.rb
@@ -7,6 +7,13 @@ class CommitStatusesController < ApplicationController
   def show
     stage = current_project.stages.find_by_permalink!(params.require(:stage_id))
     commit_status = CommitStatus.new(stage.project, params.require(:ref), stage: stage)
-    render json: {state: commit_status.state, statuses: commit_status.statuses}
+    statuses = commit_status.statuses
+    # strip dangerous tags so we can safely display as html in ref_status_typeahead.js
+    statuses.each do |status|
+      [:context, :description].each do |key|
+        status[key] = ActionController::Base.helpers.strip_tags(status[key]) if status[key]
+      end
+    end
+    render json: {state: commit_status.state, statuses: statuses}
   end
 end
--- test/controllers/commit_statuses_controller_test.rb
+++ test/controllers/commit_statuses_controller_test.rb
@@ -10,47 +10,46 @@ describe CommitStatusesController do
 
   as_a :project_deployer do
     describe '#show' do
+      def call
+        CommitStatus.stubs(new: stub(commit_status_data))
+        get :show, params: valid_params
+      end
+
       let(:stage) { stages(:test_staging) }
       let(:project) { projects(:test) }
       let(:valid_params) { {project_id: project.to_param, stage_id: stage.to_param, id: 'test/test', ref: 'bar'} }
+      let(:commit_status_data) do
+        {
+          state: 'pending',
+          statuses: [{status: 'pending', description: 'the Travis build is still running'}]
+        }
+      end
+
+      it 'responds with the status' do
+        call
+        response.body.must_equal(JSON.dump(commit_status_data))
+      end
+
+      it "escapes html so we can display in js" do
+        status = commit_status_data[:statuses][0]
+        status[:context] = status[:description] = "<a>hi</a><script>no</script>"
+        call
+        response.body.must_equal(JSON.dump(commit_status_data))
+      end
 
       it "fails with unknown project" do
-        assert_raises ActiveRecord::RecordNotFound do
-          get :show, params: valid_params.merge(project_id: 'baz')
-        end
+        valid_params[:project_id] = 'baz'
+        assert_raises(ActiveRecord::RecordNotFound) { call }
       end
 
       it "fails with unknown stage" do
         stage.update_column(:project_id, 3)
-        assert_raises(ActiveRecord::RecordNotFound) { get :show, params: valid_params }
+        assert_raises(ActiveRecord::RecordNotFound) { call }
       end
 
       it "fails without ref" do
-        assert_raises ActionController::ParameterMissing do
-          get :show, params: valid_params.merge(ref: nil)
-        end
-      end
-
-      describe 'valid' do
-        let(:commit_status_data) do
-          {
-            state: 'pending',
-            statuses: [{status: 'pending', description: 'the Travis build is still running'}]
-          }
-        end
-
-        before do
-          CommitStatus.stubs(new: stub(commit_status_data))
-          get :show, params: valid_params
-        end
-
-        it 'responds ok' do
-          response.status.must_equal(200)
-        end
-
-        it 'responds with the status' do
-          response.body.must_equal(JSON.dump(commit_status_data))
-        end
+        valid_params.delete :ref
+        assert_raises(ActionController::ParameterMissing) { call }
       end
     end
   end
